b76438cebc0e3710ae803a991d6de2d889749c02
Bill Peck <bpeck@redhat.com>
2009-05-01 10:59:17 -0400

    big changes in identity to allow ldap to be an option and not required.

--- Medusa/medusa/identity.py
+++ Medusa/medusa/identity.py
@@ -17,81 +17,69 @@ class LdapSqlAlchemyIdentityProvider(SqlAlchemyIdentityProvider):
     def __init__(self):
         super(LdapSqlAlchemyIdentityProvider, self).__init__()
 
-        self.uri = get("identity.soldapprovider.uri", "ldaps://localhost")
-        self.host = get("identity.soldapprovider.host", "localhost")
-        self.port = get("identity.soldapprovider.port", 389)
-        self.basedn  = get("identity.soldapprovider.basedn", "dc=localhost")
-        self.autocreate = get("identity.soldapprovider.autocreate", False)
+        global user_class, group_class, permission_class, visit_class
+
+        self.provider = get("identity.provider", "")
+        if self.provider == 'ldapsa':
+            self.uri = get("identity.soldapprovider.uri", "ldaps://localhost")
+            self.basedn  = get("identity.soldapprovider.basedn", "dc=localhost")
+            self.autocreate = get("identity.soldapprovider.autocreate", False)
+            # Only needed for devel. comment out for Prod.
+            ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
 
         user_class_path = get("identity.saprovider.model.user",
                               None)
-        self.user_class = load_class(user_class_path)
+        user_class = load_class(user_class_path)
         group_class_path = get("identity.saprovider.model.group",
                                 None)
-        self.group_class = load_class(group_class_path)
+        group_class = load_class(group_class_path)
         permission_class_path = get("identity.saprovider.model.permission",
                                     None)
-        self.permission_class = load_class(permission_class_path)
+        permission_class = load_class(permission_class_path)
         visit_class_path = get("identity.saprovider.model.visit",
                                None)
         log.info("Loading: %s", visit_class_path)
-        self.visit_class = load_class(visit_class_path)
+        visit_class = load_class(visit_class_path)
 
         log.info("uri :: %s" % self.uri)
-        log.info("host :: %s" % self.host)
-        log.info("port :: %d" % self.port)
         log.info("basedn :: %s" % self.basedn)
         log.info("autocreate :: %s" % self.autocreate)
 
     def validate_identity(self, user_name, password, visit_key, krb=None):
-        objects = self.validate_password(None, user_name, password, krb)
-        if objects:
-            user = session.query(self.user_class).get_by(user_name=user_name)
-            if not user:
-                if self.autocreate:
-                    user = self.user_class()
-                    user.user_name = user_name
-                    user.display_name = objects[0][1]['cn'][0]
-                    user.email_address = objects[0][1]['mail'][0]
-                    session.save(user)
-                    session.flush()
-                else:
-                    return None
-            link = session.query(self.visit_class).get_by(visit_key=visit_key)
-            if not link:
-                link = self.visit_class(visit_key=visit_key, user_id=user.user_id)
-                session.save(link)
-            else:
-                link.user_id = user.user_id
-            session.flush()
-            return SqlAlchemyIdentity(visit_key, user)
-        return None
+        user = user_class.by_user_name(user_name)
+        if not user:
+            log.warning("No such user: %s", user_name)
+            return None
+        if not krb and not self.validate_password(user, user_name, password):
+            log.warning("Passwords don't match for user: %s", user_name)
+            return None
+        log.info("Associating user (%s) with visit (%s)",
+            user_name, visit_key)
+        return SqlAlchemyIdentity(visit_key, user)
 
-    def validate_password( self, user, user_name, password, krb=None ):
+    def validate_password(self, user, user_name, password):
         '''
         Validates user_name and password against an AD domain.
         
         '''
-        # Only needed for devel.  comment out for Prod.
-        ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
-
-        #ldapcon = ldap.open(self.host, self.port)
-        ldapcon = ldap.initialize(self.uri)
-        filter = "(uid=%s)" % user_name
-        rc = ldapcon.search(self.basedn, ldap.SCOPE_SUBTREE, filter)
+        if user.password == self.encrypt_password(password):
+            return True
+        if self.provider == 'ldapsa':
+            ldapcon = ldap.initialize(self.uri)
+            filter = "(uid=%s)" % user_name
+            rc = ldapcon.search(self.basedn, ldap.SCOPE_SUBTREE, filter)
                             
-        objects = ldapcon.result(rc)[1]
+            objects = ldapcon.result(rc)[1]
 
-        if(len(objects) == 0):
-            log.warning("No such LDAP user: %s" % user_name)
-            return False
-        elif(len(objects) > 1):
-            log.error("Too many users: %s" % user_name)
-            return False
+            if(len(objects) == 0):
+                log.warning("No such LDAP user: %s" % user_name)
+                return False
+            elif(len(objects) > 1):
+                log.error("Too many users: %s" % user_name)
+                return False
 
-        dn = objects[0][0]
+            dn = objects[0][0]
 
-        if not krb:
             try:
                 rc = ldapcon.simple_bind(dn, password)
                 ldapcon.result(rc)
@@ -99,7 +87,7 @@ class LdapSqlAlchemyIdentityProvider(SqlAlchemyIdentityProvider):
                 log.error("Invalid password supplied for %s" % user_name)
                 return False
 
-	return objects
+	return True
 
     def load_identity(self, visit_key):
         user = super(LdapSqlAlchemyIdentityProvider, self).load_identity(visit_key)
@@ -115,19 +103,3 @@ class LdapSqlAlchemyIdentityProvider(SqlAlchemyIdentityProvider):
             return None
         set_login_attempted( True )
         return self.validate_identity( user_name, None, visit_key, True )
-
-    def by_name(self, user_name):
-        """
-        Looks up user_name against an AD domain.
-        """
-
-        # Only needed for devel.  comment out for Prod.
-        ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
-
-        ldapcon = ldap.initialize(self.uri)
-
-        filter = "(uid=%s*)" % user_name
-        rc = ldapcon.search(self.basedn, ldap.SCOPE_SUBTREE, filter)
-                            
-        objects = ldapcon.result(rc)[1]
-        return [object[0].split(',')[0].split('=')[1] for object in objects]
