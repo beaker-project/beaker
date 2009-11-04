5d97d9d494dc8c182fcda1cbee0ad34f4bdf45e8
Bill Peck <bpeck@redhat.com>
2009-11-04 12:39:24 -0500

    rename searchbar.js to versioned name to deal with caching browsers

--- Server/beaker/server/widgets.py
+++ Server/beaker/server/widgets.py
@@ -79,7 +79,7 @@ class myPaginateDataGrid(PaginateDataGrid):
 class SearchBar(RepeatingFormField):
     """Search Bar"""
 
-    javascript = [LocalJSLink('beaker', '/static/javascript/searchbar.js')]
+    javascript = [LocalJSLink('beaker', '/static/javascript/searchbar_v2.js')]
     template = """
     <div xmlns:py="http://purl.org/kid/ns#">
     <a id="advancedsearch" href="#">Toggle Search</a>
