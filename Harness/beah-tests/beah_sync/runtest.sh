286f7c0a524fe61fc4d729e048e8e23d7de0b11d
Marian Csontos <mcsontos@redhat.com>
2010-01-29 16:26:20 +0100

    Harness:tests: beah_update, beah_sync
    * beah_sync - dummy synchronization task for multihost
    * beah_update - increased test time as cloning repo is necessary

---
+++ Harness/beah-tests/beah_sync/runtest.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+# Copyright (c) 2006 Red Hat, Inc. All rights reserved. This copyrighted material
+# is made available to anyone wishing to use, modify, copy, or
+# redistribute it subject to the terms and conditions of the GNU General
+# Public License v.2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.# The toplevel namespace within which the test lives.
+#
+# Author: Bill Peck
+
+# Environment Variables
+# $JOBID
+# $DISTRO
+# $ARCH
+# $TEST
+# $FAMILY
+# $VARIANT
+# $ARGS
+
+# source the test script helpers
+. /usr/bin/rhts-environment.sh
+
+Client() {
+    rhts-sync-set -s READY
+    rhts-sync-block -s READY $SERVERS
+    rhtsecho "Servers are READY."
+    rhts-sync-set -s DONE
+    report_result $TEST Pass 0
+}
+
+Server() {
+    rhts-sync-set -s READY
+    rhts-sync-block -s DONE $CLIENTS
+    rhtsecho "Clients are DONE."
+    report_result $TEST Pass 0
+}
+
+rhtsecho() {
+    echo "$@" | tee -a $OUTPUTFILE
+}
+
+# ---------- Start Test -------------
+rhtsecho "***** Start of runtest.sh *****"
+
+if [ -z "$SERVERS" -o -z "$CLIENTS" ]; then
+    rhtsecho "Can not determine my Role! Client/Server Failed:"
+    rhtsecho "If you are running this as a developer try setting" \
+    rhtsecho "the environment variables CLIENTS and SERVERS" \
+    report_result $TEST Warn
+    exit 1
+fi
+
+if echo $CLIENTS | grep -q $HOSTNAME; then
+    rhtsecho "Running beah-sync test as client"
+    TEST="$TEST/client"
+    Client
+fi
+if echo $SERVERS | grep -q $HOSTNAME; then
+    rhtsecho "Running beah-sync test as server"
+    TEST="$TEST/server"
+    Server
+fi
