27f2a7fd51f813df8dadb2820d99fb79bfa04137
Raymond Mancy <rmancy@redhat.com>
2010-02-15 20:15:42 +1000

    reserve workflow - added templates/js/css/etc

---
+++ Server/beaker/server/reserve_workflow.py
@@ -0,0 +1,58 @@
+from turbogears import controllers, identity, expose, url, database, validate, flash, redirect
+from sqlalchemy.sql.expression import and_
+from beaker.server.widgets import ReserveWorkflow as ReserveWorkflowWidget
+import beaker.server.model as model
+
+import logging
+log = logging.getLogger(__name__)
+
+class ReserveWorkflow:
+    widget = ReserveWorkflowWidget()
+ 
+    @expose(template='beaker.server.templates.generic') 
+    def index(self,*args,**kw):
+        values = {}
+        if 'arch' in kw:
+            values['arch'] = kw['arch']
+        if 'distro_family' in kw:
+            values['distro_family'] = kw['distro_family']
+        if 'tag' in kw:
+            values['tag'] = kw['tag']
+        if 'method' in kw:
+            values['method'] = kw['method']
+ 
+        return dict(widget=self.widget,widget_options={'values' : values},title='Reserve Workflow')
+
+
+    @expose(allow_json=True)
+    def get_arch_options(self,distro_family,method,tag):
+        """
+        get_arch_options() will return all arch's available for a particular distro_family,
+        method and tag
+        """
+        arches = model.Arch.query().outerjoin(['distros','osversion','osmajor']).outerjoin(['distros','_tags']). \
+                                  filter(and_(model.OSMajor.osmajor == distro_family,
+                                              model.Distro.method == method,
+                                              model.DistroTag.tag == tag)) 
+        options = [elem.arch for elem in arches]
+        return {'options' : options }
+
+    @expose(allow_json=True)
+    def get_distro_options(self,arch,distro_family,method,tag):       
+        """
+        get_distro_options() will return all the distros for a given arch,
+        distro_family,method and tag
+        """
+        log.debug('arch:%s distro_family:%s method:%s tag:%s ' % (arch,distro_family,method,tag))
+        distro = model.Distro.query().join(['osversion','osmajor']).join('arch').join('_tags'). \
+                                      filter(and_(model.Arch.arch == arch, 
+                                                  model.OSMajor.osmajor == distro_family,
+                                                  model.Distro.method == method,
+                                                  model.DistroTag.tag == tag))
+        options = [elem.install_name for elem in distro]  
+        log.debug(distro) 
+        return {'options': options}
+
+      
+       
+      
