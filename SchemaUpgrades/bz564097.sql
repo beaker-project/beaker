489473d7270851ca1e02d1cb2a5eda6e953a941e
Raymond Mancy <rmancy@redhat.com>
2010-03-01 16:42:10 +1000

    schema change needed for commit 2b76b1f44c02d8fd481e07de455bfb2647609a71

---
+++ SchemaUpgrades/bz564097.sql
@@ -0,0 +1,11 @@
+USE `beaker`;
+CREATE TABLE `recipeset_activity` (
+  `id` int(11) NOT NULL,
+  `recipeset_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 
+
+
+
+
+
