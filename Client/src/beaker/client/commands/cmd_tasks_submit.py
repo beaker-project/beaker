71dc4e0fc49e9d17482d9190732bdf536a2b197b
Bill Peck <bpeck@redhat.com>
2010-03-03 11:46:16 -0500

    changes to task submit to support convert

--- Client/src/beaker/client/commands/cmd_tasks_submit.py
+++ Client/src/beaker/client/commands/cmd_tasks_submit.py
@@ -2,6 +2,7 @@
 
 
 from beaker.client.task_watcher import *
+from beaker.client.convert import Convert
 from beaker.client import BeakerCommand
 from optparse import OptionValueError
 import sys
@@ -12,11 +13,40 @@ class Tasks_Submit(BeakerCommand):
 
     def options(self):
         self.parser.usage = "%%prog %s" % self.normalized_name
+        self.parser.add_option(
+            "--debug",
+            default=False,
+            action="store_true",
+            help="print the jobxml that it would submit",
+        )
+        self.parser.add_option(
+            "--dryrun",
+            default=False,
+            action="store_true",
+            help="Don't submit job to scheduler",
+        )
+        self.parser.add_option(
+            "--convert",
+            default=False,
+            action="store_true",
+            help="convert from legacy rhts xml to beaker xml",
+        )
+        self.parser.add_option(
+            "--nowait",
+            default=False,
+            action="store_true",
+            help="Don't wait on job completion",
+        )
+
 
 
     def run(self, *args, **kwargs):
         username = kwargs.pop("username", None)
         password = kwargs.pop("password", None)
+        convert  = kwargs.pop("convert", False)
+        debug   = kwargs.pop("debug", False)
+        dryrun  = kwargs.pop("dryrun", False)
+        nowait  = kwargs.pop("nowait", False)
 
         jobs = args
 
@@ -24,13 +54,20 @@ class Tasks_Submit(BeakerCommand):
         submitted_jobs = []
         failed = False
         for job in jobs:
-            try:
-                submitted_jobs.append(self.hub.jobs.upload(open(job, "r").read()))
-            except Exception, ex:
-                failed = True
-                print ex
-        TaskWatcher.watch_tasks(self.hub, submitted_jobs)
-        for submitted_job in submitted_jobs:
-            print self.hub.taskactions.to_xml(submitted_job)
-        if failed:
-            sys.exit(1)
+            jobxml = open(job, "r").read()
+            if convert:
+                jobxml = Convert.rhts2beaker(jobxml)
+            if debug:
+                print jobxml
+            if not dryrun:
+                try:
+                    submitted_jobs.append(self.hub.jobs.upload(jobxml))
+                except Exception, ex:
+                    failed = True
+                    print ex
+        if not dryrun and not nowait:
+            TaskWatcher.watch_tasks(self.hub, submitted_jobs)
+            for submitted_job in submitted_jobs:
+                print self.hub.taskactions.to_xml(submitted_job)
+            if failed:
+                sys.exit(1)
