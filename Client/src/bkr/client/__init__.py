09fc84e5bebf633b3ed2f2497f8ca4fa46c31fe6
Bill Peck <bpeck@redhat.com>
2010-05-04 22:41:06 -0400

    WIP
    new workflow-simple command
    initial BeakerJob/RecipeSet/Recipe helper objects.

--- Client/src/bkr/client/__init__.py
+++ Client/src/bkr/client/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import os
+import xml.dom.minidom
+import sys
 from kobo.client import ClientCommand
 
 class BeakerCommand(ClientCommand):
@@ -8,9 +10,204 @@ class BeakerCommand(ClientCommand):
     conf_environ_key = 'BEAKER_CLIENT_CONF'
 
     if not os.environ.has_key(conf_environ_key):
-        user_conf = os.path.expanduser('~/.beaker')
+        user_conf = os.path.expanduser('~/.beaker_client/config')
+        old_conf = os.path.expanduser('~/.beaker')
         if os.path.exists(user_conf):
             conf = user_conf
+        elif os.path.exists(old_conf):
+            sys.stderr.write("%s is deprecated for config, please use %s instead\n" % (old_conf, 
+                                                                                         user_conf))
+            conf = old_conf
         else:
             conf = "/etc/beaker/client.conf"
         os.environ[conf_environ_key] = conf
+
+class BeakerWorkflow(BeakerCommand):
+    doc = xml.dom.minidom.Document()
+
+    def options(self):
+        """ Default options that all Workflows use """
+
+        self.parser.add_option(
+            "--debug",
+            default=False,
+            action="store_true",
+            help="print the jobxml that it would submit",
+        )
+        self.parser.add_option(
+            "--dryrun",
+            default=False,
+            action="store_true",
+            help="Don't submit job to scheduler",
+        )
+        self.parser.add_option(
+            "--arch",
+            action="append",
+            default=[],
+            help="Include this Arch in job",
+        )
+        self.parser.add_option(
+            "--distro",
+            help="Use this Distro for job",
+        )
+        self.parser.add_option(
+            "--family",
+            help="Pick latest distro of this family for job",
+        )
+        self.parser.add_option(
+            "--variant",
+            help="Pick distro with this variant for job",
+        )
+        self.parser.add_option(
+            "--machine",
+            help="Require this machine for job",
+        )
+        self.parser.add_option(
+            "--package",
+            action="append",
+            default=[],
+            help="Include tests for Package in job",
+        )
+        self.parser.add_option(
+            "--tag",
+            action="append",
+            default=[],
+            help="Pick latest distro matching this tag for job",
+        )
+        self.parser.add_option(
+            "--repo",
+            action="append",
+            default=[],
+            help="Include this repo in job",
+        )
+        self.parser.add_option(
+            "--task",
+            action="append",
+            default=[],
+            help="Include this task in job",
+        )
+        self.parser.add_option(
+            "--taskparams",
+            action="append",
+            default=[],
+            help="Set task params 'name=value'",
+        )
+        self.parser.add_option(
+            "--type",
+            action="append",
+            default=[],
+            help="Include tasks of this type in job",
+        )
+        self.parser.add_option(
+            "--keyvalues",
+            action="append",
+            default=[],
+            help="Specify a system that matches these key/values",
+        )
+        self.parser.add_option(
+            "--whiteboard",
+            default="",
+            help="Set the whiteboard for this job",
+        )
+        self.parser.add_option(
+            "--nowait",
+            default=False,
+            action="store_true",
+            help="Don't wait on job completion",
+        )
+
+    def getTasks(self, *args, **kwargs):
+        """ get all requested tasks """
+
+        tasks = list(args)
+        #FIXME add tasks based on packages
+        #FIXME add tasks based on task type
+        tasks.extend([])
+        return tasks
+
+class BeakerBase(object):
+    doc = xml.dom.minidom.Document()
+
+    def toxml(self):
+        """ return xml of job """
+        return self.node.toprettyxml()
+
+class BeakerJob(BeakerBase):
+    def __init__(self, *args, **kwargs):
+        self.node = self.doc.createElement('job')
+        whiteboard = self.doc.createElement('whiteboard')
+        whiteboard.appendChild(self.doc.createTextNode(kwargs.pop("whiteboard","")))
+        self.node.appendChild(whiteboard)
+
+    def addRecipeSet(self, recipeSet):
+        """ properly add a recipeSet to this job """
+        self.node.appendChild(recipeSet.node)
+
+    def addRecipe(self, recipe):
+        """ properly add a recipe to this job """
+        recipeSet = self.doc.createElement('recipeSet')
+        recipeSet.appendChild(recipe.node)
+        self.node.appendChild(recipeSet)
+
+class BeakerRecipeSet(BeakerBase):
+    def __init__(self, *args, **kwargs):
+        self.node = self.doc.createElement('recipeSet')
+
+    def addRecipe(self, recipe):
+        """ properly add a recipe to this recipeSet """
+        self.node.appendChild(recipe.node)
+
+class BeakerRecipe(BeakerBase):
+    def __init__(self, *args, **kwargs):
+        self.node = self.doc.createElement('recipe')
+        self.andDistroRequires = self.doc.createElement('and')
+        self.andHostRequires = self.doc.createElement('and')
+        self.tasks = self.doc.createElement('tasks')
+        distroRequires = self.doc.createElement('distroRequires')
+        hostRequires = self.doc.createElement('hostRequires')
+        distroRequires.appendChild(self.andDistroRequires)
+        hostRequires.appendChild(self.andHostRequires)
+        self.node.appendChild(distroRequires)
+        self.node.appendChild(hostRequires)
+        self.node.appendChild(self.tasks)
+
+    def addBaseRequires(self, *args, **kwargs):
+        """ Add base requires """
+        distro = kwargs.get("distro", None)
+        family = kwargs.get("family", None)
+        tags = kwargs.get("tag", [])
+        if distro:
+            distroName = self.doc.createElement('distro_name')
+            distroName.setAttribute('value', '%s' % distro)
+            self.addDistroRequires(distroName)
+        if family:
+            distroFamily = self.doc.createElement('distro_family')
+            distroFamily.setAttribute('value', '%s' % family)
+            self.addDistroRequires(distroFamily)
+        for tag in tags:
+            distroTag = self.doc.createElement('distro_tag')
+            distroTag.setAttribute('op', '=')
+            distroTag.setAttribute('value', '%s' % tag)
+            self.addDistroRequires(distroTag)
+
+    def addHostRequires(self, node):
+        self.andHostRequires.appendChild(node)
+
+    def addDistroRequires(self, node):
+        self.andDistroRequires.appendChild(node)
+
+    def addTask(self, task, role='STANDALONE', paramNodes=[], taskParams=[]):
+        recipeTask = self.doc.createElement('task')
+        recipeTask.setAttribute('name', '%s' % task)
+        recipeTask.setAttribute('role', '%s' % role)
+        params = self.doc.createElement('params')
+        for param in paramNodes:
+            params.appendChild(param)
+        for taskParam in taskParams:
+            param = self.doc.createElement('param')
+            param.setAttribute('name' , taskParam.split('=',1)[0])
+            param.setAttribute('value' , taskParam.split('=',1)[1])
+            params.appendChild(param)
+        recipeTask.appendChild(params)
+        self.tasks.appendChild(recipeTask)
+
---
+++ Client/src/bkr/client/commands/cmd_workflow_simple.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+
+from bkr.client.task_watcher import *
+from bkr.client import BeakerCommand, BeakerWorkflow, BeakerJob, BeakerRecipe
+from optparse import OptionValueError
+import sys
+import xml.dom.minidom
+import copy
+
+class Workflow_Simple(BeakerWorkflow):
+    """Simple workflow to generate job to scheduler"""
+    enabled = True
+    doc = xml.dom.minidom.Document()
+
+    def options(self):
+        super(Workflow_Simple, self).options()
+        self.parser.usage = "%%prog %s [options] <task> <task>" % self.normalized_name
+
+    def run(self, *args, **kwargs):
+        username = kwargs.pop("username", None)
+        password = kwargs.pop("password", None)
+
+        # get all tasks requested
+        requestedTasks = self.getTasks(*args, **kwargs)
+
+        debug   = kwargs.get("debug", False)
+        dryrun  = kwargs.get("dryrun", False)
+        nowait  = kwargs.get("nowait", False)
+	family = kwargs.get("family", None)
+	distro = kwargs.get("distro", None)
+	arches = kwargs.get("arch", [])
+	taskParams = kwargs.get("taskparam", [])
+
+        if not family and not distro:
+            sys.stderr.write("No Family or Distro specified\n")
+            sys.exit(1)
+
+        if not requestedTasks:
+            sys.stderr.write("You must specify a package, type or task to run\n")
+            sys.exit(1)
+
+        # Create Job
+        job = BeakerJob(*args, **kwargs)
+
+        # Create Base Recipe
+        recipeTemplate = BeakerRecipe()
+
+        # Add Distro Requirements
+        recipeTemplate.addBaseRequires(*args, **kwargs)
+
+        # Add Host Requirements
+
+        # Add tasks that have been requested
+        for task in requestedTasks:
+            recipeTemplate.addTask(task, taskParams=taskParams)
+
+        for arch in arches:
+            # Copy basic requirements
+            recipe = copy.deepcopy(recipeTemplate)
+
+            arch_node = self.doc.createElement('distro_arch')
+            arch_node.setAttribute('value', arch)
+            recipe.addDistroRequires(arch_node)
+
+            job.addRecipe(recipe)
+
+
+        # jobxml
+        jobxml = job.toxml()
+
+        if debug:
+            print jobxml
+
+        self.set_hub(username, password)
+        submitted_jobs = []
+        failed = False
+
+        if not dryrun:
+            try:
+                submitted_jobs.append(self.hub.jobs.upload(jobxml))
+            except Exception, ex:
+                failed = True
+                print ex
+        if not dryrun:
+            if not nowait:
+                TaskWatcher.watch_tasks(self.hub, submitted_jobs)
+            for submitted_job in submitted_jobs:
+                print self.hub.taskactions.to_xml(submitted_job)
+            if failed:
+                sys.exit(1)
