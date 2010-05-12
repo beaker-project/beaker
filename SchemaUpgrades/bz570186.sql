843bb50169fe08b253ef5c2582ab9f94d77da564
Raymond Mancy <rmancy@redhat.com>
2010-05-12 16:51:02 +1000

    bz570186 - Opps, needed to add cascading on FK constraint

--- SchemaUpgrades/bz570186.sql
+++ SchemaUpgrades/bz570186.sql
@@ -4,7 +4,7 @@ CREATE TABLE `system_admin_map` (
     `group_id` int(11) NOT NULL,
     UNIQUE KEY `sys_id_group_id` (`system_id`,`group_id`),
     KEY `group_id` (`group_id`),
-    CONSTRAINT `system_admin_map_ibfk_1` FOREIGN KEY (`system_id`) REFERENCES `system` (`id`),
-    CONSTRAINT `system_admin_map_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `tg_group` (`group_id`)
+    CONSTRAINT `system_admin_map_ibfk_1` FOREIGN KEY (`system_id`) REFERENCES `system` (`id`) ON DELETE CASCADE,
+    CONSTRAINT `system_admin_map_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `tg_group` (`group_id`) ON DELETE CASCADE,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8  
 
