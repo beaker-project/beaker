58827ff7e00442042d01fa660f965d6c597b03a6
Bill Peck <bpeck@redhat.com>
2010-05-17 13:17:08 -0400

    Run authenticate in a separate thread.

--- LabController/proxy/src/bkr/labcontroller/main.py
+++ LabController/proxy/src/bkr/labcontroller/main.py
@@ -1,8 +1,10 @@
 
 import os
+import time
 import sys
 import signal
 from optparse import OptionParser
+from threading import Thread
 
 import SocketServer
 import DocXMLRPCServer
@@ -16,24 +18,39 @@ from kobo.tback import Traceback, set_except_hook
 from kobo.log import add_stderr_logger
 set_except_hook()
 
-class myHandler(DocXMLRPCServer.DocXMLRPCRequestHandler):
-    def do_POST(self):
-        # try and log back in if needed
-        try:
-            self.server.instance.hub._login(verbose=self.server.instance.hub._conf.get("DEBUG_XMLRPC"))
-        except KeyboardInterrupt:
-            raise
-        except Exception, e:
-            raise
-        DocXMLRPCServer.DocXMLRPCRequestHandler.do_POST(self)
+class Authenticate(Thread):
+    def __init__ (self,proxy):
+      Thread.__init__(self)
+      self.proxy = proxy
+      self.__serving = False
 
+    def run(self):
+        self.__serving = True
+        count = 0
+        while self.__serving:
+            count += 1
+            # every minute check that we are logged in.
+            if count % 60 == 0:
+                count = 0
+                try:
+                    self.proxy.hub._login(verbose=self.proxy.hub._conf.get("DEBUG_XMLRPC"))
+                except KeyboardInterrupt:
+                    raise
+                except Exception, e:
+                    raise
+            time.sleep(1)
 
+    def stop(self):
+        """Stops the thread"""
+        self.__serving = False
+            
 class ForkingXMLRPCServer (SocketServer.ForkingMixIn,
                            DocXMLRPCServer.DocXMLRPCServer):
     allow_reuse_address = True
 
 
 def daemon_shutdown(*args, **kwargs):
+    login.stop()
     raise ShutdownException()
 
 def main_loop(conf=None, foreground=False):
@@ -49,10 +66,18 @@ def main_loop(conf=None, foreground=False):
         sys.stderr.write("Error initializing Proxy: %s\n" % ex)
         sys.exit(1)
 
-
-    server = ForkingXMLRPCServer(("", 8000),myHandler)
+    login = Authenticate(proxy)
+    login.start()
+    server = ForkingXMLRPCServer(("", 8000))
     server.register_instance(proxy)
-    server.serve_forever()
+    try:
+        server.serve_forever()
+    except (ShutdownException, KeyboardInterrupt):
+        login.stop()
+        # ignore keyboard interrupts and sigterm
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
+        signal.signal(signal.SIGTERM, signal.SIG_IGN)
+
 
 def main():
     parser = OptionParser()
