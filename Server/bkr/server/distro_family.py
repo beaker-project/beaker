01af785163eb485e7222bb0449e0ba4f25a50b87
Raymond Mancy <rmancy@redhat.com>
2010-06-17 10:38:30 +1000

    bz604906 - fixed pagination settings in Distro-> Family

--- Server/bkr/server/distro_family.py
+++ Server/bkr/server/distro_family.py
@@ -6,9 +6,8 @@ from bkr.server.widgets import myPaginateDataGrid
 class DistroFamily(OSVersions):
 
 
-    @expose(template="bkr.server.templates.admin_grid")
-    @paginate('list',limit=50, default_order='osmajor', allow_limit_override=True)
-    def index(self,*args,**kw):        
+    @expose(template="bkr.server.templates.admin_grid") 
+    def index(self,*args,**kw): 
         kw['grid'] = myPaginateDataGrid(fields=[
                                   myPaginateDataGrid.Column(name='osmajor', getter=lambda x: x, title='OS Version', options=dict(sortable=True)),
                                   myPaginateDataGrid.Column(name='arches', getter=lambda x: " ".join([arch.arch for arch in x.arches]), title='Arches', options=dict(sortable=True)), 
