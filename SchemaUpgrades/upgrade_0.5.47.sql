ed3c47ecca64c4759155d236c5f68ef8df410853
Bill Peck <bpeck@redhat.com>
2010-07-13 15:35:55 -0400

    fix upgrade sql for ack/nak.

--- SchemaUpgrades/upgrade_0.5.47.sql
+++ SchemaUpgrades/upgrade_0.5.47.sql
@@ -7,4 +7,4 @@ CREATE TABLE `response` (
 
 INSERT INTO response values(1,'ack'),(2,'nak');
 
-CREATE TABLE `recipe_set_nacked` (`recipe_set_id` int(11) NOT NULL,   `comment` varchar(255) default NULL,   `created` datetime default NULL,   PRIMARY KEY  (`recipe_set_id`),   FOREIGN KEY (`recipe_set_id`) REFERENCES `recipe_set` (`id`) ON DELETE CASCADE ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `recipe_set_nacked` (`recipe_set_id` int(11) NOT NULL,   `comment` varchar(255) default NULL,   `created` datetime default NULL,  `response_id` int(11) NOT NULL,   PRIMARY KEY  (`recipe_set_id`),   FOREIGN KEY (`recipe_set_id`) REFERENCES `recipe_set` (`id`) ON DELETE CASCADE, FOREIGN KEY (`response_id`) REFERENCES `response` (`id`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
