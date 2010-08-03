f920d31f7d4a8e936e916e9e73e9627c8508930a
Bill Peck <bpeck@redhat.com>
2010-08-03 16:08:03 -0400

    remove uneeded schema upgrades.

--- SchemaUpgrades/bz610259.sql
+++
@@ -1,2 +0,0 @@
-USE beaker;
-ALTER TABLE recipe ADD COLUMN _ks_appends text NULL;
--- SchemaUpgrades/upgrade_0.5.51.sql
+++
@@ -1,4 +0,0 @@
-#USE beaker;
-#ALTER TABLE recipe ADD COLUMN _ks_appends text NULL;
-#
-# Just run beaker-init to create the new table ks_appends.
