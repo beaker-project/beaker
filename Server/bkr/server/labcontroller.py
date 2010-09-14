bf876c1c26c83ea294b28b5ec27a624b425afd69
Bill Peck <bpeck@redhat.com>
2010-09-14 15:42:10 -0400

    full path to bkr.timeout_xmlrpclib.

--- Server/bkr/server/labcontroller.py
+++ Server/bkr/server/labcontroller.py
@@ -229,7 +229,7 @@ class LabControllers(RPCRoot):
             labcontroller = LabController.by_id(kw['id'])
             now = time.time()
             url = "http://%s/cobbler_api" % labcontroller.fqdn
-            remote = timeout_xmlrpclib.ServerProxy(url)
+            remote = bkr.timeout_xmlrpclib.ServerProxy(url)
             try:
                 token = remote.login(labcontroller.username,
                                  labcontroller.password)
