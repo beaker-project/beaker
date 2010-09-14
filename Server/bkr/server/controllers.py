beaker-project/beaker

commit c77f9196509d753d104db24f283c6b52d0af409c

bz620604 - Removed take for those without the correct permissions.

Server/bkr/server/controllers.py
@@ -825,19 +825,44 @@ class Root(RPCRoot):
         is_user = False
         if system:
             title = system.fqdn
-            if system.can_admin(user=identity.current.user): 
+            our_user = identity.current.user #simple optimisation
+            currently_held = system.user == our_user
+            if system.can_admin(user=our_user): 
                 options['owner_change_text'] = ' (Change)'
             else:
                 readonly = True
-            if system.can_loan(identity.current.user):
+            if system.can_loan(our_user):
                 options['loan_text'] = ' (Loan)'
-            if system.current_loan(identity.current.user):
+            if system.current_loan(our_user):
                 options['loan_text'] = ' (Return)'
-            if system.can_share(identity.current.user):
+
+            if system.can_share(our_user) and system.can_provision_now(our_user): #Has privs and machine is available, can take
                 options['user_change_text'] = ' (Take)'
-            if system.current_user(identity.current.user):
-                options['user_change_text'] = ' (Return)'
-                is_user = True
+                self.provision_now_rights = True
+                self.will_provision = False 
+            elif not system.can_provision_now(our_user): # If you don't have privs to take
+                if system.is_available(our_user): #And you have access to the machine, schedule
+                    self.provision_action = '/schedule_provision'
+                    self.provision_now_rights = False
+                    self.will_provision = True
+                else: #No privs to machine at all
+                    self.will_provision = False  
+                    self.provision_now_rights = False 
+            elif system.can_provision_now(our_user) and currently_held: #Has privs, and we are current user, we can provision
+                self.provision_action = '/action_provision'
+                self.provision_now_rights = True
+                self.will_provision = True
+            elif system.can_provision_now(our_user) and not currently_held: #Has privs, not current user, You need to Take it first
+                self.provision_now_rights = True
+                self.will_provision = False
+            else:
+                log.error('Could not follow logic when determining user access to machine')
+                self.will_provision = False
+                self.provision_now_rights = False
+
+        if system.current_user(our_user):
+            options['user_change_text'] = ' (Return)'
+            is_user = True
         else:
             title = 'New'
 
@@ -897,7 +922,7 @@ class Root(RPCRoot):
                                     notes     = '/save_note',
                                     groups    = '/save_group',
                                     install   = '/save_install',
-                                    provision = '/action_provision',
+                                    provision = getattr(self,'provision_action',''),
                                     power_action = '/action_power',
                                     arches    = '/save_arch',
                                     tasks     = '/tasks/do_search',
@@ -919,6 +944,8 @@ class Root(RPCRoot):
                                                 provisions = system.provisions,
                                                 prov_arch = [(arch.id, arch.arch) for arch in system.arch]),
                                    provision = dict(is_user = is_user,
+                                                    will_provision = self.will_provision,
+                                                    provision_now_rights = self.provision_now_rights,
                                                     lab_controller = system.lab_controller,
                                                     prov_install = [(distro.id, distro.install_name) for distro in system.distros().order_by(distro_table.c.install_name)]),
                                    power_action = dict(is_user=is_user),
@@ -1422,24 +1449,78 @@ class Root(RPCRoot):
 
     @expose()
     @identity.require(identity.not_anonymous())
-    def action_provision(self, id, prov_install=None, ks_meta=None, 
-                             koptions=None, koptions_post=None, reboot=None):
+    def schedule_provision(self,id, prov_install=None, ks_meta=None, koptions=None, koptions_post=None, reserve_days=None, **kw):
+        distro_id = prov_install
         try:
-            system = System.by_id(id,identity.current.user)
+            user = identity.current.user
+            system = System.by_id(id,user)
         except InvalidRequestError:
-            flash( _(u"Unable to save Provision for %s" % id) )
+            flash( _(u"Unable to save scheduled provision for %s" % id) )
             redirect("/")
         try:
-            distro = Distro.by_id(prov_install)
+            distro = Distro.by_id(distro_id)
         except InvalidRequestError:
             flash( _(u"Unable to lookup distro for %s" % id) )
+            redirect(u"/view/%s" % system.fqdn)
+    
+        reserve_days = int(reserve_days)
+        if reserve_days is None:#This should not happen
+            log.debug('reserve_days has not been set in provision page, using default')
+            reserve_days = SystemProvision.DEFAULT_RESERVE_DAYS
+        else: 
+            if reserve_days > SystemProvision.MAX_DAYS_PROVISION: #Someone is trying to cheat
+                log.debug('User has tried to set provision to %s days, which is more than the allowable %s days' % (reserve_days,SystemProvision.DEFAULT_RESERVE_DAYS))
+                reserve_days = SystemProvision.DEFAULT_RESERVE_DAYS
+
+        reserve_time =  ((reserve_days * 24) * 60) * 60    
+        job_details = dict(whiteboard = 'Provision %s' % distro.name,
+                            distro_id = distro_id,
+                            system_id = id,
+                            ks_meta = ks_meta,
+                            koptions = koptions,
+                            koptions_post = koptions_post,
+                            reservetime = reserve_time)
+
+        try:                               
+            provision_system_job = Job.provision_system_job(**job_details)
+        except BX, msg:
+            flash(_(u"%s" % msg))
+            redirect(u"/view/%s" % system.fqdn)
+
+        self.jobs.success_redirect(provision_system_job.id)
+    
+    @expose()
+    @identity.require(identity.not_anonymous())
+    def action_provision(self, id, prov_install=None, ks_meta=None, 
+                             koptions=None, koptions_post=None, reboot=None):
+
+        """
+        We schedule a job which will provision a system. 
+
+        """
+        distro_id = prov_install
+        try:
+            user = identity.current.user
+            system = System.by_id(id,user)
+        except InvalidRequestError:
+            flash( _(u"Unable to save scheduled provision for %s" % id) )
             redirect("/")
         try:
-            system.action_provision(distro = distro,
-                                    ks_meta = ks_meta,
-                                    kernel_options = koptions,
-                                    kernel_options_post = koptions_post)
-        except BX, msg:
+            distro = Distro.by_id(distro_id)
+        except InvalidRequestError:
+            flash( _(u"Unable to lookup distro for %s" % id) )
+            redirect(u"/view/%s" % system.fqdn)
+         
+        try:
+            can_provision_now = system.can_provision_now(user) #Check perms
+            if can_provision_now:
+                system.action_provision(distro = distro,
+                                        ks_meta = ks_meta,
+                                        kernel_options = koptions,
+                                        kernel_options_post = koptions_post)
+            else: #This shouldn't happen, maybe someone is trying to be funny
+                raise BX('User: %s has insufficent permissions to provision %s' % (user.user_name, system.fqdn))
+        except BX, msg: 
             activity = SystemActivity(identity.current.user, 'WEBUI', 'Provision', 'Distro', "", "%s: %s" % (msg, distro.install_name))
             system.activity.append(activity)
             flash(_(u"%s" % msg))

Server/bkr/server/jobs.py
@@ -80,6 +80,11 @@ class Jobs(RPCRoot):
         submit_text = _(u'Queue')
     )
 
+    @classmethod
+    def success_redirect(cls, id, url='/jobs', *args, **kw):
+        flash(_(u'Success! job id: %s' % id))
+        redirect('%s' % url)
+
     @expose(template='bkr.server.templates.form-post')
     @identity.require(identity.not_anonymous())
     def new(self, **kw):
@@ -174,8 +179,7 @@ class Jobs(RPCRoot):
                 )
             session.save(job)
             session.flush()
-            flash(_(u'Success! job id: %s' % job.id))
-            redirect(".")
+            self.success_redirect(job.id)
         return dict(
             title = 'Clone Job %s' % id,
             form = self.job_form,
@@ -183,6 +187,7 @@ class Jobs(RPCRoot):
             options = {},
             value = dict(textxml = "%s" % textxml),
         )
+
 
     def process_xmljob(self, xmljob, user):
         job = Job(whiteboard='%s' % xmljob.whiteboard, ttasks=0,

Server/bkr/server/model.py
@@ -1688,8 +1688,13 @@ $SNIPPET("rhts_post")
             return False
 
     def is_admin(self,group_id=None,user_id=None,groups=None,*args,**kw):
-
-        if identity.current.user.is_admin() is True: #first let's see if we are an _admin_
+        try:
+            if identity.current.user.is_admin() is True: #first let's see if we are an _admin_
+                return True
+        except AttributeError,e: pass #We may not be logged in...
+        
+        #If we are the owner.... 
+        if self.owner == User.by_id(user_id):
             return True
 
         if group_id: #Let's try this next as this will be the quicker query
@@ -1714,8 +1719,7 @@ $SNIPPET("rhts_post")
         groups = identity.current.user.groups
         for group in groups:
             if group.can_admin_system(self.id):
-                return True
-    
+                return True 
         return False
 
     def can_admin(self,user=None,group_id=None): 
@@ -1724,6 +1728,14 @@ $SNIPPET("rhts_post")
                 return True
         return False
 
+    def can_provision_now(self,user=None):
+        if user is not None and self.is_admin(user_id=user.user_id):
+            return True
+        else:
+            if user is not None and self.loaned == user:
+                return True
+        return False
+                
     def can_loan(self, user=None):
         if user and not self.loaned and not self.user:
             if self.can_admin(user):
@@ -1744,8 +1756,27 @@ $SNIPPET("rhts_post")
                or self.can_admin(user):
                 return True
         return False
+
+    def is_available(self,user=None):
+        """
+        is_available() will return true if this system is allowed to be used by the user.
+        """
+        if user:
+            if self.loaned and self.loaned == user:
+                return True
+            if self.shared:
+                # If the user is in the Systems groups
+                if self.groups:
+                    for group in user.groups:
+                        if group in self.groups:
+                            return True
+                else: 
+                    return True
         
     def can_share(self, user=None):
+        """
+        can_share() will return True id the system is currently free and allwoed to be used by the user
+        """
         if user and not self.user:
             # If the system is loaned its exclusive!
             if self.loaned:
@@ -1966,6 +1997,12 @@ $SNIPPET("rhts_post")
                          kernel_options_post=None,
                          kickstart=None,
                          ks_appends=None):
+        try:
+            if not self.can_provision_now(identity.current.user): #Needs to be an authorised user to do this
+                return False
+        except AttributeError, e: #Anonymous can't provision now
+            return False
+
         if not self.remote:
             return False
         self.remote.provision(distro=distro, 
@@ -3160,13 +3197,65 @@ class Job(TaskBase):
                     
         return current_nacks 
 
-
-
     @classmethod
     def by_whiteboard(cls,desc):
         res = Job.query().filter_by(whiteboard = desc)
         return res
 
+    @classmethod
+    def provision_system_job(cls, distro_id, **kw):
+        """ Create a new reserve job, if system_id is defined schedule it too """
+        job = Job(ttasks=0, owner=identity.current.user)
+        if kw.get('whiteboard'):
+            job.whiteboard = kw.get('whiteboard') 
+        if not isinstance(distro_id,list):
+            distro_id = [distro_id]
+
+        for id in distro_id: 
+            try:
+                distro = Distro.by_id(id)
+            except InvalidRequestError:
+                raise BX(u'Invalid Distro ID %s' % id)
+            recipeSet = RecipeSet(ttasks=2)
+            recipe = MachineRecipe(ttasks=2)
+            # Inlcude the XML definition so that cloning this job will act as expected.
+            recipe.distro_requires = distro.to_xml().toxml()
+            recipe.distro = distro
+            # Don't report panic's for reserve workflow.
+            recipe.panic = 'ignore'
+            if kw.get('system_id'):
+                try:
+                    system = System.by_id(kw.get('system_id'), identity.current.user)
+                except InvalidRequestError:
+                    raise BX(u'Invalid System ID %s' % system_id)
+                # Inlcude the XML definition so that cloning this job will act as expected.
+                recipe.host_requires = system.to_xml().toxml()
+                recipe.systems.append(system)
+            if kw.get('ks_meta'):
+                recipe.ks_meta = kw.get('ks_meta')
+            if kw.get('koptions'):
+                recipe.kernel_options = kw.get('koptions')
+            if kw.get('koptions_post'):
+                recipe.kernel_options_post = kw.get('koptions_post')
+            # Eventually we will want the option to add more tasks.
+            # Add Install task
+            recipe.append_tasks(RecipeTask(task = Task.by_name(u'/distribution/install')))
+            # Add Reserve task
+            reserveTask = RecipeTask(task = Task.by_name(u'/distribution/reservesys'))
+            if kw.get('reservetime'):
+                #FIXME add DateTimePicker to ReserveSystem Form
+                reserveTask.params.append(RecipeTaskParam( name = 'RESERVETIME', 
+                                                                value = kw.get('reservetime')
+                                                            )
+                                        )
+            recipe.append_tasks(reserveTask)
+            recipeSet.recipes.append(recipe)
+            job.recipesets.append(recipeSet)
+            job.ttasks += recipeSet.ttasks
+        session.save(job)
+        session.flush()
+        return job
+        
     def clone_link(self):
         """ return link to clone this job
         """

Server/bkr/server/reserve_workflow.py
@@ -1,13 +1,16 @@
 from turbogears import controllers, identity, expose, url, database, validate, flash, redirect
 from turbogears.database import session
 from sqlalchemy.sql.expression import and_, func, not_
+from sqlalchemy.exceptions import InvalidRequestError
 from bkr.server.widgets import ReserveWorkflow as ReserveWorkflowWidget
 from bkr.server.widgets import ReserveSystem
 from bkr.server.model import (osversion_table, distro_table, osmajor_table, arch_table, distro_tag_table,
                               Distro, Job, RecipeSet, MachineRecipe, System, RecipeTask, RecipeTaskParam,
                               Task, Arch, OSMajor, DistroTag)
-from sqlalchemy.exceptions import InvalidRequestError
+from bkr.server.model import Job
+from bkr.server.jobs import Jobs as JobController
 from bkr.server.cobbler_utils import hash_to_string
+from bexceptions import *
 import re
 import logging
 log = logging.getLogger(__name__)
@@ -20,60 +23,15 @@ class ReserveWorkflow:
     @identity.require(identity.not_anonymous())
     def doit(self, distro_id, **kw):
         """ Create a new reserve job, if system_id is defined schedule it too """
-        job = Job(ttasks=0, owner=identity.current.user)
-        if kw.get('whiteboard'):
-            job.whiteboard = kw.get('whiteboard') 
-        if not isinstance(distro_id,list):
-            distro_id = [distro_id]
-
-        for id in distro_id: 
-            try:
-                distro = Distro.by_id(id)
-            except InvalidRequestError:
-                flash(_(u'Invalid Distro ID %s' % id))
-                redirect('/')
-            recipeSet = RecipeSet(ttasks=2)
-            recipe = MachineRecipe(ttasks=2)
-            # Inlcude the XML definition so that cloning this job will act as expected.
-            recipe.distro_requires = distro.to_xml().toxml()
-            recipe.distro = distro
-            # Don't report panic's for reserve workflow.
-            recipe.panic = 'ignore'
-            if kw.get('system_id'):
-                try:
-                    system = System.by_id(kw.get('system_id'), identity.current.user)
-                except InvalidRequestError:
-                    flash(_(u'Invalid System ID %s' % system_id))
-                    redirect('/')
-                # Inlcude the XML definition so that cloning this job will act as expected.
-                recipe.host_requires = system.to_xml().toxml()
-                recipe.systems.append(system)
-            if kw.get('ks_meta'):
-                recipe.ks_meta = kw.get('ks_meta')
-            if kw.get('koptions'):
-                recipe.kernel_options = kw.get('koptions')
-            if kw.get('koptions_post'):
-                recipe.kernel_options_post = kw.get('koptions_post')
-            # Eventually we will want the option to add more tasks.
-            # Add Install task
-            recipe.append_tasks(RecipeTask(task = Task.by_name(u'/distribution/install')))
-            # Add Reserve task
-            reserveTask = RecipeTask(task = Task.by_name(u'/distribution/reservesys'))
-            if kw.get('reservetime'):
-                #FIXME add DateTimePicker to ReserveSystem Form
-                reservetask.params.append(RecipeTaskParam( name = 'RESERVETIME', 
-                                                                value = kw.get('reservetime')
-                                                            )
-                                        )
-            recipe.append_tasks(reserveTask)
-            recipeSet.recipes.append(recipe)
-            job.recipesets.append(recipeSet)
-            job.ttasks += recipeSet.ttasks
-        session.save(job)
-        session.flush()
-
-        flash(_(u'Successfully queued job %s' % job.id))
-        redirect('/jobs/%s' % job.id)
+        if 'system_id' in kw:
+            kw['id'] = kw['system_id']
+         
+        try:
+            provision_system_job = Job.provision_system_job(distro_id, **kw)
+        except BX, msg:
+            flash(_(u"%s" % msg))
+            redirect(u".")
+        JobController.success_redirect(provision_system_job.id)
 
     @expose(template='bkr.server.templates.form')
     @identity.require(identity.not_anonymous())

Server/bkr/server/templates/system_provision.kid
@@ -2,8 +2,8 @@
  name="${name}"
  action="${tg.url(action)}"
  method="${method}" width="100%">
- <span py:if="lab_controller">
-  <span py:if="is_user">
+ <span py:if="lab_controller and will_provision is True">
+  <span py:if="not tg.identity.anonymous">
    <script language="JavaScript" type="text/JavaScript">
     ${name}_0 = new Provision('${id.field_id}', '${prov_install.field_id}', '${ks_meta.field_id}','${koptions.field_id}','${koptions_post.field_id}','${tg.url('/get_installoptions')}');
     addLoadEvent(${name}_0.initialize);
@@ -38,6 +38,7 @@
      <td>${display_field_for("koptions_post")}
      </td>
     </tr>
+    <span py:if="provision_now_rights">
     <tr py:if="power_enabled">
      <td>${display_field_for("reboot")}</td>
      <td><label class="fieldlabel"
@@ -51,17 +52,37 @@
     <tr>
      <td>&nbsp;</td>
      <td>
-      <a class="button" href="javascript:document.${name}.submit();">Provision System</a>
+      <a class="button" href="javascript:document.${name}.submit();">Provision</a>
      </td>
     </tr>
+    </span>
+    <span py:if="not provision_now_rights">
+    <tr> 
+     <td><label class="fieldlabel" py:content="schedule_reserve_days.label"/>
+     </td>
+     <td>
+     ${schedule_reserve_days.display()}
+     </td>
+    </tr>
+    <tr>
+     <td>&nbsp;</td>
+     <td>
+      <a class="button" href="javascript:document.${name}.submit();">Schedule provision</a>
+     </td>
+    </tr>
+    </span>
    </table>
 
    ${display_field_for("id")}
   </span>
-  <span py:if="not is_user">
+ </span>
+  <span py:if="not will_provision and provision_now_rights">
    You can only provision if you have reserved the machine.
   </span>
- </span>
+  <span py:if="not will_provision and not provision_now_rights">
+   You do not have access required to provision or scheduler a provision job on this machine.
+  </span>
+
  <span py:if="not lab_controller">
   This system is not associated to a lab controller
  </span>

Server/bkr/server/widgets.py
@@ -992,13 +992,19 @@ class SystemProvision(Form):
     javascript = [LocalJSLink('bkr', '/static/javascript/provision.js')]
     template = "bkr.server.templates.system_provision"
     member_widgets = ["id", "prov_install", "ks_meta", "power",
-                      "koptions", "koptions_post", "reboot"]
-    params = ['options', 'is_user', 'lab_controller', 'power_enabled']
-    
+                      "koptions", "koptions_post", "reboot","schedule_reserve_days"]
+    params = ['options', 'is_user', 'lab_controller', 'power_enabled','provision_now_rights','will_provision']
+    MAX_DAYS_PROVISION = 7
+    DEFAULT_RESERVE_DAYS = 0.5
+
     def __init__(self, *args, **kw):
         super(SystemProvision, self).__init__(*args, **kw)
 	self.id           = HiddenField(name="id")
 	self.power        = HiddenField(name="power")
+        self.schedule_reserve_days = SingleSelectField(name='reserve_days',
+                                                       label=_('Days to reserve for'),
+                                                       options = range(1, self.MAX_DAYS_PROVISION + 1),
+                                                       validator=validators.NotEmpty())
         self.prov_install = SingleSelectField(name='prov_install',
                                              label=_(u'Distro'),
                                              options=[],
@@ -1017,6 +1023,10 @@ class SystemProvision(Form):
 
     def update_params(self, d):
         super(SystemProvision, self).update_params(d)
+        if 'will_provision' in d['options']:
+            d['will_provision'] = d['options']['will_provision']
+        if 'provision_now_rights' in d['options']:
+            d['provision_now_rights'] = d['options']['provision_now_rights']
         if 'is_user' in d['options']:
             d['is_user'] = d['options']['is_user']
         if 'lab_controller' in d['options']:

commit 7f90336e0e15e0ad71601c7f7ec4dfd82dfc8358

bz631421 - fix page title for systems
Looks like a small regression from c77f9196

Server/bkr/server/controllers.py
@@ -823,6 +823,7 @@ class Root(RPCRoot):
         options = {}
         readonly = False
         is_user = False
+        title = 'New'
         if system:
             title = system.fqdn
             our_user = identity.current.user #simple optimisation
@@ -860,11 +861,9 @@ class Root(RPCRoot):
                 self.will_provision = False
                 self.provision_now_rights = False
 
-        if system.current_user(our_user):
-            options['user_change_text'] = ' (Return)'
-            is_user = True
-        else:
-            title = 'New'
+            if system.current_user(our_user):
+                options['user_change_text'] = ' (Return)'
+                is_user = True
 
         if 'activities_found' in histories_return: 
             historical_data = histories_return['activities_found']