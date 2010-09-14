7d31845840fcfb996e3d9897cecae10b72aea0fa
Dan Callaghan <dcallagh@redhat.com>
2010-09-14 17:30:12 +1000

    selenium test for bz631421

---
+++ Server/bkr/server/test/selenium/test_systems.py
@@ -0,0 +1,52 @@
+# Beaker
+#
+# Copyright (C) 2010 dcallagh@redhat.com
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import unittest
+import logging
+from turbogears.database import session
+
+from bkr.server.test.selenium import SeleniumTestCase
+from bkr.server.test import data_setup
+
+class TestSystemView(SeleniumTestCase):
+
+    slow = True
+
+    def setUp(self):
+        self.system = data_setup.create_system()
+        session.flush()
+        self.selenium = self.get_selenium()
+        self.selenium.start()
+
+    def tearDown(self):
+        self.selenium.stop()
+
+    def go_to_system_view(self):
+        sel = self.selenium
+        sel.open('')
+        sel.type('simplesearch', self.system.fqdn)
+        sel.click('search')
+        sel.wait_for_page_to_load('3000')
+        sel.click('link=%s' % self.system.fqdn)
+        sel.wait_for_page_to_load('3000')
+
+    # https://bugzilla.redhat.com/show_bug.cgi?id=631421
+    def test_page_title_shows_fqdn(self):
+        self.go_to_system_view()
+        self.assertEquals(self.selenium.get_title(), self.system.fqdn)
+
