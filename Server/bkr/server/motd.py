03fb8499749223f25eaeac2ce5358066e814ba52
Raymond Mancy <rmancy@redhat.com>
2011-01-21 16:41:49 +1000

    bz667456 - message of the day
    Change-Id: I24770ab33252358ea4df758673ea8a76b95251ab

---
+++ Server/bkr/server/motd.py
@@ -0,0 +1,14 @@
+import logging
+import turbogears as tg
+from lxml import etree
+import logging
+log = logging.getLogger(__name__)
+
+try:
+    f = open(tg.config.get('beaker.motd', '/etc/beaker/motd.txt'), 'rb')
+    parser = etree.XMLParser(recover=True)
+    tree = etree.parse(f,parser)
+    the_motd = etree.tostring(tree.getroot())
+except Exception, e:
+    log.exception('Unable to read motd')
+    the_motd = None
