baa9cba3e1d8f2a390a70308d15c24713d645fd0
Raymond Mancy <rmancy@redhat.com>
2011-04-27 12:32:36 +1000

    motd change to .xml instead of .txt. Needs an update in the config as well
    Bug:699935

--- Server/bkr/server/motd.py
+++ Server/bkr/server/motd.py
@@ -5,7 +5,7 @@ import logging
 log = logging.getLogger(__name__)
 
 try:
-    f = open(tg.config.get('beaker.motd', '/etc/beaker/motd.txt'), 'rb')
+    f = open(tg.config.get('beaker.motd', '/etc/beaker/motd.xml'), 'rb')
     parser = etree.XMLParser(recover=True)
     tree = etree.parse(f,parser)
     the_motd = etree.tostring(tree.getroot())
