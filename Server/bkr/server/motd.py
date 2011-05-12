f1b78789476bb767360bb424df196894483c311c
Dan Callaghan <dcallagh@redhat.com>
2011-05-12 10:10:08 +1000

    show a less scary message when motd does not exist
    Change-Id: I280eb9b3a3a94c1e624bc5b8e8b7a9c46a2a7c73

--- Server/bkr/server/motd.py
+++ Server/bkr/server/motd.py
@@ -1,14 +1,20 @@
 import logging
+import os.path
 import turbogears as tg
 from lxml import etree
 import logging
 log = logging.getLogger(__name__)
 
 try:
-    f = open(tg.config.get('beaker.motd', '/etc/beaker/motd.xml'), 'rb')
-    parser = etree.XMLParser(recover=True)
-    tree = etree.parse(f,parser)
-    the_motd = etree.tostring(tree.getroot())
+    motd_filename = tg.config.get('beaker.motd', '/etc/beaker/motd.xml')
+    if not os.path.exists(motd_filename):
+        log.info('Motd not found at %s, ignoring', motd_filename)
+        the_motd = None
+    else:
+        f = open(motd_filename, 'rb')
+        parser = etree.XMLParser(recover=True)
+        tree = etree.parse(f,parser)
+        the_motd = etree.tostring(tree.getroot())
 except Exception, e:
     log.exception('Unable to read motd')
     the_motd = None
