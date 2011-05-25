9723be68144e265ab079257928a98627aa14c286
Bill Peck <bpeck@redhat.com>
2011-05-25 14:03:48 -0400

    beaker-proxy init script is half broken
    Bug: 704948

--- LabController/proxy/src/bkr/labcontroller/transfer.py
+++ LabController/proxy/src/bkr/labcontroller/transfer.py
@@ -116,6 +116,7 @@ def main():
     else:
         daemonize(main_loop, 
                   daemon_pid_file=pid_file,
+                  daemon_start_dir="/",
                   conf=conf, 
                   foreground=False)
 
