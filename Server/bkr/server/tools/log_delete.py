3eed28c477176bbe482f956a0542157432f51e4a
Raymond Mancy <rmancy@redhat.com>
2011-05-28 00:01:16 +1000

    Changed log_delete tobe able to handle random exceptions, as well as ensuring that it can delete all the right directories even if they are in unexpected locations
    Bug: 704563
    Change-Id: I6ef4485aa682bab909e5d350b6a6161051093ef1

--- Server/bkr/server/tools/log_delete.py
+++ Server/bkr/server/tools/log_delete.py
@@ -7,7 +7,9 @@ from turbogears.database import session
 from bkr.common.dav import BeakerRequest, DavDeleteErrorHandler, RedirectHandler
 import urllib2 as u2
 from urllib2_kerberos import HTTPKerberosAuthHandler
+import logging
 
+logger = logging.getLogger(__name__)
 
 __description__ = 'Script to delete expired log files'
 
@@ -29,8 +31,6 @@ def main():
     load_config(options.config)
     log_delete(options.verbose, options.dry_run)
 
-
-
 def log_delete(verb=False, dry=False):
     # The only way to override default HTTPRedirectHandler
     # is to pass it into build_opener(). Appending does not work
@@ -41,11 +41,12 @@ def log_delete(verb=False, dry=False):
         print 'Dry run only'
     if verb:
         print 'Getting expired jobs'
+
     for job, logs in Job.expired_logs():
         try:
-            session.begin()
             job.deleted = datetime.datetime.utcnow()
             for log in logs:
+                session.begin()
                 if not dry:
                     if 'http' in log:
                         url = log
@@ -57,18 +58,19 @@ def log_delete(verb=False, dry=False):
                         except OSError, e:
                             if e.errno == errno.ENOENT:
                                 pass
+                    session.commit()
+                    session.close()
+
+                else:
+                    session.close()
+
                 if verb:
                     print log
 
-        except Exception:
+        except Exception, e:
             session.close()
-            raise
-        else:
-            if not dry:
-                session.commit()
-                session.close()
-            else:
-                session.close()
+            logger.error(str(e))
+            continue
 
 if __name__ == '__main__':
     main()
