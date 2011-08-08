0fc9f9b83b9fe94949c7ba13e67fe84b07f5e444
Steven Lawrance <stl@redhat.com>
2011-08-09 08:35:12 +1000

    Redirect to system page after changing owner
    Bug: 729173
    Change-Id: I4c7d01c34cdfeec40302dd477cac2d425912911e

--- Server/bkr/server/controllers.py
+++ Server/bkr/server/controllers.py
@@ -1091,7 +1091,7 @@ class Root(RPCRoot):
         system.owner = user
         system.date_modified = datetime.utcnow()
         flash( _(u"OK") )
-        redirect("/")
+        redirect("/view/%s" % system.fqdn)
 
     @expose()
     @identity.require(identity.not_anonymous())
