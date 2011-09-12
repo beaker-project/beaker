beaker-project/beaker

commit 98ccf4fa10637d0604ed9332334a90b62b609d76

bkr distros-list --limit 10 does not always show 10
Bug:709815
Change-Id: I9c5f8b97bab3e2c94be23d9e8bd65e4ea1af7fe1

Server/bkr/server/distro.py
@@ -315,7 +315,7 @@ class Distros(RPCRoot):
         if treepath:
             distros = distros.filter(lab_controller_distro_map.c.tree_path.like('%s' % treepath))
         # join on lab controllers, we only want distros that are active in at least one lab controller
-        distros = distros.join('lab_controller_assocs')
+        distros = distros.filter(Distro.lab_controller_assocs.any())
         distros = distros.order_by(distro_table.c.date_created.desc())
         if limit:
             distros = distros[:limit]

commit d69c4b86c2a3e6c2c00abece79d2f9c977da2296

fix bkr distros-list --treepath
This was broken by commit 98ccf4fa which removed the join on
Distro.lab_controller_assocs.

Bug: 736989
Change-Id: Ica9620beb03e742b32306429e0d9979a26a61924

Server/bkr/server/distro.py
@@ -317,13 +317,14 @@ class Distros(RPCRoot):
             distros = distros.join('arch')
             distros = distros.filter(arch_table.c.arch=='%s' % arch)
         if treepath:
-            distros = distros.filter(lab_controller_distro_map.c.tree_path.like('%s' % treepath))
+            distros = distros.filter(Distro.lab_controller_assocs.any(
+                    LabControllerDistro.tree_path.like('%s' % treepath)))
         if labcontroller:
             distros = distros.filter(exists([1],
                     from_obj=[lab_controller_distro_map.join(lab_controller_table)])
                     .where(LabControllerDistro.distro_id == Distro.id)
                     .where(LabController.fqdn.like(labcontroller)))
-        # join on lab controllers, we only want distros that are active in at least one lab controller
+        # we only want distros that are active in at least one lab controller
         distros = distros.filter(Distro.lab_controller_assocs.any())
         distros = distros.order_by(distro_table.c.date_created.desc())
         if limit:

IntegrationTests/src/bkr/inttest/client/test_distros_list.py
@@ -38,3 +38,18 @@ class DistrosListTest(unittest.TestCase):
         output = run_client(['bkr', 'distros-list', '--labcontroller', good_lc.fqdn])
         self.assert_(any(distro_in.install_name in line for line in output.splitlines()))
         self.assert_(not any(distro_out.install_name in line for line in output.splitlines()))
+
+    # https://bugzilla.redhat.com/show_bug.cgi?id=736989
+    def test_filtering_by_treepath(self):
+        lc = data_setup.create_labcontroller()
+        distro_in = data_setup.create_distro()
+        distro_out = data_setup.create_distro()
+        session.flush()
+        distro_in.lab_controller_assocs[:] = [LabControllerDistro(lab_controller=lc,
+                tree_path='nfs://example.com/somewhere/')]
+        distro_out.lab_controller_assocs[:] = [LabControllerDistro(lab_controller=lc,
+                tree_path='nfs://example.com/nowhere/')]
+        session.flush()
+        output = run_client(['bkr', 'distros-list', '--treepath', '%somewhere%'])
+        self.assert_(any(distro_in.install_name in line for line in output.splitlines()))
+        self.assert_(not any(distro_out.install_name in line for line in output.splitlines()))