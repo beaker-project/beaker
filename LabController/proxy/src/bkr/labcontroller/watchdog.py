da9be21a97fd6217485cb1f906c0b3d5e0a48ad4
Bill Peck <bpeck@redhat.com>
2011-10-27 10:14:38 -0400

    try except handling in wrong place for beaker-watchdog
    Bug: 749551
    Change-Id: Iad6507033947004b8d498670b99ae957c3cca7aa

--- LabController/proxy/src/bkr/labcontroller/watchdog.py
+++ LabController/proxy/src/bkr/labcontroller/watchdog.py
@@ -49,85 +49,92 @@ def main_loop(conf=None, foreground=False):
         sys.stderr.write("Error initializing Watchdog: %s\n" % ex)
         sys.exit(1)
 
-    try:
-        """
-        As watchdog expire_recipe calls recipes.stop on the server
-        side it needs to be auth'ed, and we don't yet have a good
-        way to auth via MRG. So xmlrpc will still need to be used for
-        that call, and thus we must login.
-        """
-        if conf['QPID_BUS']:
-            watchdog.hub._login()
-            auth_renew = RepeatTimer(conf['RENEW_SESSION_INTERVAL'],
-                    watchdog.hub._login, stop_on_exception=False)
-            auth_renew.daemon = True
-            auth_renew.start()
-            lbb = LabBeakerBus(watchdog=watchdog)
-            active_watchdogs = lbb.rpc.recipes.tasks.watchdogs('active', lbb.lc)
-            watchdog.active_watchdogs(active_watchdogs, purge=False)
-            listen_to = conf.get('QPID_LISTEN_TO', [])
-            lbb.run(listen_to)
-            while True:
-                try:
-                    # To avoid a polling style scenario here we would need the harness to be updating
-                    # us when the monitered files change size
-                    if not watchdog.run():
-                        logger.debug(80 * '-')
-                        watchdog.sleep()
-                except (ShutdownException, KeyboardInterrupt):
-                    # ignore keyboard interrupts and sigterm
-                    auth_renew.stop()
-                    signal.signal(signal.SIGINT, signal.SIG_IGN)
-                    signal.signal(signal.SIGTERM, signal.SIG_IGN)
-                    logger.info('Exiting...')
-                    break
+    """
+    As watchdog expire_recipe calls recipes.stop on the server
+    side it needs to be auth'ed, and we don't yet have a good
+    way to auth via MRG. So xmlrpc will still need to be used for
+    that call, and thus we must login.
+    """
+    if conf['QPID_BUS']:
+        watchdog.hub._login()
+        auth_renew = RepeatTimer(conf['RENEW_SESSION_INTERVAL'],
+                watchdog.hub._login, stop_on_exception=False)
+        auth_renew.daemon = True
+        auth_renew.start()
+        lbb = LabBeakerBus(watchdog=watchdog)
+        active_watchdogs = lbb.rpc.recipes.tasks.watchdogs('active', lbb.lc)
+        watchdog.active_watchdogs(active_watchdogs, purge=False)
+        listen_to = conf.get('QPID_LISTEN_TO', [])
+        lbb.run(listen_to)
+        while True:
+            try:
+                # To avoid a polling style scenario here we would need the harness to be updating
+                # us when the monitered files change size
+                if not watchdog.run():
+                    logger.debug(80 * '-')
+                    watchdog.sleep()
+            except socket.sslerror:
+                traceback = Traceback()
+                logger.error(traceback.get_traceback())
+            except xmlrpclib.ProtocolError:
+                traceback = Traceback()
+                logger.error(traceback.get_traceback())
+            except (ShutdownException, KeyboardInterrupt):
+                # ignore keyboard interrupts and sigterm
+                auth_renew.stop()
+                signal.signal(signal.SIGINT, signal.SIG_IGN)
+                signal.signal(signal.SIGTERM, signal.SIG_IGN)
+                logger.info('Exiting...')
+                break
 
-        else:
-            time_of_last_check = 0
-            while True:
-                try:
-                    now = time.time()
-                    # Poll for watchdogs
-                    if now - time_of_last_check > 60:
-                        time_of_last_check = now
-                        watchdog.hub._login()
-                        try:
-                            active_watchdogs = watchdog.hub.recipes.tasks.watchdogs('active')
-                        except xmlrpclib.Fault:
-                            # catch any xmlrpc errors
-                            traceback = Traceback()
-                            logger.error(traceback.get_traceback())
-                        try:
-                            expired_watchdogs = watchdog.hub.recipes.tasks.watchdogs('expired')
-                        except xmlrpclib.Fault:
-                            # catch any xmlrpc errors
-                            traceback = Traceback()
-                            logger.error(traceback.get_traceback())
+    else:
+        time_of_last_check = 0
+        while True:
+            try:
+                now = time.time()
+                # Poll for watchdogs
+                if now - time_of_last_check > 60:
+                    time_of_last_check = now
+                    watchdog.hub._login()
+                    try:
+                        active_watchdogs = watchdog.hub.recipes.tasks.watchdogs('active')
+                    except xmlrpclib.Fault:
+                        # catch any xmlrpc errors
+                        traceback = Traceback()
+                        logger.error(traceback.get_traceback())
+                        active_watchdogs = []
 
-                        watchdog.expire_watchdogs(expired_watchdogs)
-                        watchdog.active_watchdogs(active_watchdogs)
-                    if not watchdog.run():
-                        logger.debug(80 * '-')
-                        watchdog.sleep()
-                    # FIXME: Check for recipes that match systems under
-                    #        this lab controller, if so take recipe and provision
-                    #        system.
-                    # write to stdout / stderr
-                    sys.stdout.flush()
-                    sys.stderr.flush()
-                except socket.sslerror:
-                    pass # try again later
-                except (ShutdownException, KeyboardInterrupt):
-                    # ignore keyboard interrupts and sigterm
-                    signal.signal(signal.SIGINT, signal.SIG_IGN)
-                    signal.signal(signal.SIGTERM, signal.SIG_IGN)
-                    logger.info('Exiting...')
-                    break
-    except:
-        # this is a little extreme: log the exception and continue
-        traceback = Traceback()
-        logger.error(traceback.get_traceback())
-        watchdog.sleep()
+                    try:
+                        expired_watchdogs = watchdog.hub.recipes.tasks.watchdogs('expired')
+                    except xmlrpclib.Fault:
+                        # catch any xmlrpc errors
+                        expired_watchdogs = []
+                        traceback = Traceback()
+                        logger.error(traceback.get_traceback())
+
+                    watchdog.expire_watchdogs(expired_watchdogs)
+                    watchdog.active_watchdogs(active_watchdogs)
+                if not watchdog.run():
+                    logger.debug(80 * '-')
+                    watchdog.sleep()
+                # FIXME: Check for recipes that match systems under
+                #        this lab controller, if so take recipe and provision
+                #        system.
+                # write to stdout / stderr
+                sys.stdout.flush()
+                sys.stderr.flush()
+            except socket.sslerror:
+                traceback = Traceback()
+                logger.error(traceback.get_traceback())
+            except xmlrpclib.ProtocolError:
+                traceback = Traceback()
+                logger.error(traceback.get_traceback())
+            except (ShutdownException, KeyboardInterrupt):
+                # ignore keyboard interrupts and sigterm
+                signal.signal(signal.SIGINT, signal.SIG_IGN)
+                signal.signal(signal.SIGTERM, signal.SIG_IGN)
+                logger.info('Exiting...')
+                break
 
 def main():
     parser = OptionParser()
