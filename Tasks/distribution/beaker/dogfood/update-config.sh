8d96f8bc7170409ffdaf86a4be04cd5b92db55bd
Dan Callaghan <dcallagh@redhat.com>
2012-01-11 14:29:29 +1000

    /distribution/beaker/dogfood: one more config option
    Change-Id: I09b87bf4a43537588f917a71b8c625260161c34d

--- Tasks/distribution/beaker/dogfood/update-config.sh
+++ Tasks/distribution/beaker/dogfood/update-config.sh
@@ -4,6 +4,7 @@ set -e
 # update Beaker config to match what the tests are expecting
 cp /etc/beaker/server.cfg /etc/beaker/server.cfg-orig
 sed -r '
+    s@^#?tg.url_domain.*$@tg.url_domain = "localhost"@
     s@^#?mail.on.*$@mail.on = True@
     s@^#?mail.smtp.server.*$@mail.smtp.server = "127.0.0.1:19999"@
     s@^#?beaker.reliable_distro_tag.*$@beaker.reliable_distro_tag = "RELEASED"@
