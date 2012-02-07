d1d36d40025b0ddce882057c8e2240434a870d6b
Dan Callaghan <dcallagh@redhat.com>
2012-02-07 19:44:27 +1000

    /distribution/beaker/custom_kickstart: some fixes

--- Tasks/distribution/beaker/custom_kickstart/runtest.sh
+++ Tasks/distribution/beaker/custom_kickstart/runtest.sh
@@ -13,85 +13,15 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-set -ex
-
 # XXX shouldn't hardcode this (comes from /distribution/beaker/add_systems)
-SYSTEM="virt-10"
-
-function test_custom_kickstart() {
-    bkr job-submit - <<EOF
-<job retention_tag="scratch">
-    <whiteboard>
-        custom kickstart
-    </whiteboard>
-    <recipeSet>
-        <recipe>
-            <distroRequires>
-                <and>
-                    <distro_arch op="=" value="x86_64"/>
-                    <distro_family op="=" value="RedHatEnterpriseLinux6"/>
-                    <distro_variant op="=" value="Server"/>
-                </and>
-            </distroRequires>
-            <hostRequires>
-                <hostname op="=" value="$SYSTEM" />
-            </hostRequires>
-            <task name="/distribution/install" role="STANDALONE">
-                <params/>
-            </task>
-            <kickstart>
-<![CDATA[
-install
-firewall --disabled
-rootpw --iscrypted $1$lol$HhoQLlgGBmDdhd0l7YaK2.
-text
-keyboard us
-lang en_AU
-selinux --permissive
-skipx
-logging --level=info
-timezone --utc Australia/Brisbane
-bootloader --location=mbr
-zerombr
-clearpart --all --initlabel
-autopart
-reboot
-
-%packages --nobase
-@core
-
-%post --log=/dev/console
-echo hello
-%end
-]]>
-            </kickstart>
-        </recipe>
-    </recipeSet>
-</job>
-EOF
-
-    # XXX find a better way to wait for the job to start
-    sleep 90
-
-    wget -nv -O kickstart.actual "http://$SERVERS/cblr/svc/op/ks/system/$SYSTEM"
-    rhts-submit-log -l kickstart.actual
-    # We could diff this against an expected kickstart, like this...
-    #diff -u kickstart.expected kickstart.actual
-    # We would get an exit status of 1 if there were some difference.
-    # But the kickstart has lots of site-specific stuff in it (distro paths etc)
-    # so that's not really practical. :-(
-    # We can at least check for our magical %post command:
-    grep -q '^echo hello$' kickstart.actual
-
-    echo PASSED
-}
+export TEST_SYSTEM="virt-10"
 
 if $(echo $SERVERS $STANDALONE | grep -q $HOSTNAME) ; then
     if [ -z "$SERVERS" ]; then
         SERVERS="$STANDALONE"
     fi
     # XXX should the setup task do this for us?
-    yum install -y "beaker-client$VERSION"
+    rhts-run-simple-test $TEST/install-beaker-client "yum install -y beaker-client$VERSION"
     mkdir ~/.beaker_client
     cat >~/.beaker_client/config <<EOF
 HUB_URL = "http://$SERVERS/bkr"
@@ -99,7 +29,8 @@ AUTH_METHOD = "password"
 USERNAME = "admin"
 PASSWORD = "testing"
 EOF
-    test_custom_kickstart
+    rhts-run-simple-test $TEST/custom-kickstart ./test_custom_kickstart.sh
+    rhts-run-simple-test $TEST/no-custom-kickstart ./test_no_custom_kickstart.sh
 else
     rhts-run-simple-test $TEST/noop true
 fi
