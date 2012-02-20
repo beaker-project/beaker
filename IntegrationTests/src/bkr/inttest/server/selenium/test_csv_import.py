4258a8e81f944fd7a5fa86597e2dc551b8310805
Dan Callaghan <dcallagh@redhat.com>
2012-02-20 16:15:38 +1000

    tests: convert test_csv_import to WebDriver
    Selenium was failing with a "Security error" when typing in the file
    upload box with Firefox 10. Probably related to
    <http://code.google.com/p/selenium/issues/detail?id=2826>. The easiest
    fix was to convert the test to WebDriver.
    
    Change-Id: I204a55b673e23fe8c714707e80c8996ea4f9ae70

--- IntegrationTests/src/bkr/inttest/server/selenium/test_csv_import.py
+++ IntegrationTests/src/bkr/inttest/server/selenium/test_csv_import.py
@@ -1,45 +1,41 @@
 #!/usr/bin/python
 # vim: set fileencoding=utf-8 :
 
-from bkr.inttest.server.selenium import SeleniumTestCase
-from bkr.inttest import data_setup
+from bkr.inttest.server.selenium import WebDriverTestCase
+from bkr.inttest.server.webdriver_utils import login, is_text_present
+from bkr.inttest import data_setup, get_server_base
 from bkr.inttest.assertions import assert_has_key_with_value
 from bkr.server.model import Arch
 from turbogears.database import session
 import unittest
 from tempfile import NamedTemporaryFile
 
-class CSVImportTest(SeleniumTestCase):
+class CSVImportTest(WebDriverTestCase):
 
     def setUp(self):
         self.system = data_setup.create_system()
         session.flush()
-        self.selenium = self.get_selenium()
-        self.selenium.start()
+        self.browser = self.get_browser()
 
     def tearDown(self):
-        self.selenium.stop()
+        self.browser.quit()
 
     def import_csv(self, contents):
-        sel = self.selenium
-        sel.open("")
-        self.login()
-        sel.click("link=Import")
-        sel.wait_for_page_to_load('30000')
+        b = self.browser
+        login(b)
+        b.get(get_server_base() + 'csv/csv_import')
         csv_file = NamedTemporaryFile(prefix=self.__module__)
         csv_file.write(contents)
         csv_file.flush()
-        sel.type("import_csv_file", csv_file.name)
-        sel.click("//input[@value='Import CSV']")
-        sel.wait_for_page_to_load('30000')
+        b.find_element_by_name('csv_file').send_keys(csv_file.name)
+        b.find_element_by_name('csv_file').submit()
 
     def test_system(self):
         orig_date_modified = self.system.date_modified
         self.import_csv((u'csv_type,fqdn,location,arch\n'
                 u'system,%s,Under my desk,ia64' % self.system.fqdn)
                 .encode('utf8'))
-        sel = self.selenium
-        self.failUnless(sel.is_text_present("No Errors"))
+        self.failUnless(is_text_present(self.browser, "No Errors"))
         session.refresh(self.system)
         self.assertEquals(self.system.location, u'Under my desk')
         self.assert_(Arch.by_name(u'ia64') in self.system.arch)
@@ -50,8 +46,7 @@ class CSVImportTest(SeleniumTestCase):
         self.import_csv((u'csv_type,fqdn,key,key_value,deleted\n'
                 u'keyvalue,%s,COMMENT,UTF 8 –,False' % self.system.fqdn)
                 .encode('utf8'))
-        sel = self.selenium
-        self.failUnless(sel.is_text_present("No Errors"))
+        self.failUnless(is_text_present(self.browser, "No Errors"))
         session.refresh(self.system)
         assert_has_key_with_value(self.system, 'COMMENT', u'UTF 8 –')
         self.assert_(self.system.date_modified > orig_date_modified)
