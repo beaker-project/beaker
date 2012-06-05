93c65064ef17edbe629d206d5cd540fb8bea2b8f
Dan Callaghan <dcallagh@redhat.com>
2012-06-05 18:01:41 +1000

    strict CSV parsing for imports
    This disables CSV dialect sniffing, which causes more problems than it
    solves. Also added some checks to ensure every row has the expected
    number of fields -- if it doesn't, it's probably in the wrong format and
    so we shouldn't import it.
    
    Bug: 802842
    Change-Id: I5c35b622ab7cc41a56921abe04e46c2f4fd60630

--- IntegrationTests/src/bkr/inttest/server/selenium/test_csv_import.py
+++ IntegrationTests/src/bkr/inttest/server/selenium/test_csv_import.py
@@ -5,8 +5,9 @@ from bkr.inttest.server.selenium import WebDriverTestCase
 from bkr.inttest.server.webdriver_utils import login, is_text_present
 from bkr.inttest import data_setup, get_server_base, with_transaction
 from bkr.inttest.assertions import assert_has_key_with_value
-from bkr.server.model import Arch
+from bkr.server.model import Arch, System, OSMajor
 from turbogears.database import session
+import pkg_resources
 import unittest
 from tempfile import NamedTemporaryFile
 
@@ -52,6 +53,54 @@ class CSVImportTest(WebDriverTestCase):
             session.refresh(self.system)
             assert_has_key_with_value(self.system, 'COMMENT', u'UTF 8 –')
             self.assert_(self.system.date_modified > orig_date_modified)
+
+    # https://bugzilla.redhat.com/show_bug.cgi?id=787519
+    def test_no_quotes(self):
+        with session.begin():
+            data_setup.create_labcontroller(fqdn=u'imhoff.bkr')
+        b = self.browser
+        login(b)
+        b.get(get_server_base() + 'csv/csv_import')
+        b.find_element_by_name('csv_file').send_keys(
+                pkg_resources.resource_filename(self.__module__, 'bz787519.csv'))
+        b.find_element_by_name('csv_file').submit()
+        self.failUnless(is_text_present(self.browser, "No Errors"))
+
+    # https://bugzilla.redhat.com/show_bug.cgi?id=802842
+    def test_doubled_quotes(self):
+        with session.begin():
+            system = data_setup.create_system(fqdn=u'mymainframe.funtimes.invalid', arch=u's390x')
+            OSMajor.lazy_create(osmajor=u'RedHatEnterpriseLinux7')
+        b = self.browser
+        login(b)
+        b.get(get_server_base() + 'csv/csv_import')
+        b.find_element_by_name('csv_file').send_keys(
+                pkg_resources.resource_filename(self.__module__, 'bz802842.csv'))
+        b.find_element_by_name('csv_file').submit()
+        self.failUnless(is_text_present(self.browser, "No Errors"))
+        with session.begin():
+            session.refresh(system)
+            self.assertEquals(system.provisions[Arch.by_name(u's390x')]\
+                    .provision_families[OSMajor.by_name(u'RedHatEnterpriseLinux7')]\
+                    .kernel_options,
+                    'rd.znet="qeth,0.0.8000,0.0.8001,0.0.8002,layer2=1,portname=lol,portno=0" '
+                    'ip=1.2.3.4::1.2.3.4:255.255.248.0::eth0:none MTU=1500 nameserver=1.2.3.4 '
+                    'DASD=20A1,21A1,22A1,23A1 MACADDR=02:DE:AD:BE:EF:16 '
+                    '!LAYER2 !DNS !PORTNO !IPADDR !GATEWAY !HOSTNAME !NETMASK ')
+
+    def test_missing_field(self):
+        orig_date_modified = self.system.date_modified
+        self.import_csv((u'csv_type,fqdn,location,arch\n'
+                u'system,%s,Under my desk' % self.system.fqdn)
+                .encode('utf8'))
+        self.assert_(is_text_present(self.browser, 'Missing fields on line 2: arch'))
+
+    def test_extraneous_field(self):
+        orig_date_modified = self.system.date_modified
+        self.import_csv((u'csv_type,fqdn,location,arch\n'
+                u'system,%s,Under my desk,ppc64,what is this field doing here' % self.system.fqdn)
+                .encode('utf8'))
+        self.assert_(is_text_present(self.browser, 'Too many fields on line 2 (expecting 4)'))
 
 if __name__ == "__main__":
     unittest.main()
