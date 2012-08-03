8c4e44cbaf6a4a2b27b20365196f67a072cacf20
Dan Callaghan <dcallagh@redhat.com>
2012-08-03 15:05:45 +1000

    console.log for host should be updated until whole recipeset completes
    Bug: 817518
    Change-Id: Ie6822d60f163a8a2c1b1cabd9b205f8bdd2dd5c0

---
+++ IntegrationTests/src/bkr/inttest/server/selenium/test_recipes_xmlrpc.py
@@ -0,0 +1,27 @@
+
+from turbogears.database import session
+from bkr.inttest import data_setup
+from bkr.inttest.server.selenium import XmlRpcTestCase
+
+class RecipesXmlRpcTest(XmlRpcTestCase):
+
+    def setUp(self):
+        with session.begin():
+            self.lc = data_setup.create_labcontroller()
+            self.lc.user.password = u'logmein'
+        self.server = self.get_server()
+
+    # https://bugzilla.redhat.com/show_bug.cgi?id=817518
+    def test_by_log_server_only_returns_completed_recipesets(self):
+        with session.begin():
+            dt = data_setup.create_distro_tree()
+            completed_recipe = data_setup.create_recipe(distro_tree=dt,
+                    system=data_setup.create_system(lab_controller=self.lc))
+            incomplete_recipe = data_setup.create_recipe(distro_tree=dt,
+                    system=data_setup.create_system(lab_controller=self.lc))
+            job = data_setup.create_job_for_recipes(
+                    [completed_recipe, incomplete_recipe])
+            data_setup.mark_recipe_complete(completed_recipe)
+        self.server.auth.login_password(self.lc.user.user_name, u'logmein')
+        result = self.server.recipes.by_log_server(self.lc.fqdn)
+        self.assertEquals(result, [])
