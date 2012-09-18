89bc38a5e2301562b1860ba3e4f93db78ae2b327
Dan Callaghan <dcallagh@redhat.com>
2012-09-18 15:36:07 +1000

    tests: port test_system_loan to WebDriver
    This also happens to fix a failure with Selenium in the
    test_loan_username_autocomplete_works method, the cause of which
    I couldn't figure out.
    
    Change-Id: I76b889dcacff9844a0aa4130ade85b5c76fcfacd

--- IntegrationTests/src/bkr/inttest/server/selenium/test_system_loan.py
+++ IntegrationTests/src/bkr/inttest/server/selenium/test_system_loan.py
@@ -1,60 +1,57 @@
 
-from bkr.inttest.server.selenium import SeleniumTestCase
-from bkr.inttest import data_setup, with_transaction
+from bkr.inttest.server.selenium import WebDriverTestCase
+from bkr.inttest.server.webdriver_utils import login
+from bkr.inttest import data_setup, with_transaction, get_server_base
 import unittest, time, re, os
 from turbogears.database import session
 
-class SystemLoanTest(SeleniumTestCase):
+class SystemLoanTest(WebDriverTestCase):
 
     @with_transaction
     def setUp(self):
-        self.selenium = self.get_selenium()
-        self.selenium.start()
+        self.browser = self.get_browser()
         self.system = data_setup.create_system()
 
     def tearDown(self):
-        self.selenium.stop()
+        self.browser.quit()
 
     def go_to_loan_page(self):
-        sel = self.selenium
-        sel.open('view/%s' % self.system.fqdn)
-        sel.wait_for_page_to_load("30000")
-        sel.click('//table[@class="list"]//a[.//text()=" (Loan)"]')
-        sel.wait_for_page_to_load("30000")
+        self.browser.get(get_server_base() + 'view/%s' % self.system.fqdn)
+        self.browser.find_element_by_link_text('(Loan)').click()
 
     def go_to_change_loan_page(self):
-        sel = self.selenium
-        sel.open('view/%s' % self.system.fqdn)
-        sel.wait_for_page_to_load("30000")
-        sel.click('//table[@class="list"]//a[.//text()=" (Change)"][preceding-sibling::a[text()=" (Return)"]]')
-        sel.wait_for_page_to_load("30000")
+        self.browser.get(get_server_base() + 'view/%s' % self.system.fqdn)
+        self.browser.find_element_by_xpath('//a[.//text()=" (Change)"][preceding-sibling::a[text()=" (Return)"]]').click()
+
+    def change_loan(self, loanee):
+        self.browser.find_element_by_id("Loan_user").send_keys(loanee.user_name)
+        self.browser.find_element_by_xpath(
+                '//*[@id="autoCompleteResultsLoan_user"]'
+                '//td[.//text()="%s"]' % loanee.user_name).click()
+        self.browser.find_element_by_xpath("//input[@value='Change']").click()
 
     def test_can_loan_when_system_has_user(self):
         with session.begin():
             user = data_setup.create_user()
             self.system.user = user
-        self.login()
+        b = self.browser
+        login(b)
         self.go_to_loan_page()
-        sel = self.selenium
-        sel.type("Loan_user", user.user_name)
-        sel.click("//input[@value='Change']")
-        sel.wait_for_page_to_load("30000")
-        self.failUnless(sel.is_text_present("%s Loaned to %s" %
-                (self.system.fqdn, user.user_name)))
+        self.change_loan(user)
+        self.assertEqual(b.find_element_by_css_selector('.flash').text,
+                "%s Loaned to %s" % (self.system.fqdn, user.user_name))
 
     def test_owner_can_loan_to_themself(self):
         p_word='password'
         with session.begin():
             user = data_setup.create_user(password=p_word)
             self.system.owner = user
-        self.login(user=user.user_name, password=p_word)
+        b = self.browser
+        login(b, user=user.user_name, password=p_word)
         self.go_to_loan_page()
-        sel = self.selenium
-        sel.type("Loan_user", user.user_name)
-        sel.click("//input[@value='Change']")
-        sel.wait_for_page_to_load("30000")
-        self.failUnless(sel.is_text_present("%s Loaned to %s" %
-                (self.system.fqdn, user.user_name)))
+        self.change_loan(user)
+        self.assertEqual(b.find_element_by_css_selector('.flash').text,
+                "%s Loaned to %s" % (self.system.fqdn, user.user_name))
 
     def test_can_not_change_loan_when_system_has_loanee_and_not_admin(self):
         p_word = 'password'
@@ -62,13 +59,11 @@ class SystemLoanTest(SeleniumTestCase):
             user = data_setup.create_user(password=p_word)
             self.system.user = user
             self.system.loaned = user
-        self.login(user=user.user_name, password=p_word)
-        try:
-            self.go_to_change_loan_page()
-            self.fail("Should not get here")
-        except Exception, e:
-            if 'not found' not in str(e):
-                raise
+        b = self.browser
+        login(b, user=user.user_name, password=p_word)
+        b.get(get_server_base() + 'view/%s' % self.system.fqdn)
+        td = b.find_element_by_xpath('//td[normalize-space(preceding-sibling::th[1]/label/text())="Loaned To"]')
+        self.assert_('(Change)' not in td.text, td.text)
 
     def test_can_change_loan_when_system_has_loanee(self):
         with session.begin():
@@ -76,14 +71,12 @@ class SystemLoanTest(SeleniumTestCase):
             user2 = data_setup.create_user()
             self.system.user = user
             self.system.loaned = user
-        self.login()
+        b = self.browser
+        login(b)
         self.go_to_change_loan_page()
-        sel = self.selenium
-        sel.type("Loan_user", user2.user_name)
-        sel.click("//input[@value='Change']")
-        sel.wait_for_page_to_load("30000")
-        self.failUnless(sel.is_text_present("%s Loaned to %s" %
-                (self.system.fqdn, user2.user_name)))
+        self.change_loan(user2)
+        self.assertEqual(b.find_element_by_css_selector('.flash').text,
+                "%s Loaned to %s" % (self.system.fqdn, user2.user_name))
         with session.begin():
             session.refresh(self.system)
             reserved_activity = self.system.activity[-1]
@@ -96,12 +89,12 @@ class SystemLoanTest(SeleniumTestCase):
     def test_loan_username_autocomplete_works(self):
         with session.begin():
             user = data_setup.create_user(user_name=u'distinctiveusername')
-        self.login()
+        b = self.browser
+        login(b)
         self.go_to_loan_page()
-        sel = self.selenium
-        sel.focus('Loan_user')
-        sel.type('Loan_user', '')
-        sel.type_keys('Loan_user', 'distinc')
-        self.wait_for_condition(lambda: sel.is_visible('autoCompleteResultsLoan_user'))
-        self.assertEquals(sel.get_text('autoCompleteResultsLoan_user'),
-                'distinctiveusername')
+        username_input = b.find_element_by_id('Loan_user')
+        username_input.click()
+        username_input.clear()
+        username_input.send_keys('distinc')
+        b.find_element_by_xpath('//*[@id="autoCompleteResultsLoan_user"]//td'
+                '[.//text()="distinctiveusername"]')
