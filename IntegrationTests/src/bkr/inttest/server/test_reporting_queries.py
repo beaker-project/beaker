4589ee0131aeb3c12d9162235c232d2c5a4d88f5
Dan Callaghan <dcallagh@redhat.com>
2012-12-17 18:45:17 +1000

    query for reporting task durations
    Bug: 877272
    Change-Id: I30361a9534bc84f46a8ab85a79af222eef6dd73e

--- IntegrationTests/src/bkr/inttest/server/test_reporting_queries.py
+++ IntegrationTests/src/bkr/inttest/server/test_reporting_queries.py
@@ -4,7 +4,7 @@ import pkg_resources
 import datetime
 from decimal import Decimal
 from turbogears.database import session
-from bkr.server.model import System
+from bkr.server.model import System, RecipeTask
 from bkr.inttest import data_setup
 
 class ReportingQueryTest(unittest.TestCase):
@@ -40,3 +40,24 @@ class ReportingQueryTest(unittest.TestCase):
         self.assertEquals(user_rows[1].year_month, 201212)
         self.assertEquals(user_rows[1].arch, 'ppc64')
         self.assertEquals(user_rows[1].machine_hours, Decimal('2.0'))
+
+    # https://bugzilla.redhat.com/show_bug.cgi?id=877272
+    def test_task_durations(self):
+        short_task = data_setup.create_task()
+        long_task = data_setup.create_task()
+        r = data_setup.create_recipe()
+        r.tasks[:] = [RecipeTask(task=short_task), RecipeTask(task=long_task)]
+        data_setup.mark_job_complete(
+                data_setup.create_job_for_recipes([r]))
+        r.tasks[0].start_time = datetime.datetime(2012, 10, 15, 10, 54, 0)
+        r.tasks[0].finish_time = datetime.datetime(2012, 10, 15, 11, 0, 0)
+        r.tasks[1].start_time = datetime.datetime(2012, 10, 15, 11, 0, 0)
+        r.tasks[1].finish_time = datetime.datetime(2012, 10, 15, 21, 0, 0)
+        session.flush()
+        rows = list(self.execute_reporting_query('task-durations-by-arch'))
+        short_task_row, = [row for row in rows if row.task == short_task.name]
+        self.assertEquals(short_task_row.executions, 1)
+        self.assertEquals(short_task_row.avg_duration, Decimal('0.1'))
+        long_task_row, = [row for row in rows if row.task == long_task.name]
+        self.assertEquals(long_task_row.executions, 1)
+        self.assertEquals(long_task_row.avg_duration, Decimal('10.0'))
