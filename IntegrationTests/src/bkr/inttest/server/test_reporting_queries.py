c157e9de22c4f4903647ed8d1ef785386633e631
Dan Callaghan <dcallagh@redhat.com>
2013-01-09 12:45:22 +1000

    split machine-hours query into two: one by recipe, one by reservation
    The original query was really "recipe-hours", since it was from the
    point of view of recipes, which means it includes time spent running on
    RHEV but doesn't cover manual reservations. Added a second query for
    "machine-hours", from the point of view of system reservations, which
    means it includes manual reservations but not time spent running on
    RHEV.
    
    It doesn't really make sense to try and combine them, especially given
    the difficulties with the arch breakdown on the reservation side (due to
    systems supporting multiple arches).
    
    Bug: 877264
    Change-Id: Iaaa580899e05fa976e1bd448e6242965e8ac0f53

--- IntegrationTests/src/bkr/inttest/server/test_reporting_queries.py
+++ IntegrationTests/src/bkr/inttest/server/test_reporting_queries.py
@@ -124,26 +124,56 @@ class ReportingQueryTest(unittest.TestCase):
         self.assertEquals(system_rows[0].failed_recipes, 1)
 
     # https://bugzilla.redhat.com/show_bug.cgi?id=877264
-    def test_machine_hours(self):
+    def test_recipe_hours(self):
         user = data_setup.create_user()
+        # recipes/reservations straddle the boundary of the reporting period
+        # to test we clamp them properly
         data_setup.create_completed_job(owner=user,
                 distro_tree=data_setup.create_distro_tree(arch=u'ia64'),
-                start_time=datetime.datetime(2012, 12, 16, 0, 0, 0),
-                finish_time=datetime.datetime(2012, 12, 16, 1, 30, 0))
+                start_time=datetime.datetime(2012, 9, 30, 12, 0, 0),
+                finish_time=datetime.datetime(2012, 10, 1, 1, 30, 0))
         data_setup.create_completed_job(owner=user,
                 distro_tree=data_setup.create_distro_tree(arch=u'ppc64'),
-                start_time=datetime.datetime(2012, 12, 16, 0, 0, 0),
-                finish_time=datetime.datetime(2012, 12, 16, 2, 0, 0))
+                start_time=datetime.datetime(2012, 10, 31, 22, 0, 0),
+                finish_time=datetime.datetime(2012, 11, 1, 10, 0, 0))
         session.flush()
-        rows = self.execute_reporting_query('machine-hours-by-arch-user-month')
+        rows = self.execute_reporting_query('recipe-hours-by-user-arch')
         user_rows = [row for row in rows if row.username == user.user_name]
         self.assertEquals(len(user_rows), 2, user_rows)
-        self.assertEquals(user_rows[0].year_month, 201212)
         self.assertEquals(user_rows[0].arch, 'ia64')
-        self.assertEquals(user_rows[0].machine_hours, Decimal('1.5'))
-        self.assertEquals(user_rows[1].year_month, 201212)
+        self.assertEquals(user_rows[0].recipe_hours, Decimal('1.5'))
         self.assertEquals(user_rows[1].arch, 'ppc64')
-        self.assertEquals(user_rows[1].machine_hours, Decimal('2.0'))
+        self.assertEquals(user_rows[1].recipe_hours, Decimal('2.0'))
+
+    # https://bugzilla.redhat.com/show_bug.cgi?id=877264
+    def test_machine_hours(self):
+        user = data_setup.create_user()
+        # recipes/reservations straddle the boundary of the reporting period
+        # to test we clamp them properly
+        data_setup.create_completed_job(owner=user,
+                distro_tree=data_setup.create_distro_tree(arch=u'ia64'),
+                start_time=datetime.datetime(2012, 9, 30, 23, 0, 0),
+                finish_time=datetime.datetime(2012, 10, 1, 1, 0, 0))
+        data_setup.create_manual_reservation(user=user,
+                system=data_setup.create_system(arch=u'ia64'),
+                start=datetime.datetime(2012, 10, 31, 22, 30, 0),
+                finish=datetime.datetime(2012, 11, 1, 1, 0, 0))
+        data_setup.create_completed_job(owner=user,
+                distro_tree=data_setup.create_distro_tree(arch=u'ppc64'),
+                start_time=datetime.datetime(2012, 9, 30, 20, 0, 0),
+                finish_time=datetime.datetime(2012, 10, 1, 2, 0, 0))
+        data_setup.create_manual_reservation(user=user,
+                system=data_setup.create_system(arch=u'ppc64'),
+                start=datetime.datetime(2012, 10, 31, 23, 0, 0),
+                finish=datetime.datetime(2012, 11, 1, 10, 0, 0))
+        session.flush()
+        rows = self.execute_reporting_query('machine-hours-by-user-arch')
+        user_rows = [row for row in rows if row.username == user.user_name]
+        self.assertEquals(len(user_rows), 2, user_rows)
+        self.assertEquals(user_rows[0].arch, 'ia64')
+        self.assertEquals(user_rows[0].machine_hours, Decimal('2.5'))
+        self.assertEquals(user_rows[1].arch, 'ppc64')
+        self.assertEquals(user_rows[1].machine_hours, Decimal('3.0'))
 
     # https://bugzilla.redhat.com/show_bug.cgi?id=877272
     def test_task_durations(self):
