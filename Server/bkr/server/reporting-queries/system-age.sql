22bd88f171be33ee19548b094413eb12eaf016e7
Dan Callaghan <dcallagh@redhat.com>
2013-01-10 15:27:47 +1000

    fix queries to be more SQL92-compliant
    Specifically we need to avoid grouping by expressions, or selecting
    non-grouped non-aggregated columns. This way the queries should be
    executable in Teiid.
    
    Bug: 193142
    Change-Id: I0858f4bc9b6e7a4992528cecc17685b92fe4bb18

--- Server/bkr/server/reporting-queries/system-age.sql
+++ Server/bkr/server/reporting-queries/system-age.sql
@@ -4,10 +4,12 @@
 
 SELECT
     system.fqdn AS fqdn,
-    TIMESTAMPDIFF(DAY, system.date_added, UTC_TIMESTAMP()) AS age_days,
-    COUNT(system_resource.id) AS recipe_count
+    (SELECT TIMESTAMPDIFF(DAY, system.date_added, UTC_TIMESTAMP())
+    FROM system s1
+    WHERE s1.id = system.id) AS age_days,
+    (SELECT COUNT(system_resource.id)
+    FROM system_resource
+    WHERE system_resource.system_id = system.id) AS recipe_count
 FROM system
-LEFT OUTER JOIN system_resource ON system_resource.system_id = system.id
 WHERE system.status != 'Removed'
-GROUP BY system.id
 ORDER BY age_days DESC;
