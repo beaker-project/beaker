aa2389594dbd97dd2528b4e5358027ba2d0591b1
Dan Callaghan <dcallagh@redhat.com>
2013-03-25 13:01:50 +1000

    /distribution/beaker/dogfood: reduce number of Apache worker processes
    The dogfood task requires a lot of memory and will swap on any box with
    <2GB. Most of that is unavoidable but we can reduce memory pressure
    a bit by cutting down on Apache worker processes.
    
    Change-Id: I3df240f0ae866b029f2841af4137c9ccd631e0f0

--- Tasks/distribution/beaker/dogfood/update-config.sh
+++ Tasks/distribution/beaker/dogfood/update-config.sh
@@ -2,8 +2,7 @@
 set -e
 
 # update Beaker config to match what the tests are expecting
-cp /etc/beaker/server.cfg /etc/beaker/server.cfg-orig
-sed -r '
+sed --regexp-extended --in-place=-orig --copy -e '
     s@^#?tg.url_domain.*$@tg.url_domain = "localhost"@
     s@^#?mail.on.*$@mail.on = True@
     s@^#?mail.smtp.server.*$@mail.smtp.server = "127.0.0.1:19999"@
@@ -11,4 +10,9 @@ sed -r '
     s@^#?beaker.motd.*$@beaker.motd = "/usr/lib/python2.6/site-packages/bkr/inttest/server/motd.xml"@
     s@^#?beaker.max_running_commands .*$@beaker.max_running_commands = 10@
     s@^#?beaker.kernel_options .*$@beaker.kernel_options = "noverifyssl"@
-' </etc/beaker/server.cfg-orig >/etc/beaker/server.cfg
+    ' /etc/beaker/server.cfg
+
+# reduce number of Apache worker processes, to save a bit of memory
+sed --regexp-extended --in-place=-orig --copy -e '
+    /^WSGIDaemonProcess/ s@processes=[0-9]+@processes=2@
+    ' /etc/httpd/conf.d/beaker-server.conf
