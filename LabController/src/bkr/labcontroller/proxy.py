beaker-project/beaker

commit 32dea6ec7cfcf76aa32b2640230809cef0cc171a

HTTP API for uploading logs
Bug: 915128
Change-Id: I7261640920c693019981701a6fa83218cb526779

LabController/src/bkr/labcontroller/proxy.py
@@ -19,24 +19,20 @@ from threading import Thread, Event
 from werkzeug.wrappers import Response
 from werkzeug.exceptions import BadRequest, NotAcceptable
 from werkzeug.utils import redirect
+from werkzeug.http import parse_content_range_header
 import kobo.conf
 from kobo.client import HubProxy
 from kobo.exceptions import ShutdownException
 from kobo.xmlrpc import retry_request_decorator, CookieTransport, \
         SafeCookieTransport
 from bkr.labcontroller.config import get_conf
+from bkr.labcontroller.log_storage import LogStorage
 from kobo.process import kill_process_group
-from bkr.upload import Uploader
 import utils
 try:
     from subprocess import check_output
 except ImportError:
     from utils import check_output
-
-try:
-    from hashlib import md5 as md5_constructor
-except ImportError:
-    from md5 import new as md5_constructor
 
 logger = logging.getLogger(__name__)
 
@@ -67,9 +63,9 @@ class ProxyHelper(object):
             TransportClass = retry_request_decorator(CookieTransport)
         self.hub = HubProxy(logger=logging.getLogger('kobo.client.HubProxy'), conf=self.conf,
                 transport=TransportClass(timeout=120), auto_logout=False, **kwargs)
-        self.log_base_url = "http://%s/beaker/logs" % self.conf.get("SERVER", gethostname())
-        self.basepath = self.conf.get("CACHEPATH", "/var/www/beaker/logs")
-        self.upload = Uploader('%s' % self.basepath).uploadFile
+        self.log_storage = LogStorage(self.conf.get("CACHEPATH"),
+                "http://%s/beaker/logs" % self.conf.get("SERVER", gethostname()),
+                self.hub)
 
     def recipe_upload_file(self, 
                          recipe_id, 
@@ -89,17 +85,13 @@ class ProxyHelper(object):
             Files can be uploaded in chunks, if so the md5 and the size 
             describe the chunk rather than the whole file.  The offset
             indicates where the chunk belongs
-            the special offset -1 is used to indicate the final chunk
         """
+        # Originally offset=-1 had special meaning, but that was unused
         logger.debug("recipe_upload_file recipe_id:%s name:%s offset:%s size:%s",
                 recipe_id, name, offset, size)
-        if int(offset) == 0:
-            self.hub.recipes.register_file(
-                    '%s/recipes/%s/' % (self.log_base_url, recipe_id),
-                    recipe_id, path, name,
-                    '%s/recipes/%s/' % (self.basepath, recipe_id))
-        return self.upload('/recipes/%s/%s' % (recipe_id, path),
-                name, size, md5sum, offset, data)
+        with self.log_storage.recipe(recipe_id, os.path.join(path, name)) as log_file:
+            log_file.update_chunk(base64.decodestring(data), int(offset or 0))
+        return True
 
     def task_result(self, 
                     task_id, 
@@ -264,16 +256,8 @@ class WatchFile(object):
             elif size < self.blocksize and where == now:
                 return False
             else:
-                self.where = now
-                data = base64.encodestring(line)
-                md5sum = md5_constructor(line).hexdigest()
-                self.proxy.recipe_upload_file(self.watchdog['recipe_id'],
-                                             "/",
-                                             self.filename,
-                                             size,
-                                             md5sum,
-                                             where,
-                                             data)
+                with self.proxy.log_storage.recipe(self.watchdog['recipe_id'], self.filename) as log_file:
+                    log_file.update_chunk(line, now)
                 return True
         return False
 
@@ -305,7 +289,7 @@ class Watchdog(ProxyHelper):
     def transfer_recipe_logs(self, recipe_id):
         """ If Cache is turned on then move the recipes logs to their final place
         """
-        tmpdir = tempfile.mkdtemp(dir=self.basepath)
+        tmpdir = tempfile.mkdtemp(dir=self.conf.get("CACHEPATH"))
         try:
             # Move logs to tmp directory layout
             mylogs = self.hub.recipes.files(recipe_id)
@@ -454,9 +438,7 @@ class Monitor(ProxyHelper):
         self.watchdog = watchdog
         self.conf = obj.conf
         self.hub = obj.hub
-        self.upload = obj.upload
-        self.basepath = obj.basepath
-        self.log_base_url = obj.log_base_url
+        self.log_storage = obj.log_storage
         logger.info("Initialize monitor for system: %s", self.watchdog['system'])
         self.console_watch = WatchFile(
                 "%s/%s" % (self.conf["CONSOLE_LOGS"], self.watchdog["system"]),
@@ -486,17 +468,13 @@ class Proxy(ProxyHelper):
             Files can be uploaded in chunks, if so the md5 and the size 
             describe the chunk rather than the whole file.  The offset
             indicates where the chunk belongs
-            the special offset -1 is used to indicate the final chunk
         """
+        # Originally offset=-1 had special meaning, but that was unused
         logger.debug("task_upload_file task_id:%s name:%s offset:%s size:%s",
                 task_id, name, offset, size)
-        if int(offset) == 0:
-            self.hub.recipes.tasks.register_file(
-                    '%s/tasks/%s/' % (self.log_base_url, task_id),
-                    task_id, path, name,
-                    '%s/tasks/%s/' % (self.basepath, task_id))
-        return self.upload('/tasks/%s/%s' % (task_id, path),
-                name, size, md5sum, offset, data)
+        with self.log_storage.task(task_id, os.path.join(path, name)) as log_file:
+            log_file.update_chunk(base64.decodestring(data), int(offset or 0))
+        return True
 
     def task_start(self,
                    task_id,
@@ -578,17 +556,13 @@ class Proxy(ProxyHelper):
             Files can be uploaded in chunks, if so the md5 and the size 
             describe the chunk rather than the whole file.  The offset
             indicates where the chunk belongs
-            the special offset -1 is used to indicate the final chunk
         """
+        # Originally offset=-1 had special meaning, but that was unused
         logger.debug("result_upload_file result_id:%s name:%s offset:%s size:%s",
                 result_id, name, offset, size)
-        if int(offset) == 0:
-            self.hub.recipes.tasks.register_result_file(
-                    '%s/results/%s/' % (self.log_base_url, result_id),
-                    result_id, path, name,
-                    '%s/results/%s/' % (self.basepath, result_id))
-        return self.upload('/results/%s/%s' % (result_id, path),
-                name, size, md5sum, offset, data)
+        with self.log_storage.result(result_id, os.path.join(path, name)) as log_file:
+            log_file.update_chunk(base64.decodestring(data), int(offset or 0))
+        return True
 
     def push(self, fqdn, inventory):
         """ Push inventory data to Scheduler
@@ -676,3 +650,39 @@ class ProxyHTTP(object):
             raise BadRequest('Invalid "seconds" parameter %r' % req.form['seconds'])
         self.hub.recipes.extend(recipe_id, seconds)
         return Response(status=204)
+
+    # XXX should do streaming here, so that clients can send
+    # big files without chunking
+
+    def _put_log(self, log_file, req):
+        if not req.content_length:
+            raise BadRequest('Missing "Content-Length" header')
+        content_range = parse_content_range_header(req.headers.get('Content-Range'))
+        if content_range:
+            # a few sanity checks
+            if req.content_length != (content_range.stop - content_range.start):
+                raise BadRequest('Content length does not match range length')
+            if content_range.length and content_range.length < content_range.stop:
+                raise BadRequest('Total length is smaller than range end')
+        with log_file:
+            if content_range:
+                if content_range.length: # length may be '*' meaning unspecified
+                    log_file.truncate(content_range.length)
+                log_file.update_chunk(req.data, content_range.start)
+            else:
+                # no Content-Range, therefore the request is the whole file
+                log_file.truncate(req.content_length)
+                log_file.update_chunk(req.data, 0)
+        return Response(status=204)
+
+    def put_recipe_log(self, req, recipe_id, path):
+        log_file = self.log_storage.recipe(recipe_id, path)
+        return self._put_log(log_file, req)
+
+    def put_task_log(self, req, recipe_id, task_id, path):
+        log_file = self.log_storage.task(task_id, path)
+        return self._put_log(log_file, req)
+
+    def put_result_log(self, req, recipe_id, task_id, result_id, path):
+        log_file = self.log_storage.result(result_id, path)
+        return self._put_log(log_file, req)

Common/bkr/upload.py
@@ -1,134 +1,0 @@
-import base64
-import os
-import stat
-import errno
-import fcntl
-import logging
-
-try:
-    from hashlib import md5 as md5_constructor
-except ImportError:
-    from md5 import new as md5_constructor
-
-logger = logging.getLogger(__name__)
-
-def decode_int(n):
-    """If n is not an integer, attempt to convert it"""
-    if isinstance(n, (int, long)):
-        return n
-    return int(n)
-
-def ensuredir(directory):
-    """Create directory, if necessary."""
-    if os.path.isdir(directory):
-        return
-    try:
-        os.makedirs(directory)
-    except OSError:
-        #thrown when dir already exists (could happen in a race)
-        if not os.path.isdir(directory):
-            #something else must have gone wrong
-            raise
-
-class Uploader:
-    def __init__(self, basepath):
-        self.basepath = basepath
-
-    def uploadFile(self, path, name, size, md5sum, offset, data):
-        #path: the relative path to upload to
-        #name: the name of the file
-        #size: size of contents (bytes)
-        #md5: md5sum (hex digest) of contents
-        #data: base64 encoded file contents
-        #offset: the offset of the chunk
-        # files can be uploaded in chunks, if so the md5 and size describe
-        # the chunk rather than the whole file. the offset indicates where
-        # the chunk belongs
-        # the special offset -1 is used to indicate the final chunk
-        contents = base64.decodestring(data)
-        del data
-        # we will accept offset and size as strings to work around xmlrpc limits
-        offset = decode_int(offset)
-        size = decode_int(size)
-        if offset != -1:
-            if size is not None:
-                if size != len(contents): return False
-            if md5sum:
-                if md5sum != md5_constructor(contents).hexdigest():
-                    return False
-        uploadpath = self.basepath
-        #XXX - have an incoming dir and move after upload complete
-        # SECURITY - ensure path remains under uploadpath
-        path = os.path.normpath(path)
-        if path.startswith('..'):
-            logger.warn('Upload path not allowed: %s' % path)
-            raise BX(_("Upload path not allowed: %s" % path))
-        udir = "%s/%s" % (uploadpath,path)
-        ensuredir(udir)
-        fn = "%s/%s" % (udir,name)
-        try:
-            st = os.lstat(fn)
-        except OSError, e:
-            # Only offset 0 should be allowed to create a file.
-            if e.errno == errno.ENOENT and offset == 0:
-                pass
-            else:
-                logger.warn('attempted to append to nonexistent file: %s' % fn)
-                raise
-        else:
-            if not stat.S_ISREG(st.st_mode):
-                logger.warn('destination not a file: %s' % fn)
-                raise BX(_("destination not a file: %s" % fn))
-        fd = os.open(fn, os.O_RDWR | os.O_CREAT, 0666)
-        # log_error("fd=%r" %fd)
-        try:
-            if offset == 0 or (offset == -1 and size == len(contents)):
-                #truncate file
-                fcntl.lockf(fd, fcntl.LOCK_EX|fcntl.LOCK_NB)
-                try:
-                    os.ftruncate(fd, 0)
-                    # log_error("truncating fd %r to 0" %fd)
-                finally:
-                    fcntl.lockf(fd, fcntl.LOCK_UN)
-            if offset == -1:
-                os.lseek(fd,0,2)
-            else:
-                os.lseek(fd,offset,0)
-            #write contents
-            fcntl.lockf(fd, fcntl.LOCK_EX|fcntl.LOCK_NB, len(contents), 0, 2)
-            try:
-                os.write(fd, contents)
-                # log_error("wrote contents")
-            finally:
-                fcntl.lockf(fd, fcntl.LOCK_UN, len(contents), 0, 2)
-            if offset == -1:
-                if size is not None:
-                    #truncate file
-                    fcntl.lockf(fd, fcntl.LOCK_EX|fcntl.LOCK_NB)
-                    try:
-                        os.ftruncate(fd, size)
-                        # log_error("truncating fd %r to size %r" % (fd,size))
-                    finally:
-                        fcntl.lockf(fd, fcntl.LOCK_UN)
-                if md5sum:
-                    #check final md5sum
-                    sum = md5_constructor()
-                    fcntl.lockf(fd, fcntl.LOCK_SH|fcntl.LOCK_NB)
-                    try:
-                        # log_error("checking md5sum")
-                        os.lseek(fd,0,0)
-                        while True:
-                            block = os.read(fd, 819200)
-                            if not block: break
-                            sum.update(block)
-                        if md5sum != sum.hexdigest():
-                            # log_error("md5sum did not match")
-                            #os.close(fd)
-                            logger.warn('md5sum did not match for %s' % fn)
-                            return False
-                    finally:
-                        fcntl.lockf(fd, fcntl.LOCK_UN)
-        finally:
-            os.close(fd)
-        return True
-

IntegrationTests/src/bkr/inttest/labcontroller/test_proxy.py
@@ -218,7 +218,7 @@ class PostrebootTest(LabControllerTestCase):
             session.expire_all()
             self.assertEqual(self.system.command_queue[0].action, 'reboot')
 
-class FileUploadTest(LabControllerTestCase):
+class LogUploadTest(LabControllerTestCase):
 
     def setUp(self):
         with session.begin():
@@ -226,7 +226,7 @@ class FileUploadTest(LabControllerTestCase):
             data_setup.create_job_for_recipes([self.recipe])
             data_setup.mark_recipe_running(self.recipe)
 
-    def test_recipe_file(self):
+    def test_xmlrpc_recipe_log(self):
         s = xmlrpclib.ServerProxy(self.get_proxy_url(), allow_none=True)
         s.recipe_upload_file(self.recipe.id, '/', 'recipe-log', 10, None, 0,
                 b64encode('a' * 10))
@@ -247,7 +247,30 @@ class FileUploadTest(LabControllerTestCase):
                     open(os.path.join(local_log_dir, 'recipe-log'), 'r').read(),
                     'aaaaaaaaaabbbbbbbbbb')
 
-    def test_recipetask_file(self):
+    def test_PUT_recipe_log(self):
+        upload_url = '%srecipes/%s/logs/PUT-recipe-log' % (self.get_proxy_url(),
+                self.recipe.id)
+        response = requests.put(upload_url, data='a' * 10)
+        self.assertEquals(response.status_code, 204)
+        local_log_dir = '%s/recipes/%s/' % (get_conf().get('CACHEPATH'), self.recipe.id)
+        with session.begin():
+            self.assertEquals(self.recipe.logs[0].path, '/')
+            self.assertEquals(self.recipe.logs[0].filename, 'PUT-recipe-log')
+            self.assertEquals(self.recipe.logs[0].server,
+                    'http://localhost/beaker/logs/recipes/%s/' % self.recipe.id)
+            self.assertEquals(self.recipe.logs[0].basepath, local_log_dir)
+            self.assertEquals(
+                    open(os.path.join(local_log_dir, 'PUT-recipe-log'), 'r').read(),
+                    'aaaaaaaaaa')
+        response = requests.put(upload_url, data='b' * 10,
+                headers={'Content-Range': 'bytes 10-19/20'})
+        self.assertEquals(response.status_code, 204)
+        with session.begin():
+            self.assertEquals(
+                    open(os.path.join(local_log_dir, 'PUT-recipe-log'), 'r').read(),
+                    'aaaaaaaaaabbbbbbbbbb')
+
+    def test_xmlrpc_task_log(self):
         with session.begin():
             task = self.recipe.tasks[0]
         s = xmlrpclib.ServerProxy(self.get_proxy_url(), allow_none=True)
@@ -270,7 +293,32 @@ class FileUploadTest(LabControllerTestCase):
                     open(os.path.join(local_log_dir, 'task-log'), 'r').read(),
                     'aaaaaaaaaabbbbbbbbbb')
 
-    def test_recipetaskresult_file(self):
+    def test_PUT_task_log(self):
+        with session.begin():
+            task = self.recipe.tasks[0]
+        upload_url = '%srecipes/%s/tasks/%s/logs/PUT-task-log' % (self.get_proxy_url(),
+                self.recipe.id, task.id)
+        response = requests.put(upload_url, data='a' * 10)
+        self.assertEquals(response.status_code, 204)
+        local_log_dir = '%s/tasks/%s/' % (get_conf().get('CACHEPATH'), task.id)
+        with session.begin():
+            self.assertEquals(task.logs[0].path, '/')
+            self.assertEquals(task.logs[0].filename, 'PUT-task-log')
+            self.assertEquals(task.logs[0].server,
+                    'http://localhost/beaker/logs/tasks/%s/' % task.id)
+            self.assertEquals(task.logs[0].basepath, local_log_dir)
+            self.assertEquals(
+                    open(os.path.join(local_log_dir, 'PUT-task-log'), 'r').read(),
+                    'aaaaaaaaaa')
+        response = requests.put(upload_url, data='b' * 10,
+                headers={'Content-Range': 'bytes 10-19/20'})
+        self.assertEquals(response.status_code, 204)
+        with session.begin():
+            self.assertEquals(
+                    open(os.path.join(local_log_dir, 'PUT-task-log'), 'r').read(),
+                    'aaaaaaaaaabbbbbbbbbb')
+
+    def test_xmlrpc_result_log(self):
         with session.begin():
             self.recipe.tasks[0].pass_('', 0, 'Pass')
             result = self.recipe.tasks[0].results[0]
@@ -292,4 +340,31 @@ class FileUploadTest(LabControllerTestCase):
         with session.begin():
             self.assertEquals(
                     open(os.path.join(local_log_dir, 'result-log'), 'r').read(),
+                    'aaaaaaaaaabbbbbbbbbb')
+
+    def test_PUT_result_log(self):
+        with session.begin():
+            task = self.recipe.tasks[0]
+            task.pass_('', 0, 'Pass')
+            result = self.recipe.tasks[0].results[0]
+        upload_url = '%srecipes/%s/tasks/%s/results/%s/logs/PUT-result-log' % (
+                self.get_proxy_url(), self.recipe.id, task.id, result.id)
+        response = requests.put(upload_url, data='a' * 10)
+        self.assertEquals(response.status_code, 204)
+        local_log_dir = '%s/results/%s/' % (get_conf().get('CACHEPATH'), result.id)
+        with session.begin():
+            self.assertEquals(result.logs[0].path, '/')
+            self.assertEquals(result.logs[0].filename, 'PUT-result-log')
+            self.assertEquals(result.logs[0].server,
+                    'http://localhost/beaker/logs/results/%s/' % result.id)
+            self.assertEquals(result.logs[0].basepath, local_log_dir)
+            self.assertEquals(
+                    open(os.path.join(local_log_dir, 'PUT-result-log'), 'r').read(),
+                    'aaaaaaaaaa')
+        response = requests.put(upload_url, data='b' * 10,
+                headers={'Content-Range': 'bytes 10-19/20'})
+        self.assertEquals(response.status_code, 204)
+        with session.begin():
+            self.assertEquals(
+                    open(os.path.join(local_log_dir, 'PUT-result-log'), 'r').read(),
                     'aaaaaaaaaabbbbbbbbbb')

LabController/src/bkr/labcontroller/default.conf
@@ -17,6 +17,9 @@ SLEEP_TIME = 20
 # How often to renew our session on the server
 RENEW_SESSION_INTERVAL = 300
 
+# Location of locally stored job logs
+CACHEPATH = "/var/www/beaker/logs"
+
 # Location of system console logs
 CONSOLE_LOGS = "/var/consoles"
 

LabController/src/bkr/labcontroller/log_storage.py
@@ -1,0 +1,100 @@
+
+# We are talking about job logs here, not logs produced by the daemons.
+
+import os, os.path
+import errno
+from bkr.common.helpers import makedirs_ignore
+
+class LogFile(object):
+
+    def __init__(self, path, register_func):
+        self.path = path #: absolute path where the log will be stored
+        self.register_func = register_func #: called only if the file was created
+
+    def __enter__(self):
+        makedirs_ignore(os.path.dirname(self.path), 0755)
+        created = False
+        try:
+            # stdio does not have any mode string which corresponds to this 
+            # combination of flags, so we have to use raw os.open :-(
+            fd = os.open(self.path, os.O_RDWR | os.O_CREAT | os.O_EXCL, 0644)
+            created = True
+        except (OSError, IOError), e:
+            if e.errno != errno.EEXIST:
+                raise
+            fd = os.open(self.path, os.O_RDWR)
+        try:
+            self.f = os.fdopen(fd, 'r+')
+            if created:
+                # first time we have touched this file, need to register it
+                self.register_func()
+            return self
+        except Exception:
+            os.close(fd)
+            raise
+
+    def __exit__(self, type, value, traceback):
+        self.f.close()
+        del self.f
+
+    def truncate(self, size):
+        self.f.truncate(size)
+
+    def update_chunk(self, data, offset):
+        if offset < 0:
+            raise ValueError('Offset cannot be negative')
+        self.f.seek(offset, os.SEEK_SET)
+        # XXX the original uploadFile acquires an exclusive lock while writing, 
+        # for no reason that I can discern
+        self.f.write(data)
+        self.f.flush()
+
+class LogStorage(object):
+
+    """
+    Handles storage of job logs on the local filesystem.
+
+    The old XML-RPC API doesn't include the recipe ID with the task or result 
+    upload calls. So for now, everything is stored flat. Eventually it would be 
+    nice to arrange things hierarchically with everything under recipe instead.
+    """
+
+    def __init__(self, base_dir, base_url, hub):
+        self.base_dir = base_dir
+        if not base_url.endswith('/'):
+            base_url += '/' # really it is always a directory
+        self.base_url = base_url
+        self.hub = hub
+
+    def recipe(self, recipe_id, path):
+        path = os.path.normpath(path.lstrip('/'))
+        if path.startswith('../'):
+            raise ValueError('Upload path not allowed: %s' % path)
+        recipe_base = os.path.join(self.base_dir, 'recipes', str(recipe_id))
+        return LogFile(os.path.join(recipe_base, path),
+                lambda: self.hub.recipes.register_file(
+                    '%srecipes/%s/' % (self.base_url, recipe_id),
+                    recipe_id, os.path.dirname(path), os.path.basename(path),
+                    recipe_base + '/'))
+
+    def task(self, task_id, path):
+        path = os.path.normpath(path.lstrip('/'))
+        if path.startswith('../'):
+            raise ValueError('Upload path not allowed: %s' % path)
+        task_base = os.path.join(self.base_dir, 'tasks', str(task_id))
+        return LogFile(os.path.join(task_base, path),
+                lambda: self.hub.recipes.tasks.register_file(
+                    '%stasks/%s/' % (self.base_url, task_id),
+                    task_id, os.path.dirname(path), os.path.basename(path),
+                    task_base + '/'))
+
+    def result(self, result_id, path):
+        path = os.path.normpath(path.lstrip('/'))
+        if path.startswith('../'):
+            raise ValueError('Upload path not allowed: %s' % path)
+        result_base = os.path.join(self.base_dir, 'results', str(result_id))
+        return LogFile(os.path.join(result_base, path),
+                lambda: self.hub.recipes.tasks.register_result_file(
+                    '%sresults/%s/' % (self.base_url, result_id),
+                    result_id, os.path.dirname(path), os.path.basename(path),
+                    result_base + '/'))

LabController/src/bkr/labcontroller/main.py
@@ -63,6 +63,12 @@ class WSGIApplication(object):
                     endpoint=(self.proxy_http, 'post_watchdog')),
             Rule('/recipes/<recipe_id>/tasks/<task_id>/results/', methods=['POST'],
                     endpoint=(self.proxy_http, 'post_result')),
+            Rule('/recipes/<recipe_id>/logs/<path:path>', methods=['PUT'],
+                    endpoint=(self.proxy_http, 'put_recipe_log')),
+            Rule('/recipes/<recipe_id>/tasks/<task_id>/logs/<path:path>',
+                    methods=['PUT'], endpoint=(self.proxy_http, 'put_task_log')),
+            Rule('/recipes/<recipe_id>/tasks/<task_id>/results/<result_id>/logs/<path:path>',
+                    methods=['PUT'], endpoint=(self.proxy_http, 'put_result_log')),
         ])
 
     @Request.application

Server/bkr/server/recipesets.py
@@ -29,7 +29,6 @@ from bkr.server.xmlrpccontroller import RPCRoot
 from bkr.server.helpers import *
 from bkr.server.recipetasks import RecipeTasks
 from socket import gethostname
-from bkr.upload import Uploader
 import exceptions
 import time
 

commit 913b112b79eb9013b8efad34e141b5e3e073dee1

fix console log storage in beaker-watchdog
This was a typo introduced by 32dea6ec.

Change-Id: I95a68087b980954704bc89fe2800a910ae2ec6de

LabController/src/bkr/labcontroller/proxy.py
@@ -262,8 +262,9 @@ class WatchFile(object):
             elif size < self.blocksize and where == now:
                 return False
             else:
+                self.where = now
                 with self.proxy.log_storage.recipe(self.watchdog['recipe_id'], self.filename) as log_file:
-                    log_file.update_chunk(line, now)
+                    log_file.update_chunk(line, where)
                 return True
         return False
 