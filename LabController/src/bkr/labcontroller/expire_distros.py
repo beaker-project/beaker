7d12a3c3ae6bf526bb8eeda05a24bff0cd2cfc9c
Dan Callaghan <dcallagh@redhat.com>
2013-04-05 11:28:04 +1000

    beaker-expire-distros: distinguish missing trees from server errors
    Change-Id: Id76f09b6adc7dcd81383b59eb204b12aedbd7036

--- LabController/src/bkr/labcontroller/expire_distros.py
+++ LabController/src/bkr/labcontroller/expire_distros.py
@@ -4,32 +4,27 @@ import xmlrpclib
 import urllib2
 import urlparse
 
-def url_exists(url):
+def check_http(url):
     try:
         urllib2.urlopen(url, timeout=20)
-    except urllib2.URLError:
-        return False
-    except urllib2.HTTPError:
-        return False
-    except IOError, e:
-        # errno 21 is you tried to retrieve a directory.  Thats ok. We just
-        # want to ensure the path is valid so far.
-        if e.errno == 21:
-            pass
+        return True
+    except urllib2.HTTPError, e:
+        if e.code in (404, 410):
+            return False
         else:
             raise
-    return True
 
-def check_uri(tree):
-    """ Make sure the tree is accessible, check that the server is up first.
-    """
-    url = urlparse.urlparse(tree)
-    server_url = '%s://%s' % (url[0], url[1])
-    tree_url = '%s%s' % (server_url, url[2])
-
-    if url_exists(server_url) and not url_exists(tree_url):
+def check_ftp(url):
+    try:
+        urllib2.urlopen(url, timeout=20)
         return True
-    return False
+    except urllib2.URLError, e:
+        if '550' in e.reason:
+            return False
+        else:
+            raise
+
+class NFSServerInaccessible(ValueError): pass
 
 def check_nfs(tree):
     """ Make sure the tree is accessible, check that the server is up first.
@@ -39,33 +34,52 @@ def check_nfs(tree):
     if nfs_path.startswith('/'):
         nfs_path = nfs_path[1:]
     tree_path = os.path.join(server_path, nfs_path)
-    if os.path.exists(server_path) and not os.path.exists(tree_path):
-        return True
-    return False
+    if not os.path.exists(server_path):
+        raise NFSServerInaccessible('Cannot access NFS server %s '
+                'or autofs not running (%s does not exist)'
+                % (nfs_server, server_path))
+    if not os.path.exists(tree_path):
+        return False
+    return True
 
-def main():
+def check_url(url):
+    """
+    Returns True if the given URL exists.
+    """
+    scheme = urlparse.urlparse(url).scheme
+    if scheme == 'nfs' or scheme.startswith('nfs+'):
+        return check_nfs(url)
+    elif scheme == 'http':
+        return check_http(url)
+    elif scheme == 'ftp':
+        return check_ftp(url)
+    else:
+        raise ValueError('Unrecognised URL scheme %s for tree %s' % (scheme, url))
+
+def check_all_trees(ignore_errors=False):
     proxy = xmlrpclib.ServerProxy('http://localhost:8000', allow_none=True)
     rdistro_trees = []
     distro_trees = proxy.get_distro_trees()
     for distro_tree in distro_trees:
         accessable = False
         for lc, url in distro_tree['available']:
-            if url.startswith('nfs://'):
-                # Check nfs
-                if check_nfs(url):
-                    sys.stderr.write("%s is missing\n" % url)
-                else:
+            try:
+                if check_url(url):
                     accessable = True
-            else:
-                # use urllib
-                if check_uri(url):
-                    sys.stderr.write("%s is missing\n" % url)
                 else:
+                    print '%s is missing' % url
+            except (urllib2.URLError, urllib2.HTTPError, NFSServerInaccessible), e:
+                if ignore_errors:
+                    # suppress exception, assume the tree still exists
                     accessable = True
-        else:
-            if not accessable:
-                # All methods were unaccessable!
-                rdistro_trees.append(distro_tree)
+                else:
+                    sys.stderr.write('Error checking for existence of URL %s '
+                            'for distro tree %s:\n%s\n'
+                            % (url, distro_tree['distro_tree_id'], e))
+                    sys.exit(1)
+        if not accessable:
+            # All methods were unaccessable!
+            rdistro_trees.append(distro_tree)
 
     # if all distro_trees are expired then something is probably wrong.
     if len(distro_trees) != len(rdistro_trees):
@@ -76,6 +90,17 @@ def main():
     else:
         sys.stderr.write("All distros are missing! Please check your server!!\n")
         sys.exit(1)
+
+def main():
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option('--ignore-errors', default=False, action='store_true',
+            help='Ignore all network errors when communicating with mirrors')
+    options, args = parser.parse_args()
+    try:
+        check_all_trees(options.ignore_errors)
+    except KeyboardInterrupt:
+        pass
 
 if __name__ == '__main__':
     main()
