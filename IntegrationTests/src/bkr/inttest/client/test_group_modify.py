8abc382af1cf9d7f38bc75ae5d9a8267a71638b3
Amit Saha <asaha@redhat.com>
2013-05-16 13:19:15 +1000

    Add/Remove group members
    This patch adds support for adding/removing group members
    by the owner of a group.
    
    Bug: 908176
    Change-Id: If90775ed63483cad2ac504c7a5c7311cade76389

--- IntegrationTests/src/bkr/inttest/client/test_group_modify.py
+++ IntegrationTests/src/bkr/inttest/client/test_group_modify.py
@@ -52,6 +52,19 @@ class GroupModifyTest(unittest.TestCase):
             self.assert_('Exactly one group name must be specified' in
                          e.stderr_output, e.stderr_output)
 
+    def test_group_modify_not_owner(self):
+        display_name = 'A New Group Display Name'
+
+        try:
+            out = run_client(['bkr', 'group-modify',
+                              '--display-name', display_name,
+                              self.group.group_name],
+                             config = self.rand_client_config)
+            self.fail('Must fail or die')
+        except ClientError, e:
+            self.assert_('You are not an owner of group' in
+                         e.stderr_output, e.stderr_output)
+
     def test_group_modify_display_name(self):
         display_name = 'A New Group Display Name'
         out = run_client(['bkr', 'group-modify',
@@ -60,7 +73,7 @@ class GroupModifyTest(unittest.TestCase):
                          config = self.client_config)
 
         with session.begin():
-            session.expunge_all()
+            session.refresh(self.group)
             group = Group.by_name(self.group.group_name)
             self.assertEquals(group.display_name, display_name)
             self.assertEquals(group.activity[-1].action, u'Changed')
@@ -78,7 +91,7 @@ class GroupModifyTest(unittest.TestCase):
                          config = self.client_config)
 
         with session.begin():
-            session.expunge_all()
+            session.refresh(self.group)
             group = Group.by_name(group_name)
             self.assertEquals(group.group_name, group_name)
             self.assertEquals(group.activity[-1].action, u'Changed')
@@ -98,20 +111,110 @@ class GroupModifyTest(unittest.TestCase):
                          config = self.client_config)
 
         with session.begin():
-            session.expunge_all()
+            session.refresh(self.group)
             group = Group.by_name(group_name)
             self.assertEquals(group.display_name, display_name)
             self.assertEquals(group.group_name, group_name)
 
-    def test_group_modify_not_owner(self):
-        display_name = 'A New Group Display Name'
+    def test_group_modify_add_member(self):
+        with session.begin():
+            user = data_setup.create_user()
+
+        out = run_client(['bkr', 'group-modify',
+                          '--add-member', user.user_name,
+                          self.group.group_name],
+                         config = self.client_config)
+
+        with session.begin():
+            session.refresh(self.group)
+            group = Group.by_name(self.group.group_name)
+            self.assert_(user.user_name in
+                         [u.user_name for u in group.users])
 
         try:
             out = run_client(['bkr', 'group-modify',
-                              '--display-name', display_name,
+                              '--add-member', 'idontexist',
                               self.group.group_name],
-                             config = self.rand_client_config)
-            self.fail()
+                             config = self.client_config)
+            self.fail('Must fail or die')
         except ClientError, e:
-            self.assert_('You are not an owner of group' in
+            self.assert_('User does not exist' in
                          e.stderr_output, e.stderr_output)
+
+        try:
+            out = run_client(['bkr', 'group-modify',
+                              '--add-member', user.user_name,
+                              self.group.group_name],
+                             config = self.client_config)
+            self.fail('Must fail or die')
+        except ClientError, e:
+            self.assert_('User %s is already in group' % user.user_name in
+                         e.stderr_output, e.stderr_output)
+
+        with session.begin():
+            session.refresh(self.group)
+            group = Group.by_name(self.group.group_name)
+            self.assertEquals(group.activity[-1].action, u'Added')
+            self.assertEquals(group.activity[-1].field_name, u'User')
+            self.assertEquals(group.activity[-1].user.user_id,
+                              self.user.user_id)
+            self.assertEquals(group.activity[-1].new_value, user.user_name)
+            self.assertEquals(group.activity[-1].service, u'XMLRPC')
+
+    def test_group_modify_remove_member(self):
+        with session.begin():
+            user = data_setup.create_user()
+            self.group.users.append(user)
+            session.flush()
+            self.assert_(user in self.group.users)
+
+        out = run_client(['bkr', 'group-modify',
+                          '--remove-member', user.user_name,
+                          self.group.group_name],
+                         config = self.client_config)
+
+        with session.begin():
+            session.refresh(self.group)
+            group = Group.by_name(self.group.group_name)
+            self.assert_(user.user_name not in
+                         [u.user_name for u in group.users])
+
+        try:
+            out = run_client(['bkr', 'group-modify',
+                              '--remove-member', 'idontexist',
+                              self.group.group_name],
+                             config = self.client_config)
+            self.fail('Must fail or die')
+        except ClientError, e:
+            self.assert_('User does not exist' in
+                         e.stderr_output, e.stderr_output)
+        try:
+            out = run_client(['bkr', 'group-modify',
+                              '--remove-member', user.user_name,
+                              self.group.group_name],
+                             config = self.client_config)
+            self.fail('Must fail or die')
+        except ClientError, e:
+            self.assert_('No user %s in group' % user.user_name in
+                         e.stderr_output, e.stderr_output)
+
+        try:
+            out = run_client(['bkr', 'group-modify',
+                              '--remove-member', self.user.user_name,
+                              self.group.group_name],
+                             config = self.client_config)
+            self.fail('Must fail or die')
+        except ClientError, e:
+            self.assert_('You are the only owner of group' in
+                         e.stderr_output, e.stderr_output)
+
+        with session.begin():
+            session.refresh(self.group)
+            group = Group.by_name(self.group.group_name)
+            self.assertEquals(group.activity[-1].action, u'Removed')
+            self.assertEquals(group.activity[-1].field_name, u'User')
+            self.assertEquals(group.activity[-1].user.user_id,
+                              self.user.user_id)
+            self.assertEquals(group.activity[-1].old_value, user.user_name)
+            self.assertEquals(group.activity[-1].new_value, '')
+            self.assertEquals(group.activity[-1].service, u'XMLRPC')
