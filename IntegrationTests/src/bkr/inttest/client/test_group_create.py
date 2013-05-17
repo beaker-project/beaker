3e7b94d93c0c234a6d33a7acff0e0257b2007ef7
Amit Saha <asaha@redhat.com>
2013-05-18 00:17:42 +1000

    Add LDAP support to bkr group-create.
    Bug: 908173
    Change-Id: I4fc6097e57eb6421d684bacb3d4e2c84c6a25146

--- IntegrationTests/src/bkr/inttest/client/test_group_create.py
+++ IntegrationTests/src/bkr/inttest/client/test_group_create.py
@@ -1,8 +1,8 @@
 import unittest
 from turbogears.database import session
-from bkr.server.model import Activity, Group
+from bkr.server.model import Activity, Group, User
 from bkr.inttest import data_setup
-from bkr.inttest.client import run_client, ClientError
+from bkr.inttest.client import run_client, ClientError, create_client_config
 
 class GroupCreateTest(unittest.TestCase):
 
@@ -42,6 +42,36 @@ class GroupCreateTest(unittest.TestCase):
         except ClientError,e:
             self.assert_('Exactly one group name must be specified' in
                          e.stderr_output, e.stderr_output)
+
+    def test_ldap_group(self):
+
+        group_name = 'wyfp'
+        display_name = 'My LDAP Group'
+        out = run_client(['bkr', 'group-create', '--ldap',
+                          '--display-name', display_name,
+                          group_name])
+
+        group = Group.by_name(group_name)
+        self.assertEquals(group.ldap, True)
+        self.assertEquals(group.users, [User.by_user_name(u'asaha')])
+
+        with session.begin():
+            rand_user = data_setup.create_user(password = 'asdf')
+
+        rand_client_config = create_client_config(username=rand_user.user_name,
+                                                  password='asdf')
+
+        group_name = 'alp'
+        display_name = 'ALP'
+        try:
+            out = run_client(['bkr', 'group-create', '--ldap',
+                          '--display-name', display_name,
+                              group_name],
+                             config = rand_client_config)
+            self.fail('Must fail or die')
+        except ClientError, e:
+            self.assert_('Only admins can create LDAP groups' in
+                         e.stderr_output)
 
     def test_group_duplicate(self):
         group_name = data_setup.unique_name('group%s')
