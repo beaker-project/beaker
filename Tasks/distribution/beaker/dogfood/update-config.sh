d2edd4599f767db8ac775e8ad1f1a1236878c27d
Dan Callaghan <dcallagh@redhat.com>
2013-05-24 16:12:37 +1000

    /distribution/beaker/dogfood: set up config as expected for tests
    LDAP config and SLEEP_TIME are new. Also rearranged the sed'ing, and
    updated it to only touch files which exist, to better support running
    the task on a separate server and lab controller.
    
    Change-Id: I4bad8fb4a9de006fe4ac43e899f0f6b1c7edb19d

--- Tasks/distribution/beaker/dogfood/update-config.sh
+++ Tasks/distribution/beaker/dogfood/update-config.sh
@@ -1,18 +1,38 @@
 #!/bin/sh
 set -e
 
-# update Beaker config to match what the tests are expecting
-sed --regexp-extended --in-place=-orig --copy -e '
-    s@^#?tg.url_domain.*$@tg.url_domain = "localhost"@
-    s@^#?mail.on.*$@mail.on = True@
-    s@^#?mail.smtp.server.*$@mail.smtp.server = "127.0.0.1:19999"@
-    s@^#?beaker.reliable_distro_tag.*$@beaker.reliable_distro_tag = "RELEASED"@
-    s@^#?beaker.motd.*$@beaker.motd = "/usr/lib/python2.6/site-packages/bkr/inttest/server/motd.xml"@
-    s@^#?beaker.max_running_commands .*$@beaker.max_running_commands = 10@
-    s@^#?beaker.kernel_options .*$@beaker.kernel_options = "noverifyssl"@
-    ' /etc/beaker/server.cfg
+# Updates the Beaker config to match what the tests are expecting.
 
-# reduce number of Apache worker processes, to save a bit of memory
-sed --regexp-extended --in-place=-orig --copy -e '
-    /^WSGIDaemonProcess/ s@processes=[0-9]+@processes=2@
-    ' /etc/httpd/conf.d/beaker-server.conf
+if [ -e /etc/beaker/server.cfg ] ; then
+    sed --regexp-extended --in-place=-orig --copy -e '
+        /^#?tg\.url_domain/c                tg.url_domain = "localhost"
+        /^#?mail\.on/c                      mail.on = True
+        /^#?mail\.smtp\.server/c            mail.smtp.server = "127.0.0.1:19999"
+        /^#?beaker\.reliable_distro_tag/c   beaker.reliable_distro_tag = "RELEASED"
+        /^#?beaker\.motd/c                  beaker.motd = "/usr/lib/python2.6/site-packages/bkr/inttest/server/motd.xml"
+        /^#?beaker\.max_running_commands /c beaker.max_running_commands = 10
+        /^#?beaker\.kernel_options /c       beaker.kernel_options = "noverifyssl"
+        /^#?identity\.ldap\.enabled/c       identity.ldap.enabled = True
+        /^#?identity\.soldapprovider\.uri/c identity.soldapprovider.uri = "ldap://localhost:3899/"
+        /^#?identity\.soldapprovider\.basedn/c identity.soldapprovider.basedn = "dc=example,dc=invalid"
+        /^#?identity\.soldapprovider\.autocreate/c identity.soldapprovider.autocreate = True
+        ' /etc/beaker/server.cfg
+fi
+
+if [ -e /etc/httpd/conf.d/beaker-server.conf ] ; then
+    # reduce number of Apache worker processes, to save a bit of memory
+    sed --regexp-extended --in-place=-orig --copy -e '
+        /^WSGIDaemonProcess/ s@processes=[0-9]+@processes=2@
+        ' /etc/httpd/conf.d/beaker-server.conf
+    service httpd reload
+fi
+
+if [ -e /etc/beaker/labcontroller.conf ] ; then
+    sed --regexp-extended --in-place=-orig --copy -e '
+        $a SLEEP_TIME = 5
+        ' /etc/beaker/labcontroller.conf
+    service beaker-proxy condrestart
+    service beaker-provision condrestart
+    service beaker-watchdog condrestart
+    service beaker-transfer condrestart
+fi
