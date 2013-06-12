223c7524f14a44b8d29b5b627e84e7aa8a456c6a
Dan Callaghan <dcallagh@redhat.com>
2013-06-12 12:02:25 +1000

    push up transaction handling to Flask instead of TurboGears
    This way Flask request handlers get the same transaction management as
    existing TurboGears controller methods.
    
    Change-Id: I80ce5e5c2294f0d91477a343f31fc045b8aa4780

--- Server/bkr/server/wsgi.py
+++ Server/bkr/server/wsgi.py
@@ -14,8 +14,10 @@ del orig_signal_signal
 import sys
 import logging
 from turbogears import config
+from turbogears.database import session
 import cherrypy
 import cherrypy._cpwsgi
+from cherrypy.filters.basefilter import BaseFilter
 from flask import Flask
 
 log = logging.getLogger(__name__)
@@ -34,6 +36,16 @@ import bkr.server.controllers
 
 @app.before_first_request
 def init():
+    # Make TG's run_with_transaction a no-op, we manage the transaction here 
+    # through Flask instead.
+    import turbogears.database
+    def run_with_transaction_noop(func, *args, **kwargs):
+        return func(*args, **kwargs)
+    turbogears.database.run_with_transaction = run_with_transaction_noop
+    class EndTransactionsFilterNoop(BaseFilter): pass
+    turbogears.database.EndTransactionsFilter = EndTransactionsFilterNoop
+    turbogears.startup.EndTransactionsFilter = EndTransactionsFilterNoop
+
     # Set up old CherryPy stuff.
     cherrypy.root = bkr.server.controllers.Root()
     cherrypy.server.start(init_only=True, server_class=None)
@@ -51,6 +63,33 @@ def init():
     tgmochikit.init(register_static_directory, config)
 
     log.debug('Application initialised')
+
+@app.before_request
+def begin_session():
+    session.begin()
+
+@app.after_request
+def commit_or_rollback_session(response):
+    # Matches behaviour of TG's sa_rwt: commit on success or redirect, 
+    # roll back on error.
+    if session.is_active:
+        if response.status_code >= 200 and response.status_code < 400:
+            session.commit()
+        else:
+            log.debug('Rolling back for %s response', response.status_code)
+            session.rollback()
+    return response
+
+@app.teardown_appcontext
+def close_session(exception=None):
+    try:
+        if session.is_active:
+            log.warn('Session active when tearing down app context, rolling back')
+            session.rollback()
+        session.close()
+    except Exception, e:
+        # log and suppress
+        log.exception('Error closing session when tearing down app context')
 
 @app.after_request
 def fall_back_to_cherrypy(response):
