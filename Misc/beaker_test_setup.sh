58a08a949768b56b5a6b81d98194282475314e0a
Amit Saha <asaha@redhat.com>
2013-08-19 14:05:00 +1000

    Changes to test scripts and dogfood task:
    - Use selenium-2.35.0 JAR to run the tests
    - Update Misc/beaker_test_setup.sh script
    - Update dogfood task to use 2.35.0

--- Misc/beaker_test_setup.sh
+++ Misc/beaker_test_setup.sh
@@ -4,23 +4,37 @@
 # and copies selenium JAR to the appropriate location. All of these
 # are needed to run Beaker's test suite.
 
-# Tested on Fedora 18
+# Tested on Fedora 19+20
 # You should run this as root
 
 # Abort if any of these fails
 set -e
 
-# Beaker server repository
-wget -O /etc/yum.repos.d/beaker-server.repo http://beaker-project.org/yum/beaker-server-Fedora.repo
+# create beaker RPMs
+
+pushd ../
+
+yum-builddep -y beaker.spec
+yum -y install tito createrepo
+tito build --test --rpm
+createrepo --no-database /tmp/tito/noarch
+
+cat >/etc/yum.repos.d/beaker-local-builds.repo <<"EOF"
+[tito]
+name=tito
+baseurl=file:///tmp/tito/noarch/
+EOF
+
+popd
 
 # Find the dependencies
 yum deplist beaker-server beaker-lab-controller beaker-integration-tests beaker-client beaker | grep 'provider' | grep -v 'beaker*' | awk '{print $2'} | sort -u > beaker_deplist
 
-# Need this for doing a make which sets up the path for the integration
-# TODO: Perhaps worth fixing this.
-echo "python-sphinx" >> beaker_deplist
+# others
 echo "git" >> beaker_deplist
-echo "mysql-server" >> beaker_deplist
+echo "mariadb-server" >> beaker_deplist
+echo "mariadb" >> beaker_deplist
+echo "openldap-servers" >> beaker_deplist
 
 # Install them
 while read line
@@ -28,16 +42,15 @@ do
     yum -y install `yum info $line | grep 'Name' | awk '{print $3}'`
 done <beaker_deplist
 
-#setup mysql
+
+#setup mariadb
 cp /etc/my.cnf /etc/my.cnf-orig
 cat /etc/my.cnf-orig | awk '
-        {print $1};
+        {print $0};
         /\[mysqld\]/ {
-            print "default-storage-engine=INNODB";
-            print "max_allowed_packet=50M";
             print "character-set-server=utf8";
         }' > /etc/my.cnf
-service mysqld restart
+systemctl restart mysqld
 echo "CREATE DATABASE beaker_test;" | mysql
 echo "GRANT ALL ON beaker_test.* TO 'beaker'@'localhost' IDENTIFIED BY
 'beaker';" | mysql
@@ -45,5 +58,5 @@ echo "GRANT ALL ON beaker_test.* TO 'beaker'@'localhost' IDENTIFIED BY
 # Download selenium JAR
 mkdir -p /usr/local/share/selenium
 pushd /usr/local/share/selenium
-wget http://selenium.googlecode.com/files/selenium-server-standalone-2.33.0.jar
+wget http://selenium.googlecode.com/files/selenium-server-standalone-2.35.0.jar
 popd
