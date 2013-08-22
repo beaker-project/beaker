beaker-project/beaker

commit d1a31ded9b2b23d35151dc78abda0fb909bba4c5

use conditional INSERT instead of nested transactions in lazy_create
This lets us avoid several classes of race conditions when concurrent
transactions invoke lazy_create (the bug report has the gory details).
As an added bonus, it should improve performance due to less SQLAlchemy
object expiry.

Unfortunately it makes the implementation of lazy_create a bit
trickier...

Bug: 874386
Change-Id: I5b4a1ce3b59f82003e8c2983c5d2ca66f20fc2d6

Server/bkr/server/model.py
@@ -12,17 +12,19 @@ from sqlalchemy import (Table, Column, Index, ForeignKey, UniqueConstraint,
 
 from sqlalchemy.orm import relation, backref, synonym, dynamic_loader, \
         query, object_mapper, mapper, column_property, contains_eager, \
-        relationship
+        relationship, class_mapper
 from sqlalchemy.orm.interfaces import AttributeExtension
 from sqlalchemy.orm.attributes import NEVER_SET
-from sqlalchemy.sql import exists, union, literal
-from sqlalchemy.sql.expression import join
+from sqlalchemy.orm.util import has_identity
+from sqlalchemy.sql import exists, union, literal, Insert
+from sqlalchemy.sql.expression import join, Executable, ClauseElement
 from sqlalchemy.exc import InvalidRequestError, IntegrityError
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.types import TypeDecorator, BINARY
 from bkr.server.installopts import InstallOptions, global_install_options
 from sqlalchemy.orm.collections import attribute_mapped_collection, MappedCollection, collection
 from sqlalchemy.util import OrderedDict
+from sqlalchemy.ext import compiler
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.ext.declarative import declarative_base
 import socket
@@ -95,6 +97,41 @@ def decl_base_constructor(self, **kwargs):
 
 DeclBase = declarative_base(constructor=decl_base_constructor,
                             metadata=metadata)
+
+
+class ConditionalInsert(Insert):
+    def __init__(self, table, unique_values, extra_values={}):
+        """
+        An INSERT statement which will atomically insert a row with the given 
+        values if one does not exist, or otherwise do nothing. extra_values are 
+        values which do not participate in the unique identity of the row.
+
+        unique_values and extra_values are dicts of (column -> scalar).
+        """
+        values = dict(unique_values)
+        values.update(extra_values)
+        super(ConditionalInsert, self).__init__(table, values)
+        self.unique_condition = and_(*[col == value
+                for col, value in unique_values.iteritems()])
+
+@compiler.compiles(ConditionalInsert)
+def visit_conditional_insert(element, compiler, **kwargs):
+    # magic copied from sqlalchemy.sql.compiler.SQLCompiler.visit_insert
+    compiler.isinsert = True
+    colparams = compiler._get_colparams(element)
+    text = 'INSERT INTO %s' % compiler.process(element.table, asfrom=True)
+    text += ' (%s)\n' % ', '.join(compiler.process(c[0]) for c in colparams)
+    text += 'SELECT %s\n' % ', '.join(c[1] for c in colparams)
+    text += 'FROM DUAL\n'
+    # We need FOR UPDATE in the inner SELECT for MySQL, to ensure we acquire an 
+    # exclusive lock immediately, instead of acquiring a shared lock and then 
+    # subsequently upgrading it to an exclusive lock, which is subject to 
+    # deadlocks if another transaction is doing the same thing.
+    text += 'WHERE NOT EXISTS (SELECT 1 FROM %s\nWHERE %s FOR UPDATE)' % (
+            compiler.process(element.table, asfrom=True),
+            compiler.process(element.unique_condition))
+    return text
+
 
 class TaskStatus(DeclEnum):
 
@@ -1431,21 +1468,69 @@ class MappedObject(object):
     query = session.query_property()
 
     @classmethod
-    def lazy_create(cls, **kwargs):
+    def _convert_attr_and_value(cls, table, key, value):
+        # This is kind of a reimplementation of some bits of the ORM... we 
+        # probably shouldn't be playing with this magic, but it makes things 
+        # much nicer for the callers of lazy_create.
+        prop = class_mapper(cls).get_property(key)
+        if hasattr(prop, 'columns'):
+            # ColumnProperties are pretty easy...
+            assert len(prop.columns) == 1
+            assert prop.columns[0].table == table
+            return prop.columns[0], value
+        elif hasattr(prop, 'remote_side'):
+            # we can handle simple RelationshipProperties too...
+            assert len(prop.local_side) == 1
+            assert len(prop.remote_side) == 1
+            assert has_identity(value), 'Value %r must be persisted' % value
+            attr_name = prop.mapper.get_property_by_column(prop.remote_side[0]).key
+            return prop.local_side[0], getattr(value, attr_name)
+        else:
+            # ... but anything else is too hard
+            raise AssertionError('Unknown property type %r' % prop)
+
+    @classmethod
+    def lazy_create(cls, _extra_attrs={}, **kwargs):
         """
         Returns the instance identified by the given uniquely-identifying 
-        attributes. If it doesn't exist yet, it is inserted first. If it is
-        not in the session, it is added.
+        attributes. If it doesn't exist yet, it is inserted first.
+
+        _extra_attrs is for attributes which do not make up the unique 
+        identity, but which need to be specified during creation because they 
+        are not NULLable. Subclasses should override this and pass attributes 
+        up in _extra_attrs if needed.
+
+        BEWARE: this method bypasses SQLAlchemy's ORM machinery. It makes some 
+        attempts to understand relationship properties, but anything other than 
+        simple many-to-one relationships onto persistent instances will raise 
+        AssertionError. Pass column properties instead of relationship 
+        properties (for example, osmajor_id not osmajor) if that is an issue.
         """
-        session.begin_nested()
-        try:
-            item = cls(**kwargs)
-            session.add(item)
-            session.commit()
-        except IntegrityError:
-            session.rollback()
-            item = cls.query.filter_by(**kwargs).one()
-        return item
+        # We do a conditional INSERT, which will always succeed or 
+        # silently have no effect.
+        # http://stackoverflow.com/a/6527838/120202
+        # Note that (contrary to that StackOverflow answer) on InnoDB the 
+        # INSERT must come before the UPDATE to avoid deadlocks.
+        unique_params = {}
+        extra_params = {}
+        assert len(class_mapper(cls).tables) == 1
+        table = class_mapper(cls).tables[0]
+        for k, v in kwargs.iteritems():
+            col, value = cls._convert_attr_and_value(table, k, v)
+            unique_params[col] = value
+        for k, v in _extra_attrs.iteritems():
+            col, value = cls._convert_attr_and_value(table, k, v)
+            extra_params[col] = value
+
+        session.connection(cls).execute(ConditionalInsert(table,
+                unique_params, extra_params))
+
+        if extra_params:
+            session.connection(cls).execute(table.update()
+                    .values(extra_params)
+                    .where(and_(*[col == value for col, value in unique_params.iteritems()])))
+
+        return cls.query.with_lockmode('update').filter_by(**kwargs).one()
 
     def __init__(self, **kwargs):
         for k, v in kwargs.iteritems():
@@ -3265,7 +3350,14 @@ class OSVersion(MappedObject):
     def __repr__(self):
         return "%s.%s" % (self.osmajor,self.osminor)
 
-    
+    def add_arch(self, arch):
+        """
+        Adds the given arch to this OSVersion if it's not already present.
+        """
+        session.connection(self.__class__).execute(ConditionalInsert(
+                osversion_arch_map,
+                {osversion_arch_map.c.osversion_id: self.id,
+                 osversion_arch_map.c.arch_id: arch.id}))
 
 
 class LabControllerDistroTree(MappedObject):
@@ -3513,20 +3605,8 @@ class Distro(MappedObject):
 
     @classmethod
     def lazy_create(cls, name, osversion):
-        """
-        Distro is unique on name only, but osversion_id also needs to be
-        supplied on insertion because it is not NULLable. So this is
-        a specialisation of the usual lazy_create method.
-        """
-        session.begin_nested()
-        try:
-            item = cls(name=name, osversion=osversion)
-            session.commit()
-        except IntegrityError:
-            session.rollback()
-            item = cls.query.filter_by(name=name).one()
-            item.osversion = osversion
-        return item
+        return super(Distro, cls).lazy_create(name=name,
+                _extra_attrs=dict(osversion=osversion))
 
     @classmethod
     def by_name(cls, name):
@@ -3723,11 +3803,21 @@ class DistroTree(MappedObject):
 
 class DistroTreeRepo(MappedObject):
 
-    pass
+    @classmethod
+    def lazy_create(cls, distro_tree, repo_id, repo_type, path):
+        return super(DistroTreeRepo, cls).lazy_create(
+                distro_tree=distro_tree,
+                repo_id=repo_id,
+                _extra_attrs=dict(repo_type=repo_type, path=path))
 
 class DistroTreeImage(MappedObject):
 
-    pass
+    @classmethod
+    def lazy_create(cls, distro_tree, image_type, kernel_type, path):
+        return super(DistroTreeImage, cls).lazy_create(
+                distro_tree=distro_tree,
+                image_type=image_type, kernel_type=kernel_type,
+                _extra_attrs=dict(path=path))
 
 class DistroTag(MappedObject):
     def __init__(self, tag=None):
@@ -3975,21 +4065,7 @@ class Log(MappedObject):
 
     @classmethod
     def lazy_create(cls, path=None, **kwargs):
-        """
-        Unlike the "real" lazy_create above, we can't rely on unique
-        constraints here because 'path' and 'filename' are TEXT columns and
-        MySQL can't index those. :-(
-
-        So we just use a query-then-insert approach. There is a race window
-        between querying and inserting, but it's about the best we can do.
-        """
-        item = cls.query.filter_by(path=cls._normalized_path(path),
-                **kwargs).first()
-        if item is None:
-            item = cls(path=path, **kwargs)
-            session.add(item)
-            session.flush()
-        return item
+        return super(Log, cls).lazy_create(path=cls._normalized_path(path), **kwargs)
 
     def __init__(self, path=None, filename=None,
                  server=None, basepath=None, parent=None):
@@ -6889,14 +6965,15 @@ class Task(MappedObject):
         """Create a new task object based on details retrieved from an RPM"""
 
         tinfo = testinfo.parse_string(raw_taskinfo['desc'])
-        task = cls.lazy_create(name=tinfo.test_name)
 
-        if len(task.name) > 255:
+        if len(tinfo.test_name) > 255:
             raise BX(_("Task name should be <= 255 characters"))
-        if task.name.endswith('/'):
+        if tinfo.test_name.endswith('/'):
             raise BX(_(u'Task name must not end with slash'))
-        if '//' in task.name:
+        if '//' in tinfo.test_name:
             raise BX(_(u'Task name must not contain redundant slashes'))
+
+        task = cls.lazy_create(name=tinfo.test_name)
 
         # RPM is the same version we have. don't process
         if task.version == raw_taskinfo['hdr']['ver']:

IntegrationTests/src/bkr/inttest/server/selenium/test_labcontrollers.py
@@ -1,5 +1,7 @@
 
+import time
 import datetime
+from threading import Thread, Event
 from turbogears.database import session
 from bkr.inttest.server.selenium import SeleniumTestCase, XmlRpcTestCase, \
     WebDriverTestCase
@@ -7,8 +9,10 @@ from bkr.inttest.server.webdriver_utils import login, is_activity_row_present
 from bkr.inttest import data_setup, get_server_base, fix_beakerd_repodata_perms
 from bkr.server.model import Distro, DistroTree, Arch, ImageType, Job, \
         System, SystemStatus, TaskStatus, CommandActivity, CommandStatus, \
-        KernelType, LabController, Recipe, Activity
+        KernelType, LabController, Recipe, Activity, User, OSMajor, OSVersion
 from bkr.server.tools import beakerd
+from bkr.server.wsgi import app
+from bkr.server import identity
 from bkr.inttest.server.selenium.test_activity import is_activity_row_present
 
 class LabControllerViewTest(WebDriverTestCase):
@@ -231,6 +235,118 @@ class AddDistroTreeXmlRpcTest(XmlRpcTestCase):
             self.assertEquals(distro.osversion.osmajor.osmajor,
                     u'RedHatEnterpriseLinux6')
             self.assertEquals(distro.osversion.osminor, u'1')
+
+    def add_distro_trees_concurrently(self, distro_data1, distro_data2):
+        # This doesn't actually call through XML-RPC, it calls the 
+        # controller directly in two separate threads, in order to simulate two 
+        # lab controllers importing the same distro tree at the same instant.
+        from bkr.server.labcontroller import LabControllers
+        controller = LabControllers()
+        class DistroImportThread(Thread):
+            def __init__(self, lc_user_name=None, distro_data=None, **kwargs):
+                super(DistroImportThread, self).__init__(**kwargs)
+                self.lc_user_name = lc_user_name
+                self.distro_data = distro_data
+                self.ready_evt = Event()
+                self.start_evt = Event()
+                self.commit_evt = Event()
+                self.success = False
+            def run(self):
+                with app.test_request_context('/RPC2'):
+                    session.begin()
+                    self.ready_evt.set()
+                    self.start_evt.wait()
+                    lc_user = User.by_user_name(self.lc_user_name)
+                    identity.set_authentication(lc_user)
+                    controller.add_distro_tree(self.distro_data)
+                    self.commit_evt.wait()
+                    session.commit()
+                self.success = True
+
+        thread1 = DistroImportThread(name='add_distro_trees_thread1',
+                lc_user_name=self.lc.user.user_name,
+                distro_data=distro_data1)
+        thread2 = DistroImportThread(name='add_distro_trees_thread2',
+                lc_user_name=self.lc2.user.user_name,
+                distro_data=distro_data2)
+        thread1.start()
+        thread2.start()
+        thread1.ready_evt.wait()
+        thread2.ready_evt.wait()
+        # If you're debugging this test, uncommenting these prints and sleeps, 
+        # and enabling logging for sqlalchemy.engine, will help.
+        #print '\n\n\n*********** THREAD 1 GO'
+        thread1.start_evt.set()
+        #time.sleep(1)
+        #print '\n\n\n*********** THREAD 2 GO'
+        thread2.start_evt.set()
+        #time.sleep(1)
+        #print '\n\n*********** THREAD 1 COMMIT'
+        thread1.commit_evt.set()
+        #time.sleep(1)
+        #print '\n\n*********** THREAD 2 COMMIT'
+        thread2.commit_evt.set()
+        thread1.join()
+        thread2.join()
+        self.assert_(thread1.success)
+        self.assert_(thread2.success)
+
+    # https://bugzilla.redhat.com/show_bug.cgi?id=874386
+    def test_concurrent_different_trees(self):
+        distro_data = dict(self.distro_data)
+        # ensure osmajor, osversion, and distro already exist
+        with session.begin():
+            osmajor = OSMajor.lazy_create(osmajor=distro_data['osmajor'])
+            osversion = OSVersion.lazy_create(osmajor=osmajor,
+                    osminor=distro_data['osminor'])
+            osversion.arches = [Arch.lazy_create(arch=arch)
+                    for arch in distro_data['arches']]
+            Distro.lazy_create(name=distro_data['name'], osversion=osversion)
+        # ensure two different trees
+        distro_data['variant'] = u'Workstation'
+        distro_data2 = dict(distro_data)
+        distro_data2['variant'] = u'Server'
+        self.add_distro_trees_concurrently(distro_data, distro_data2)
+
+    def test_concurrent_same_tree(self):
+        distro_data = dict(self.distro_data)
+        # ensure osmajor, osversion, and distro already exist
+        with session.begin():
+            osmajor = OSMajor.lazy_create(osmajor=distro_data['osmajor'])
+            osversion = OSVersion.lazy_create(osmajor=osmajor,
+                    osminor=distro_data['osminor'])
+            osversion.arches = [Arch.lazy_create(arch=arch)
+                    for arch in distro_data['arches']]
+            Distro.lazy_create(name=distro_data['name'], osversion=osversion)
+        self.add_distro_trees_concurrently(distro_data, distro_data)
+
+    def test_concurrent_new_distro(self):
+        distro_data = dict(self.distro_data)
+        # ensure osmajor and osversion already exist
+        with session.begin():
+            osmajor = OSMajor.lazy_create(osmajor=distro_data['osmajor'])
+            osversion = OSVersion.lazy_create(osmajor=osmajor,
+                    osminor=distro_data['osminor'])
+            osversion.arches = [Arch.lazy_create(arch=arch)
+                    for arch in distro_data['arches']]
+        # ... but distro is new
+        distro_data['name'] = 'concurrent-new-distro'
+        self.add_distro_trees_concurrently(distro_data, distro_data)
+
+    def test_concurrent_new_osversion(self):
+        distro_data = dict(self.distro_data)
+        # ensure osmajor already exists
+        with session.begin():
+            osmajor = OSMajor.lazy_create(osmajor=distro_data['osmajor'])
+        # ... but osversion is new
+        distro_data['osminor'] = '6969'
+        self.add_distro_trees_concurrently(distro_data, distro_data)
+
+    def test_concurrent_new_osmajor(self):
+        distro_data = dict(self.distro_data)
+        # ensure osmajor is new
+        distro_data['osmajor'] = 'ConcurrentEnterpriseLinux6'
+        self.add_distro_trees_concurrently(distro_data, distro_data)
 
 class GetDistroTreesXmlRpcTest(XmlRpcTestCase):
 

IntegrationTests/src/bkr/inttest/server/selenium/test_provision.py
@@ -17,7 +17,7 @@ class SystemManualProvisionTest(SeleniumTestCase):
             self.lab_controller = data_setup.create_labcontroller()
             self.user = data_setup.create_user(password=u'password')
             self.distro_tree = data_setup.create_distro_tree(osmajor=u'Fedora',
-                    arch=u'i386')
+                    arch=u'i386', lab_controllers=[self.lab_controller])
             self.system = data_setup.create_system(arch=u'i386',
                     owner=self.user, status=u'Manual', shared=True)
             self.system.lab_controller = self.lab_controller
@@ -72,7 +72,8 @@ class SystemManualProvisionInstallOptionsTest(WebDriverTestCase):
             self.system = data_setup.create_system(lab_controller=self.lab_controller, \
                                                        owner=self.user,status=u'Manual', shared=True)
             self.system.user = self.user
-            self.distro_tree = data_setup.create_distro_tree(osmajor=u'Fedora', arch=u'i386')
+            self.distro_tree = data_setup.create_distro_tree(osmajor=u'Fedora', arch=u'i386',
+                    lab_controllers=[self.lab_controller])
             self.system.provisions[self.distro_tree.arch] = \
                 Provision(arch=self.distro_tree.arch,
                           kernel_options=u'key1=value1 key1=value2 key1 key2=value key3')

IntegrationTests/src/bkr/inttest/server/selenium/test_remove_lc.py
@@ -10,7 +10,7 @@ class RemoveLabController(SeleniumTestCase):
         self.lc = data_setup.create_labcontroller(
             fqdn=data_setup.unique_name(u'%d1111'))
         self.system.lab_controller = self.lc
-        self.distro_tree = data_setup.create_distro_tree()
+        self.distro_tree = data_setup.create_distro_tree(lab_controllers=[self.lc])
         self.selenium = self.get_selenium()
         self.selenium.start()
         self.login()

IntegrationTests/src/bkr/inttest/server/selenium/test_reserve_system.py
@@ -123,7 +123,8 @@ class ReserveSystem(WebDriverTestCase):
         with session.begin():
             self.lc = data_setup.create_labcontroller()
             self.system = data_setup.create_system(arch=u'i386')
-            self.distro_tree = data_setup.create_distro_tree(arch=u'i386')
+            self.distro_tree = data_setup.create_distro_tree(arch=u'i386',
+                    lab_controllers=[self.lc])
             self.system.lab_controller = self.lc
             self.system.shared = True
         self.browser = self.get_browser()

IntegrationTests/src/bkr/inttest/server/selenium/test_system_view.py
@@ -44,7 +44,8 @@ class SystemViewTestWD(WebDriverTestCase):
             self.lab_controller = data_setup.create_labcontroller()
             self.user = data_setup.create_user(password=u'password')
             self.system = data_setup.create_system(lab_controller=self.lab_controller)
-            self.distro_tree = data_setup.create_distro_tree()
+            self.distro_tree = data_setup.create_distro_tree(
+                    lab_controllers=[self.lab_controller])
 
         self.browser = self.get_browser()
 
@@ -146,7 +147,8 @@ class SystemViewTest(SeleniumTestCase):
         self.lab_controller = data_setup.create_labcontroller()
         self.system_owner = data_setup.create_user()
         self.unprivileged_user = data_setup.create_user(password=u'password')
-        self.distro_tree = data_setup.create_distro_tree()
+        self.distro_tree = data_setup.create_distro_tree(
+                lab_controllers=[self.lab_controller])
         self.system = data_setup.create_system(owner=self.system_owner,
                 status=u'Automated', arch=u'i386')
         self.system.shared = True

IntegrationTests/src/bkr/inttest/server/selenium/test_systems_xmlrpc.py
@@ -368,7 +368,7 @@ class SystemProvisionXmlRpcTest(XmlRpcTestCase):
     def setUp(self):
         self.lab_controller = data_setup.create_labcontroller()
         self.distro_tree = data_setup.create_distro_tree(osmajor=u'Fedora',
-                arch=u'i386')
+                arch=u'i386', lab_controllers=[self.lab_controller])
         self.usable_system = data_setup.create_system(arch=u'i386',
                 owner=User.by_user_name(data_setup.ADMIN_USER),
                 status=u'Manual', shared=True)

IntegrationTests/src/bkr/inttest/server/test_beakerd.py
@@ -307,6 +307,7 @@ class TestBeakerd(unittest.TestCase):
     def test_just_in_time_systems_multihost(self):
         with session.begin():
             systemA = data_setup.create_system(lab_controller=self.lab_controller)
+            systemB = data_setup.create_system(lab_controller=self.lab_controller)
             r1 = data_setup.create_recipe()
             j1 = data_setup.create_job_for_recipes([r1])
             j1_id = j1.id
@@ -322,7 +323,7 @@ class TestBeakerd(unittest.TestCase):
             r1 = Recipe.by_id(r1_id)
             data_setup.mark_recipe_running(r1)
             systemA = System.by_fqdn(systemA.fqdn, User.by_user_name('admin'))
-            systemB = data_setup.create_system(lab_controller=self.lab_controller)
+            systemB = System.by_fqdn(systemB.fqdn, User.by_user_name('admin'))
             r2 = data_setup.create_recipe()
             r3 = data_setup.create_recipe()
             j2 = data_setup.create_job_for_recipes([r2,r3])
@@ -446,7 +447,8 @@ class TestBeakerd(unittest.TestCase):
             system2.lab_controller = lc2
             system3 = data_setup.create_system(arch=u'i386', shared=True)
             system3.lab_controller = lc3
-            job = data_setup.create_job(owner=user)
+            distro_tree = data_setup.create_distro_tree(lab_controllers=[lc1, lc2, lc3])
+            job = data_setup.create_job(owner=user, distro_tree=distro_tree)
             job.recipesets[0].recipes[0]._host_requires = (u"""
                    <hostRequires>
                     <or>

commit a14a54a4003075f34c657195a20ec833011e0bd7

handle None values in lazy_create
This fixes an error introduced in commit d1a31ded. It is indeed caught
by the test suite, but I wasn't paying proper attention when I merged
that commit.

We need to accept None values for mapped relationships in case the
relationship is NULLable.

Change-Id: I28b69a6e04404c4db719e5445db238a3ae6edb16

Server/bkr/server/model.py
@@ -1483,6 +1483,8 @@ class MappedObject(object):
             # we can handle simple RelationshipProperties too...
             assert len(prop.local_side) == 1
             assert len(prop.remote_side) == 1
+            if value is None:
+                return prop.local_side[0], None
             assert has_identity(value), 'Value %r must be persisted' % value
             attr_name = prop.mapper.get_property_by_column(prop.remote_side[0]).key
             return prop.local_side[0], getattr(value, attr_name)