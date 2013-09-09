001f58250fd6ee5076ad6a7b971f5574dacb4c84
Amit Saha <asaha@redhat.com>
2013-09-09 16:00:30 +1000

    Add support for removing user accounts via command line
    - Adds a new command bkr remove-account
    - Fixes BZ# 966292
    
    Bug: 966292
    Change-Id: Ie8a8cb923f70e519c11d312b65e4fcde3ad2dcf9

---
+++ Client/src/bkr/client/commands/cmd_remove_account.py
@@ -0,0 +1,60 @@
+"""
+bkr remove-account: Remove user accounts
+========================================
+
+.. program:: bkr remove-account
+
+Synopsis
+--------
+
+| :program:`bkr remove-account` <user>...
+
+Description
+-----------
+
+Remove a Beaker user account.
+
+Removing a user account cancels any running job(s), returns all
+the systems in use by the user, modifies the ownership of the
+systems owned to the admin closing the account, and disables the
+account for further login.
+
+
+Common :program:`bkr` options are described in the :ref:`Options
+<common-options>` section of :manpage:`bkr(1)`.
+
+Exit status
+-----------
+
+Non-zero on error, otherwise zero.
+
+Examples
+--------
+
+Close the accounts of users, user1 and user2::
+
+    bkr remove-account user1 user2
+
+See also
+--------
+
+:manpage:`bkr(1)`
+"""
+from bkr.client import BeakerCommand
+
+class Remove_Account(BeakerCommand):
+    """Remove user accounts"""
+
+    enabled=True
+
+    def options(self):
+        self.parser.usage = "%%prog %s" % self.normalized_name
+
+    def run(self, *args, **kwargs):
+
+        if not args:
+            self.parser.error('Please provide the username(s) of the account to remove')
+
+        self.set_hub(**kwargs)
+        for username in args:
+            self.hub.users.remove_account(username)
---
+++ IntegrationTests/src/bkr/inttest/client/test_remove_account.py
@@ -0,0 +1,43 @@
+import unittest2 as unittest
+from turbogears.database import session
+from bkr.inttest import data_setup, with_transaction
+from bkr.inttest.client import run_client, create_client_config, ClientError
+
+class RemoveAccountTest(unittest.TestCase):
+
+    def test_admin_delete_self(self):
+        try:
+            run_client(['bkr', 'remove-account', 'admin'])
+            fail('Must fail or die')
+        except ClientError, e:
+            self.assertIn('You cannot remove yourself', e.stderr_output)
+
+    def test_delete_user(self):
+        with session.begin():
+            user1 = data_setup.create_user(password=u'asdf')
+            user2 = data_setup.create_user(password=u'qwerty')
+
+        run_client(['bkr', 'remove-account', user1.user_name, user2.user_name])
+        with session.begin():
+            session.refresh(user1)
+            session.refresh(user2)
+            self.assertTrue(user1.removed)
+            self.assertTrue(user2.removed)
+        try:
+            run_client(['bkr', 'remove-account', user1.user_name])
+            self.fail('Must fail or die')
+        except ClientError, e:
+            self.assertIn('User already removed', e.stderr_output)
+
+    def test_non_admin_cannot_delete(self):
+        with session.begin():
+            user3 = data_setup.create_user(password=u'qwerty')
+            client_config1 = create_client_config(username = user3.user_name,
+                                                  password='qwerty')
+        try:
+            # it's okay to use the same user since we won't reach there anyway :-)
+            run_client(['bkr', 'remove-account', user3.user_name],
+                       config=client_config1)
+            self.fail('Must fail or die')
+        except ClientError, e:
+            self.assertIn('Not member of group: admin', e.stderr_output)
