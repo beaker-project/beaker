beaker-project/beaker

commit 44b4735e97fd65a09e5b1fc4dc4831a448a60ecf

clean up Take/Provision Now/Schedule Provision mess
Take/Return and immediate provisioning are only permitted if the system
is set to Manual. Otherwise, the system can only be reserved or
provisioned through the scheduler.

Bug: 855333
Change-Id: Ie5a5c7301ecf17ee9d881ae1fccc546524d81801

Server/bkr/server/templates/system_provision.kid
@@ -2,8 +2,8 @@
  name="${name}"
  action="${tg.url(action)}"
  method="${method}" width="100%">
- <span py:if="lab_controller and will_provision is True">
-  <span py:if="not tg.identity.anonymous">
+  <span py:if="lab_controller">
+    <span py:if="(automated and can_reserve) or (not automated and reserved)">
    <script language="JavaScript" type="text/JavaScript">
     ${name}_0 = new Provision('${id.field_id}', '${prov_install.field_id}', '${ks_meta.field_id}','${koptions.field_id}','${koptions_post.field_id}','${tg.url('/get_installoptions')}');
     addLoadEvent(${name}_0.initialize);
@@ -37,7 +37,7 @@
               ${display_field_for("koptions_post")}
             </div>
           </div>
-    <span py:if="provision_now_rights">
+    <span py:if="not automated">
           <div class="control-group" py:if="power_enabled">
             <div class="controls">
               <label class="checkbox">
@@ -50,7 +50,7 @@
             This system is not configured for reboot support
           </div>
     </span>
-    <span py:if="not provision_now_rights">
+    <span py:if="automated">
           <div class="control-group">
             <label class="control-label"
                 for="${schedule_reserve_days.field_id}"
@@ -63,20 +63,22 @@
         </div>
       </div>
       <div class="form-actions">
-        <button type="submit" class="btn btn-primary" py:if="provision_now_rights">Provision</button>
-        <button type="submit" class="btn btn-primary" py:if="not provision_now_rights">Schedule provision</button>
+        <button type="submit" class="btn btn-primary" py:if="not automated">Provision</button>
+        <button type="submit" class="btn btn-primary" py:if="automated">Schedule provision</button>
       </div>
 
    ${display_field_for("id")}
-  </span>
- </span>
-  <span py:if="not will_provision and provision_now_rights">
+    </span>
+    <span py:if="automated and not can_reserve">
+      You do not have access to schedule a job on this system.
+    </span>
+    <span py:if="not automated and not reserved and can_reserve">
    You can only provision if you have reserved the system.
+    </span>
+    <span py:if="not automated and not reserved and not can_reserve">
+      You do not have access to reserve this system.
+    </span>
   </span>
-  <span py:if="not will_provision and not provision_now_rights">
-   You do not have access to provision or schedule a job on this system.
-  </span>
-
  <span py:if="not lab_controller">
   This system is not associated to a lab controller
  </span>

IntegrationTests/src/bkr/inttest/server/selenium/test_system_return.py
@@ -22,7 +22,10 @@ class SystemReturnTestWD(WebDriverTestCase):
         system = self.recipe.resource.system
         login(b)
         b.get(get_server_base() + 'view/%s' % system.fqdn)
-        b.find_element_by_link_text('Return').click()
+        # "Return" button should be absent
+        b.find_element_by_xpath('//form[@name="form" and not(.//a[normalize-space(string(.))="Return"])]')
+        # try doing it directly
+        b.get(get_server_base() + 'user_change?id=%s' % system.id)
         self.assertEquals(b.find_element_by_css_selector('.flash').text,
             "Failed to return %s: Currently running R:%s" % (system.fqdn, self.recipe.id))
 
@@ -55,8 +58,7 @@ class SystemReturnTest(SeleniumTestCase):
         # Test for https://bugzilla.redhat.com/show_bug.cgi?id=747328
         sel.open('user_change?id=%s' % self.system.id)
         sel.wait_for_page_to_load("30000")
-        self.assert_('You were unable to change the user for %s' % self.system.fqdn in sel.get_text('//body'))
-
+        self.assertIn('cannot unreserve system', sel.get_text('css=.flash'))
 
     def test_return_with_no_lc(self):
         sel = self.selenium

IntegrationTests/src/bkr/inttest/server/selenium/test_system_take.py
@@ -67,7 +67,8 @@ class SystemTakeTest(WebDriverTestCase):
             user = data_setup.create_user(password=u'testing')
         b = self.browser
         login(b, user=user.user_name, password='testing')
-        self.check_cannot_take(system)
+        self.check_cannot_take_automated(system)
+        self.check_schedule_provision(system)
 
     def test_system_no_group_manual(self):
         with session.begin():
@@ -88,7 +89,7 @@ class SystemTakeTest(WebDriverTestCase):
             data_setup.add_group_to_system(system, group)
         b = self.browser
         login(b, user=user.user_name, password='testing')
-        self.check_cannot_take(system)
+        self.check_cannot_take_automated(system)
         self.check_cannot_schedule_provision(system)
 
     def test_system_has_group_manual(self):
@@ -101,7 +102,7 @@ class SystemTakeTest(WebDriverTestCase):
             data_setup.add_group_to_system(system, group)
         b = self.browser
         login(b, user=user.user_name, password='testing')
-        self.check_cannot_take(system)
+        self.check_cannot_take_manual(system)
         self.check_cannot_schedule_provision(system)
 
     def test_system_group_user_group(self):
@@ -116,7 +117,8 @@ class SystemTakeTest(WebDriverTestCase):
             data_setup.add_group_to_system(system, group)
         b = self.browser
         login(b, user=user.user_name, password='testing')
-        self.check_cannot_take(system)
+        self.check_cannot_take_automated(system)
+        self.check_cannot_schedule_provision(system)
 
     def test_system_in_user_group(self):
         with session.begin():
@@ -128,7 +130,8 @@ class SystemTakeTest(WebDriverTestCase):
             data_setup.add_group_to_system(system, group)
         b = self.browser
         login(b, user=user.user_name, password='testing')
-        self.check_take(system)
+        self.check_cannot_take_automated(system)
+        self.check_schedule_provision(system)
 
     def test_manual_system_in_user_group(self):
         with session.begin():
@@ -142,6 +145,26 @@ class SystemTakeTest(WebDriverTestCase):
         login(b, user=user.user_name, password='testing')
         self.check_take(system)
 
+    def test_automated_system_loaned_to_another_user(self):
+        with session.begin():
+            system = data_setup.create_system(status=SystemStatus.automated,
+                    shared=True, lab_controller=self.lc)
+            system.loaned = data_setup.create_user()
+            user = data_setup.create_user(password=u'testing')
+        b = self.browser
+        login(b, user=user.user_name, password='testing')
+        self.check_schedule_provision(system)
+
+    def test_manual_system_loaned_to_another_user(self):
+        with session.begin():
+            system = data_setup.create_system(status=SystemStatus.manual,
+                    shared=True, lab_controller=self.lc)
+            system.loaned = data_setup.create_user()
+            user = data_setup.create_user(password=u'testing')
+        b = self.browser
+        login(b, user=user.user_name, password='testing')
+        self.check_cannot_take_manual(system)
+
     def go_to_system_view(self, system):
         self.browser.get(get_server_base() + 'view/%s' % system.fqdn)
 
@@ -152,7 +175,7 @@ class SystemTakeTest(WebDriverTestCase):
         self.assertEquals(b.find_element_by_class_name('flash').text,
                 'Reserved %s' % system.fqdn)
 
-    def check_cannot_take(self, system):
+    def check_cannot_take_manual(self, system):
         self.go_to_system_view(system)
         b = self.browser
         # "Take" link should be absent
@@ -160,8 +183,20 @@ class SystemTakeTest(WebDriverTestCase):
         # Try taking it directly as well:
         # https://bugzilla.redhat.com/show_bug.cgi?id=747328
         b.get(get_server_base() + 'user_change?id=%s' % system.id)
-        self.assertEquals(b.find_element_by_class_name('flash').text,
-                'You were unable to change the user for %s' % system.fqdn)
+        self.assertIn('cannot reserve system',
+                b.find_element_by_class_name('flash').text)
+
+    def check_cannot_take_automated(self, system):
+        self.go_to_system_view(system)
+        b = self.browser
+        # "Take" link should be absent
+        b.find_element_by_xpath('//form[@name="form" and not(.//a[normalize-space(string(.))="Take"])]')
+        # Try taking it directly as well:
+        # https://bugzilla.redhat.com/show_bug.cgi?id=747328
+        b.get(get_server_base() + 'user_change?id=%s' % system.id)
+        self.assertIn(
+                'Cannot manually reserve automated system',
+                b.find_element_by_class_name('flash').text)
 
     def check_schedule_provision(self, system):
         self.go_to_system_view(system)

IntegrationTests/src/bkr/inttest/server/selenium/test_systems_xmlrpc.py
@@ -57,8 +57,7 @@ class ReserveSystemXmlRpcTest(XmlRpcTestCase):
             server.systems.reserve(system.fqdn)
             self.fail('should raise')
         except xmlrpclib.Fault, e:
-            self.assert_('Cannot reserve system with status Automated'
-                    in e.faultString)
+            self.assertIn('Cannot manually reserve automated system', e.faultString)
 
     def test_cannot_reserve_system_in_use(self):
         with session.begin():
@@ -71,8 +70,7 @@ class ReserveSystemXmlRpcTest(XmlRpcTestCase):
             server.systems.reserve(system.fqdn)
             self.fail('should raise')
         except xmlrpclib.Fault, e:
-            self.assert_('cannot reserve system' in e.faultString,
-                    e.faultString)
+            self.assertIn('already reserved', e.faultString)
 
     def test_reserve_system(self):
         with session.begin():
@@ -169,8 +167,7 @@ class ReleaseSystemXmlRpcTest(XmlRpcTestCase):
             server.systems.release(system.fqdn)
             self.fail('should raise')
         except xmlrpclib.Fault, e:
-            self.assert_('System is reserved by a different user'
-                    in e.faultString)
+            self.assertIn('cannot unreserve system', e.faultString)
 
     def test_release_system(self):
         with session.begin():
@@ -399,9 +396,7 @@ class SystemProvisionXmlRpcTest(XmlRpcTestCase):
         try:
             self.server.systems.provision(system.fqdn, 'distro')
         except xmlrpclib.Fault, e:
-            # It's not really a permissions issue, but oh well
-            self.assert_('has insufficient permissions to provision'
-                    in e.faultString)
+            self.assertIn('Reserve a system before provisioning', e.faultString)
         with session.begin():
             self.assertEquals(system.command_queue, [])
 

Server/bkr/server/controller_utilities.py
@@ -44,41 +44,6 @@ class _FKLogEntry:
         self.mapper_column_name = mapper_column_name
 
 
-class SystemTab:
-
-    @classmethod
-    def get_provision_perms(cls, system, our_user, currently_held):
-
-        if system.can_share(our_user) and system.can_provision_now(our_user): #Has privs and machine is available, can take
-            provision_now_rights = False
-            will_provision = True # User with take privs can also schedule it they wish
-            provision_action = '/schedule_provision'
-        elif not system.can_provision_now(our_user): # If you don't have privs to take
-            if system.is_available(our_user): #And you have access to the machine, schedule
-                provision_action = '/schedule_provision'
-                provision_now_rights = False
-                will_provision = True
-            else: #No privs to machine at all
-                will_provision = False
-                provision_now_rights = False
-                provision_action = ''
-        elif system.can_provision_now(our_user) and currently_held: #Has privs, and we are current user, we can provision
-            provision_action = '/action_provision'
-            provision_now_rights = True
-            will_provision = True
-        elif system.can_provision_now(our_user) and not currently_held: #Has privs, not current user, You need to Take it first
-            provision_now_rights = False
-            will_provision = True
-            provision_action = '/schedule_provision'
-        else:
-            log.error('Could not follow logic when determining user access to machine')
-            will_provision = False
-            provision_now_rights = False
-            provision_action = ''
-
-        return provision_now_rights,will_provision,provision_action
-
-
 class _SystemSaveFormHandler:
 
     @classmethod

Server/bkr/server/controllers.py
@@ -42,7 +42,7 @@ from bkr.server.tasks import Tasks
 from bkr.server.task_actions import TaskActions
 from bkr.server.kickstart import KickstartController
 from bkr.server.controller_utilities import Utility, \
-    SystemSaveForm, SystemTab, restrict_http_method
+    SystemSaveForm, restrict_http_method
 from bkr.server.bexceptions import *
 import bkr.server.validators as beaker_validators
 from cherrypy import request, response
@@ -707,13 +707,12 @@ class Root(RPCRoot):
 
         options['loan_widget'] = LoanWidget()
 
-        # Has privs and machine is available, can take
-        if system.can_share(our_user) and \
-            system.can_provision_now(our_user):
+        if system.status != SystemStatus.automated:
+            if system.user and system.can_unreserve(our_user):
+                options['user_change_text'] = 'Return'
+            elif system.is_free() and system.can_reserve(our_user):
                 options['user_change_text'] = 'Take'
 
-        if system.current_user(our_user):
-            options['user_change_text'] = 'Return'
         if system.open_reservation is not None and \
             system.open_reservation.recipe:
                 job_id = system.open_reservation.recipe.recipeset.job.id
@@ -753,20 +752,21 @@ class Root(RPCRoot):
             flash( _(u"No given system to view") )
             redirect("/")
         our_user = identity.current.user
-        if system.can_admin(user=our_user):
-            readonly = False
+        if our_user:
+            readonly = not system.can_admin(our_user)
+            is_user = (system.user == our_user)
+            can_reserve = system.can_reserve(our_user)
         else:
             readonly = True
-        if system.current_user(our_user):
-            is_user = True
-        else:
             is_user = False
+            can_reserve = False
         title = system.fqdn
-        currently_held = system.user == our_user
         options = self._get_system_options(system)
         options['edit'] = False
-        self.provision_now_rights,self.will_provision,self.provision_action = \
-                SystemTab.get_provision_perms(system, our_user, currently_held)
+        if system.status == SystemStatus.automated:
+            provision_action = '/schedule_provision'
+        else:
+            provision_action = '/action_provision'
 
         if 'activities_found' in histories_return:
             historical_data = histories_return['activities_found']
@@ -829,7 +829,7 @@ class Root(RPCRoot):
                                     notes     = url('/save_note'),
                                     groups    = url('/save_group'),
                                     install   = url('/save_install'),
-                                    provision = getattr(self,'provision_action',''),
+                                    provision = url(provision_action),
                                     power_action = url('/action_power'),
                                     arches    = url('/save_arch'),
                                     tasks     = '/tasks/do_search',
@@ -851,9 +851,9 @@ class Root(RPCRoot):
                                    install   = dict(readonly = readonly,
                                                 provisions = system.provisions,
                                                 prov_arch = [(arch.id, arch.arch) for arch in system.arch]),
-                                   provision = dict(is_user = is_user,
-                                                    will_provision = self.will_provision,
-                                                    provision_now_rights = self.provision_now_rights,
+                                   provision = dict(reserved=is_user,
+                                                    automated=system.status == SystemStatus.automated,
+                                                    can_reserve=can_reserve,
                                                     lab_controller = system.lab_controller,
                                                     prov_install = [(dt.id, unicode(dt)) for dt in system.distro_trees().order_by(Distro.name, DistroTree.variant, DistroTree.arch_id)]),
                                    power_action = dict(is_user=is_user),
@@ -1046,22 +1046,20 @@ class Root(RPCRoot):
         except InvalidRequestError:
             flash( _(u"Unable to find system with id of %s" % id) )
             redirect("/")
-        if system.current_user(current_identity):
+        if system.user:
             try:
                 system.unreserve_manually_reserved(service=u'WEBUI')
                 flash(_(u'Returned %s') % system.fqdn)
             except BeakerException, e:
                 log.exception('Failed to return')
                 flash(_(u'Failed to return %s: %s') % (system.fqdn, e))
-        elif system.can_share(current_identity) and system.can_provision_now(current_identity):
+        else:
             try:
-                system.reserve(service=u'WEBUI', reservation_type=u'manual')
+                system.reserve_manually(service=u'WEBUI')
                 flash(_(u'Reserved %s') % system.fqdn)
             except BeakerException, e:
                 log.exception('Failed to reserve')
                 flash(_(u'Failed to reserve %s: %s') % (system.fqdn, e))
-        else:
-            flash(_(u'You were unable to change the user for %s' % system.fqdn))
         redirect("/view/%s" % system.fqdn)
 
     system_cc_form = widgets.TableForm(
@@ -1520,8 +1518,7 @@ class Root(RPCRoot):
                              koptions=None, koptions_post=None, reboot=None):
 
         """
-        We schedule a job which will provision a system. 
-
+        Immediately provision a Manual system. The system must already be reserved.
         """
         distro_tree_id = prov_install
         try:
@@ -1539,21 +1536,20 @@ class Root(RPCRoot):
         if user.rootpw_expired:
             flash( _(u"Your root password has expired, please change or clear it in order to submit jobs.") )
             redirect(u"/view/%s" % system.fqdn)
+        if system.user != user:
+            flash(_(u'Reserve a system before provisioning'))
+            redirect(u'view/%s' % system.fqdn)
 
         try:
-            can_provision_now = system.can_provision_now(user) #Check perms
-            if can_provision_now:
-                from bkr.server.kickstart import generate_kickstart
-                install_options = system.install_options(distro_tree).combined_with(
-                        InstallOptions.from_strings(ks_meta, koptions, koptions_post))
-                if 'ks' not in install_options.kernel_options:
-                    kickstart = generate_kickstart(install_options,
-                            distro_tree=distro_tree, system=system, user=user)
-                    install_options.kernel_options['ks'] = kickstart.link
-                system.configure_netboot(distro_tree,
-                        install_options.kernel_options_str, service=u'WEBUI')
-            else: #This shouldn't happen, maybe someone is trying to be funny
-                raise BX('User: %s has insufficent permissions to provision %s' % (user.user_name, system.fqdn))
+            from bkr.server.kickstart import generate_kickstart
+            install_options = system.install_options(distro_tree).combined_with(
+                    InstallOptions.from_strings(ks_meta, koptions, koptions_post))
+            if 'ks' not in install_options.kernel_options:
+                kickstart = generate_kickstart(install_options,
+                        distro_tree=distro_tree, system=system, user=user)
+                install_options.kernel_options['ks'] = kickstart.link
+            system.configure_netboot(distro_tree,
+                    install_options.kernel_options_str, service=u'WEBUI')
         except Exception, msg:
             log.exception('Failed to provision system %s', id)
             activity = SystemActivity(user=identity.current.user, service=u'WEBUI',

Server/bkr/server/model.py
@@ -2440,19 +2440,6 @@ class System(SystemObject):
                 return True
         return False
 
-    def can_provision_now(self,user=None):
-        if user is None:
-            return False
-        elif self.loaned == user:
-            return True
-        elif self._user_in_systemgroup(user):
-            return True
-        elif user is None:
-            return False
-        if self.status==SystemStatus.manual: #If it's manual then we us our original perm system.
-            return self._has_regular_perms(user)
-        return False
-
     def can_loan(self, user=None):
         if user and not self.loaned:
             if self.can_admin(user):
@@ -2521,18 +2508,38 @@ class System(SystemObject):
                 return True
         return False
 
-    def current_user(self, user=None):
-        if user and self.user:
-            if self.user  == user \
-               or self.can_admin(user):
-                return True
-        return False
-
     def _user_in_systemgroup(self,user=None):
         if self.groups:
             for group in user.groups:
                 if group in self.groups:
                     return True
+
+    def can_reserve(self, user):
+        """
+        Does the given user have permission to reserve this system?
+        """
+        if self.owner == user:
+            return True
+        if self.loaned == user:
+            return True
+        if self.shared:
+            if self.groups:
+                if self._user_in_systemgroup(user):
+                    return True
+            else:
+                return True
+        return False
+
+    def can_unreserve(self, user):
+        """
+        Does the given user have permission to return the current reservation 
+        on this system?
+        """
+        if self.can_admin(user):
+            return True
+        if self.user and self.user == user:
+            return True
+        return False
 
     def _update_loan_comment(self, comment=None, service=u'WEBUI'):
         """Updates the comment associated with a loan
@@ -2557,61 +2564,6 @@ class System(SystemObject):
             _update_comment()
         else:
             raise BX(_('Insufficient permissions to change loan comment'))
-
-    def is_available(self,user=None):
-        """
-        is_available() will return true if this system is allowed to be used by the user.
-        """
-        if user:
-            if self.shared:
-                # If the user is in the Systems groups
-                if self.groups:
-                    if self._user_in_systemgroup(user):
-                        return True
-                else:
-                    return True
-            elif self.loaned and self.loaned == user:
-                return True
-            elif self.owner == user:
-                return True
-        
-    def can_share(self, user=None):
-        """
-        can_share() will return True id the system is currently free and allwoed to be used by the user
-        """
-        if user and not self.user:
-            return self._has_regular_perms(user)
-        return False
-
-    def _has_regular_perms(self, user=None, *args, **kw):
-        """
-        This represents the basic system perms,loanee, owner,  shared and in group or shared and no group
-        """
-        # If user is None then probably not logged in.
-        if not user:
-            return False
-        if self.loaned:
-            if user == self.loaned:
-                return True
-            else:
-                return False
-        # If its the owner always allow.
-        if user == self.owner:
-            return True
-        if self.shared:
-            # If the user is in the Systems groups
-            return self._in_group(user)
-
-    def _in_group(self, user=None, *args, **kw):
-            if user is None:
-                return False
-            if self.groups:
-                for group in user.groups:
-                    if group in self.groups:
-                        return True
-            else:
-                # If the system has no groups
-                return True
 
     ALLOWED_ATTRS = ['vendor', 'model', 'memory'] #: attributes which the inventory scripts may set
     PRESERVED_ATTRS = ['vendor', 'model'] #: attributes which should only be set when empty
@@ -3035,15 +2987,29 @@ class System(SystemObject):
             log.warn(reason)
             self.mark_broken(reason=reason)
 
+    def reserve_manually(self, service, user=None):
+        if self.status == SystemStatus.automated:
+            raise BX(_(u'Cannot manually reserve automated system, '
+                    'schedule a job instead'))
+        return self.reserve(service, user=user, reservation_type=u'manual')
+
     def reserve(self, service, user=None, reservation_type=u'manual'):
         if user is None:
             user = identity.current.user
+
         if self.user is not None and self.user == user:
             raise StaleSystemUserException(_(u'User %s has already reserved '
                 'system %s') % (user, self))
-        if not self.can_share(user):
+        if not self.can_reserve(user):
             raise InsufficientSystemPermissions(_(u'User %s cannot '
                 'reserve system %s') % (user, self))
+        if self.loaned:
+            # loans give exclusive rights to reserve
+            if user != self.loaned and user != self.owner:
+                raise InsufficientSystemPermissions(_(u'User %s cannot reserve '
+                        'system %s while it is loaned to user %s')
+                        % (user, self, self.loaned))
+
         # Atomic operation to reserve the system
         session.flush()
         if session.connection(System).execute(system_table.update(
@@ -3066,11 +3032,12 @@ class System(SystemObject):
         if user is None:
             user = identity.current.user
 
-        # Some sanity checks
         if self.user is None:
             raise BX(_(u'System is not reserved'))
-        if not self.current_user(user):
-            raise BX(_(u'System is reserved by a different user'))
+        if not self.can_unreserve(user):
+            raise InsufficientSystemPermissions(
+                    _(u'User %s cannot unreserve system %s, reserved by %s')
+                    % (user, self, self.user))
 
         # Update reservation atomically first, to avoid races
         session.flush()

Server/bkr/server/systems.py
@@ -54,9 +54,7 @@ class SystemsController(controllers.Controller):
         .. versionadded:: 0.6
         """
         system = System.by_fqdn(fqdn, identity.current.user)
-        if system.status != SystemStatus.manual:
-            raise BX(_(u'Cannot reserve system with status %s') % system.status)
-        system.reserve(service=u'XMLRPC', reservation_type=u'manual')
+        system.reserve_manually(service=u'XMLRPC')
         return system.fqdn # because turbogears makes us return something
 
     @expose()
@@ -203,9 +201,6 @@ class SystemsController(controllers.Controller):
            See :meth:`distrotrees.filter`.
         """
         system = System.by_fqdn(fqdn, identity.current.user)
-        if not system.can_provision_now(identity.current.user):
-            raise BX(_(u'User %s has insufficient permissions to provision %s')
-                    % (identity.current.user.user_name, system.fqdn))
         if not system.user == identity.current.user:
             raise BX(_(u'Reserve a system before provisioning'))
         distro_tree = DistroTree.by_id(distro_tree_id)

commit 1d2e66261c9ae97bd374171393866c6c3849e2c2

fix /bkr/bkr in Schedule Provision action, caused by commit 44b4735e
Change-Id: I22a4765158f180d9ecd8196ffcaff0b83024a189

Server/bkr/server/templates/system_provision.kid
@@ -1,6 +1,6 @@
 <form xmlns:py="http://purl.org/kid/ns#"
  name="${name}"
- action="${tg.url(action)}"
+ action="${action}"
  method="${method}" width="100%">
   <span py:if="lab_controller">
     <span py:if="(automated and can_reserve) or (not automated and reserved)">