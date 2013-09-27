36b3213e4c4848fcb3835d60cfee255e833b72a4
Dan Callaghan <dcallagh@redhat.com>
2013-09-27 11:43:24 +1000

    restore missing Add button on key types page
    Bug: 1012404
    Change-Id: Idd0e77782e86186c8ef2d1fc5df06541aad28a26

---
+++ IntegrationTests/src/bkr/inttest/server/selenium/test_keytypes.py
@@ -0,0 +1,25 @@
+
+from bkr.inttest import get_server_base
+from bkr.inttest.server.selenium import WebDriverTestCase
+from bkr.inttest.server.webdriver_utils import login
+
+class KeyTypesTest(WebDriverTestCase):
+
+    def setUp(self):
+        self.browser = self.get_browser()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    # https://bugzilla.redhat.com/show_bug.cgi?id=1012404
+    def test_add_key_type(self):
+        new_key_name = u'AARDVARK'
+        b = self.browser
+        login(b)
+        b.get(get_server_base() + 'keytypes/')
+        b.find_element_by_link_text('Add').click()
+        b.find_element_by_xpath('//h1[text()="New Key Type"]')
+        b.find_element_by_name('key_name').send_keys(new_key_name)
+        b.find_element_by_id('keytypes').submit()
+        b.find_element_by_xpath('//table/tbody/tr/td[1]'
+                '[normalize-space(string(.))="%s"]' % new_key_name)
