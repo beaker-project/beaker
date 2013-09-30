895f29d490a3e3f229d3db5f209611314a683a82
Dan Callaghan <dcallagh@redhat.com>
2013-09-30 17:29:14 +1000

    Automatic commit of release 0.15.0

--- Common/setup.py
+++ Common/setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "bkr",
-    version='0.15.0rc2',
+    version='0.15.0',
     license = "GPLv2+",
 
     packages=find_packages('.'),
