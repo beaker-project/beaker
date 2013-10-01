0d9c5760c8fe83f681e709eb492e5e534940745a
Dan Callaghan <dcallagh@redhat.com>
2013-10-02 09:46:36 +1000

    test_async: run gevent loop to completion in each test
    LoopExit should actually be expected in all tests, as an indication that
    all running greenlets have terminated. But rather than catching it
    directly, the proper interface is gevent.run(). The body of each test
    should also be spawned in a separate greenlet so that everything works
    properly.
    
    Change-Id: I68875226837c1087c1d7e77650a9ddfa58bd1da6

--- LabController/src/bkr/labcontroller/test_async.py
+++ LabController/src/bkr/labcontroller/test_async.py
@@ -5,7 +5,7 @@ import unittest2 as unittest
 import subprocess
 import signal
 from time import sleep
-from gevent.hub import LoopExit
+import gevent
 from bkr.labcontroller.async import MonitoredSubprocess
 
 class SubprocessTest(unittest.TestCase):
@@ -26,24 +26,31 @@ class SubprocessTest(unittest.TestCase):
                 self.fail("The process group should've already been removed")
 
     def test_runaway_output_is_discarded(self):
-        p = MonitoredSubprocess(['seq', '--format=%0.0f cans of spam on the wall',
-                str(1024 * 1024)], stdout=subprocess.PIPE)
-        try:
+        def _test():
+            p = MonitoredSubprocess(['seq', '--format=%0.0f cans of spam on the wall',
+                    str(1024 * 1024)], stdout=subprocess.PIPE)
             p.dead.wait()
-        except LoopExit:
-            pass # Already terminated
-        out = p.stdout_reader.get()
-        self.assert_(len(out) <= 4096013, len(out))
-        self.assert_(out.endswith('+++ DISCARDED'), out[:-10240])
+            out = p.stdout_reader.get()
+            self.assert_(len(out) <= 4096013, len(out))
+            self.assert_(out.endswith('+++ DISCARDED'), out[:-10240])
+        gevent.spawn(_test)
+        gevent.run()
 
     def test_timeout_is_enforced(self):
-        p = MonitoredSubprocess(['sleep', '10'], timeout=1)
-        p.dead.wait()
-        self.assertEquals(p.returncode, -signal.SIGTERM)
+        def _test():
+            p = MonitoredSubprocess(['sleep', '10'], timeout=1)
+            p.dead.wait()
+            self.assertEquals(p.returncode, -signal.SIGTERM)
+        gevent.spawn(_test)
+        gevent.run()
 
     def test_child_is_process_group_leader(self):
-        p = MonitoredSubprocess(['sleep', '5'])
-        self._assert_child_is_process_group_leader(p)
+        def _test():
+            p = MonitoredSubprocess(['sleep', '1'])
+            self._assert_child_is_process_group_leader(p)
+            p.dead.wait()
+        gevent.spawn(_test)
+        gevent.run()
 
     def test_process_group_is_killed_on_leader_timeout(self):
         # This test makes the following process tree:
@@ -62,12 +69,15 @@ class SubprocessTest(unittest.TestCase):
         #
         # The process group leader should timeout, and everything in the
         # process group should be terminated/killed.
-        p = MonitoredSubprocess(['bash', '-c', '{ sleep 30 ; } & sleep 10'], timeout=1)
-        # The rest of this test hinges on this assertion
-        self._assert_child_is_process_group_leader(p)
-        p.dead.wait()
-        self.assertIn(p.returncode, [-signal.SIGTERM, -signal.SIGKILL])
-        self._assert_process_group_is_removed(p.pid)
+        def _test():
+            p = MonitoredSubprocess(['bash', '-c', '{ sleep 30 ; } & sleep 10'], timeout=1)
+            # The rest of this test hinges on this assertion
+            self._assert_child_is_process_group_leader(p)
+            p.dead.wait()
+            self.assertIn(p.returncode, [-signal.SIGTERM, -signal.SIGKILL])
+            self._assert_process_group_is_removed(p.pid)
+        gevent.spawn(_test)
+        gevent.run()
 
     def test_orphan_child_is_killed_when_parent_exits(self):
         # This test makes the following process tree:
@@ -86,16 +96,22 @@ class SubprocessTest(unittest.TestCase):
         #
         # These should all be in the same process group and should
         # all be killed when the process group leader exits normally.
-        p = MonitoredSubprocess(['bash', '-c', '{ sleep 60 ; } & sleep 1'], timeout=10)
-        # The rest of this test hinges on this assertion
-        self._assert_child_is_process_group_leader(p)
-        p.dead.wait()
-        self.assertEquals(p.returncode, 0)
-        self._assert_process_group_is_removed(p.pid)
+        def _test():
+            p = MonitoredSubprocess(['bash', '-c', '{ sleep 60 ; } & sleep 1'], timeout=10)
+            # The rest of this test hinges on this assertion
+            self._assert_child_is_process_group_leader(p)
+            p.dead.wait()
+            self.assertEquals(p.returncode, 0)
+            self._assert_process_group_is_removed(p.pid)
+        gevent.spawn(_test)
+        gevent.run()
 
     # https://bugzilla.redhat.com/show_bug.cgi?id=832250
     def test_reaper_race(self):
-        procs = [MonitoredSubprocess(['true'], timeout=10)
-                for _ in xrange(800)]
-        for p in procs:
-            p.dead.wait()
+        def _test():
+            procs = [MonitoredSubprocess(['true'], timeout=10)
+                    for _ in xrange(800)]
+            for p in procs:
+                p.dead.wait()
+        gevent.spawn(_test)
+        gevent.run()
