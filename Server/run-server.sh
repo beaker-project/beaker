73c99e8b66aee21d46e4251ab4b81419760df02f
Raymond Mancy <rmancy@redhat.com>
2013-10-28 14:16:55 +1000

    Makes local debugging easier when the thread doesn't timeout in 30 seconds.
    Change-Id: Ifb0e427a7ad0b3abe9441cda3dd96790b5cbbe1c

--- Server/run-server.sh
+++ Server/run-server.sh
@@ -5,5 +5,5 @@ exec python -c 'import sys, os.path; \
                 sys.path[0] = os.path.abspath(sys.path[0]); \
                 __requires__ = ["CherryPy < 3.0"]; import pkg_resources; \
                 from gunicorn.app.wsgiapp import run; run()' \
-               --bind :8080 --workers 8 --access-logfile - \
+               --bind :8080 -t 3600 --access-logfile - \
                --preload bkr.server.wsgi:application
