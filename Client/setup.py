acb41b6c7c83700a72a5c9aee39dcb2e57e0ead6
Raymond Mancy <rmancy@redhat.com>
2013-10-31 11:51:59 +1000

    Automatic commit of release 0.14.2rc2
    Change-Id: Ie5add1669691f18215233700409e78fbc1682f08

--- Client/setup.py
+++ Client/setup.py
@@ -13,7 +13,7 @@ def bash_completion_dir():
 
 setup(
     name = "bkr.client",
-    version='0.14.2rc1',
+    version='0.14.2rc2',
     license = "GPLv2+",
 
     packages=find_packages('src'),
--- Common/setup.py
+++ Common/setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "bkr",
-    version='0.14.2rc1',
+    version='0.14.2rc2',
     license = "GPLv2+",
 
     packages=find_packages('.'),
