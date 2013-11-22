3b1960572abedd37cd1711d32e83c263801bd1d7
Dan Callaghan <dcallagh@redhat.com>
2013-11-22 17:29:15 +1000

    tasks have moved to separate git repos
    The following tasks which are essential to Beaker's operation have been
    moved to beaker-core-tasks:
    
        http://git.beaker-project.org/cgit/beaker-core-tasks/
    
        /distribution/install
        /distribution/inventory
        /distribution/reservesys
        /distribution/updateDistro
        /distribution/virt/install
        /distribution/virt/start
    
    The following tasks for testing Beaker itself have been moved to
    beaker-meta-tasks:
    
        http://git.beaker-project.org/cgit/beaker-meta-tasks/
    
        /distribution/beaker/Sanity/bz880440-fqdn-self-reporting
        /distribution/beaker/Sanity/sync-set_block-tests
        /distribution/beaker/custom_kickstart
        /distribution/beaker/dogfood
        /distribution/beaker/fake_systems
        /dsitribution/beaker/import
        /dsitribution/beaker/setup
    
    Change-Id: I18ec2a2debe01058ceecdd0a58c2cd0b1ffdda7c

--- Tasks/distribution/beaker/fake_systems/hosts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-: ${TEST_SYSTEM_COUNT:=0}
-: ${TEST_SYSTEM_PREFIX:=test-}
-
-seq $TEST_SYSTEM_COUNT | while read n ; do
-    echo "127.1.$(($n / 256)).$(($n % 256))  $TEST_SYSTEM_PREFIX$n.invalid"
-done
