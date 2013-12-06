091f5cded13463c20737b0a9381e547ebdf7393b
Dan Callaghan <dcallagh@redhat.com>
2013-12-06 17:25:46 +1000

    split out database type definitions
    Change-Id: Ifdb5d1b6855733b0edb903ecefd47e2f20599120

--- Server/bkr/server/model/__init__.py
+++ Server/bkr/server/model/__init__.py
@@ -19,7 +19,6 @@ from sqlalchemy.orm.util import has_identity
 from sqlalchemy.sql import exists, union, literal
 from sqlalchemy.exc import InvalidRequestError, OperationalError
 from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.types import TypeDecorator, BINARY
 from bkr.server.installopts import InstallOptions, global_install_options
 from sqlalchemy.orm.collections import attribute_mapped_collection
 from sqlalchemy.ext.associationproxy import association_proxy
@@ -30,7 +29,6 @@ from bkr.server.bexceptions import BeakerException, BX, \
         VMCreationFailedException, StaleTaskStatusException, \
         InsufficientSystemPermissions, StaleSystemUserException, \
         StaleCommandStatusException, NoChangeException
-from bkr.server.enum import DeclEnum
 from bkr.server.hybrid import hybrid_property, hybrid_method
 from bkr.server.helpers import make_link, make_fake_link
 from bkr.server.util import unicode_truncate, absolute_url, run_createrepo
@@ -63,179 +61,13 @@ from bkr.common.helpers import (AtomicFileReplacement, Flock,
 
 from .sql import ConditionalInsert
 from .base import DeclBase, MappedObject, SystemObject
+from .types import (TaskStatus, CommandStatus, TaskResult, TaskPriority,
+        SystemStatus, SystemType, ReleaseAction, ImageType, ResourceType,
+        RecipeVirtStatus, SystemPermission, UUID, MACAddress,
+        mac_unix_padded_dialect)
 
 import logging
 log = logging.getLogger(__name__)
-
-
-class TaskStatus(DeclEnum):
-
-    symbols = [
-        ('new',       u'New',       dict(severity=10, finished=False, queued=True)),
-        ('processed', u'Processed', dict(severity=20, finished=False, queued=True)),
-        ('queued',    u'Queued',    dict(severity=30, finished=False, queued=True)),
-        ('scheduled', u'Scheduled', dict(severity=40, finished=False, queued=True)),
-        # RUNNING and WAITING are transient states.  It will never be final.
-        #  But having it the lowest Severity will show a job as 
-        #  Running until it finishes with either Completed, Cancelled or 
-        #  Aborted.
-        ('waiting',   u'Waiting',   dict(severity=7, finished=False, queued=False)),
-        ('running',   u'Running',   dict(severity=5, finished=False, queued=False)),
-        ('completed', u'Completed', dict(severity=50, finished=True, queued=False)),
-        ('cancelled', u'Cancelled', dict(severity=60, finished=True, queued=False)),
-        ('aborted',   u'Aborted',   dict(severity=70, finished=True, queued=False)),
-    ]
-
-    @classmethod
-    def max(cls):
-        return max(cls, key=lambda s: s.severity)
-
-class CommandStatus(DeclEnum):
-
-    symbols = [
-        ('queued',    u'Queued',    dict()),
-        ('running',   u'Running',   dict()),
-        ('completed', u'Completed', dict()),
-        ('failed',    u'Failed',    dict()),
-        ('aborted',   u'Aborted',   dict()),
-    ]
-
-class TaskResult(DeclEnum):
-
-    symbols = [
-        ('new',   u'New',   dict(severity=10)),
-        ('pass_', u'Pass',  dict(severity=20)),
-        ('warn',  u'Warn',  dict(severity=30)),
-        ('fail',  u'Fail',  dict(severity=40)),
-        ('panic', u'Panic', dict(severity=50)),
-        ('none',  u'None',  dict(severity=15)),
-    ]
-
-    @classmethod
-    def min(cls):
-        return min(cls, key=lambda r: r.severity)
-
-class TaskPriority(DeclEnum):
-
-    symbols = [
-        ('low',    u'Low',    dict()),
-        ('medium', u'Medium', dict()),
-        ('normal', u'Normal', dict()),
-        ('high',   u'High',   dict()),
-        ('urgent', u'Urgent', dict()),
-    ]
-
-    @classmethod
-    def default_priority(cls):
-        return cls.normal
-
-class SystemStatus(DeclEnum):
-
-    # Changing a system from a "bad" status to a "good" status will cause its 
-    # status_reason to be cleared, see 
-    # bkr.server.controller_utilities._SystemSaveFormHandler
-
-    symbols = [
-        ('automated', u'Automated', dict(bad=False)),
-        ('broken',    u'Broken',    dict(bad=True)),
-        ('manual',    u'Manual',    dict(bad=False)),
-        ('removed',   u'Removed',   dict(bad=True)),
-    ]
-
-class SystemType(DeclEnum):
-
-    symbols = [
-        ('laptop',    u'Laptop',    dict()),
-        ('machine',   u'Machine',   dict()),
-        ('prototype', u'Prototype', dict()),
-        ('resource',  u'Resource',  dict()),
-    ]
-
-
-class ReleaseAction(DeclEnum):
-
-    symbols = [
-        ('power_off',   u'PowerOff',    dict()),
-        ('leave_on',    u'LeaveOn',     dict()),
-        ('reprovision', u'ReProvision', dict()),
-    ]
-
-class ImageType(DeclEnum):
-
-    symbols = [
-        ('kernel', u'kernel', dict()),
-        ('initrd', u'initrd', dict()),
-        ('live', u'live', dict()),
-        ('uimage', u'uimage', dict()),
-        ('uinitrd', u'uinitrd', dict())
-    ]
-
-class ResourceType(DeclEnum):
-    """Type discriminator for RecipeResource classes."""
-    symbols = [
-        ('system', u'system', dict()),
-        ('virt',   u'virt',   dict()),
-        ('guest',  u'guest',  dict()),
-    ]
-
-class RecipeVirtStatus(DeclEnum):
-
-    symbols = [
-        ('possible',    u'Possible',    dict()),
-        ('precluded',   u'Precluded',   dict()),
-        ('succeeded',   u'Succeeded',   dict()),
-        ('skipped',     u'Skipped',     dict()),
-        ('failed',      u'Failed',      dict()),
-    ]
-
-class UUID(TypeDecorator):
-    """
-    Database type for storing UUIDs as BINARY(16).
-    """
-    impl = BINARY
-
-    def __init__(self):
-        super(UUID, self).__init__(length=16)
-
-    def process_bind_param(self, value, dialect):
-        if value is None:
-            return value
-        if isinstance(value, uuid.UUID):
-            return value.bytes
-        raise TypeError('Expected UUID but got %r' % value)
-
-    def process_result_value(self, value, dialect):
-        if value is None:
-            return value
-        return uuid.UUID(bytes=value)
-
-    def is_mutable(self):
-        return False
-
-# A netaddr "dialect" for formatting MAC addresses... this is the most common 
-# format, and is expected by virt-install, so I'm not sure why netaddr doesn't 
-# ship with it...
-class _mac_unix(netaddr.mac_unix):
-    word_fmt = '%02x'
-
-class MACAddress(TypeDecorator):
-    """
-    Database type for MAC (EUI) addresses. Stores them as raw integers, which 
-    lets us do arithmetic on them in the database.
-    """
-    impl = BigInteger
-
-    def process_bind_param(self, value, dialect):
-        if value is None:
-            return value
-        if isinstance(value, netaddr.EUI):
-            return int(value)
-        return int(netaddr.EUI(value))
-
-    def process_result_value(self, value, dialect):
-        if value is None:
-            return value
-        return netaddr.EUI(value, dialect=_mac_unix)
 
 xmldoc = xml.dom.minidom.Document()
 
@@ -3058,17 +2890,6 @@ class SystemAccessPolicy(DeclBase, MappedObject):
                 user_id=user.user_id if user else None,
                 group_id=group.group_id if group else None))
         return self.rules[-1]
-
-class SystemPermission(DeclEnum):
-
-    symbols = [
-        ('edit_policy',    u'edit_policy',    dict(label=_(u'Edit this policy'))),
-        ('edit_system',    u'edit_system',    dict(label=_(u'Edit system details'))),
-        ('loan_any',       u'loan_any',       dict(label=_(u'Loan to anyone'))),
-        ('loan_self',      u'loan_self',      dict(label=_(u'Loan to self'))),
-        ('control_system', u'control_system', dict(label=_(u'Control power'))),
-        ('reserve',        u'reserve',        dict(label=_(u'Reserve'))),
-    ]
 
 class SystemAccessPolicyRule(DeclBase, MappedObject):
 
@@ -6600,7 +6421,7 @@ class RecipeResource(MappedObject):
                 .order_by(left_side.c.mac_address).limit(1))
         # The type of (left_side.c.mac_address + 1) comes out as Integer
         # instead of MACAddress, I think it's a sqlalchemy bug :-(
-        return netaddr.EUI(free_addr, dialect=_mac_unix)
+        return netaddr.EUI(free_addr, dialect=mac_unix_padded_dialect)
 
 class SystemResource(RecipeResource):
     """
@@ -6621,7 +6442,7 @@ class SystemResource(RecipeResource):
     def mac_address(self):
         # XXX the type of system.mac_address should be changed to MACAddress,
         # but for now it's not
-        return netaddr.EUI(self.system.mac_address, dialect=_mac_unix)
+        return netaddr.EUI(self.system.mac_address, dialect=mac_unix_padded_dialect)
 
     @property
     def link(self):
---
+++ Server/bkr/server/model/types.py
@@ -0,0 +1,189 @@
+
+"""
+Database type definitions.
+"""
+
+import uuid
+import netaddr
+from sqlalchemy.types import TypeDecorator, BINARY, BigInteger
+from bkr.server.enum import DeclEnum
+
+class TaskStatus(DeclEnum):
+
+    symbols = [
+        ('new',       u'New',       dict(severity=10, finished=False, queued=True)),
+        ('processed', u'Processed', dict(severity=20, finished=False, queued=True)),
+        ('queued',    u'Queued',    dict(severity=30, finished=False, queued=True)),
+        ('scheduled', u'Scheduled', dict(severity=40, finished=False, queued=True)),
+        # RUNNING and WAITING are transient states.  It will never be final.
+        #  But having it the lowest Severity will show a job as 
+        #  Running until it finishes with either Completed, Cancelled or 
+        #  Aborted.
+        ('waiting',   u'Waiting',   dict(severity=7, finished=False, queued=False)),
+        ('running',   u'Running',   dict(severity=5, finished=False, queued=False)),
+        ('completed', u'Completed', dict(severity=50, finished=True, queued=False)),
+        ('cancelled', u'Cancelled', dict(severity=60, finished=True, queued=False)),
+        ('aborted',   u'Aborted',   dict(severity=70, finished=True, queued=False)),
+    ]
+
+    @classmethod
+    def max(cls):
+        return max(cls, key=lambda s: s.severity)
+
+class CommandStatus(DeclEnum):
+
+    symbols = [
+        ('queued',    u'Queued',    dict()),
+        ('running',   u'Running',   dict()),
+        ('completed', u'Completed', dict()),
+        ('failed',    u'Failed',    dict()),
+        ('aborted',   u'Aborted',   dict()),
+    ]
+
+class TaskResult(DeclEnum):
+
+    symbols = [
+        ('new',   u'New',   dict(severity=10)),
+        ('pass_', u'Pass',  dict(severity=20)),
+        ('warn',  u'Warn',  dict(severity=30)),
+        ('fail',  u'Fail',  dict(severity=40)),
+        ('panic', u'Panic', dict(severity=50)),
+        ('none',  u'None',  dict(severity=15)),
+    ]
+
+    @classmethod
+    def min(cls):
+        return min(cls, key=lambda r: r.severity)
+
+class TaskPriority(DeclEnum):
+
+    symbols = [
+        ('low',    u'Low',    dict()),
+        ('medium', u'Medium', dict()),
+        ('normal', u'Normal', dict()),
+        ('high',   u'High',   dict()),
+        ('urgent', u'Urgent', dict()),
+    ]
+
+    @classmethod
+    def default_priority(cls):
+        return cls.normal
+
+class SystemStatus(DeclEnum):
+
+    # Changing a system from a "bad" status to a "good" status will cause its 
+    # status_reason to be cleared, see 
+    # bkr.server.controller_utilities._SystemSaveFormHandler
+
+    symbols = [
+        ('automated', u'Automated', dict(bad=False)),
+        ('broken',    u'Broken',    dict(bad=True)),
+        ('manual',    u'Manual',    dict(bad=False)),
+        ('removed',   u'Removed',   dict(bad=True)),
+    ]
+
+class SystemType(DeclEnum):
+
+    symbols = [
+        ('laptop',    u'Laptop',    dict()),
+        ('machine',   u'Machine',   dict()),
+        ('prototype', u'Prototype', dict()),
+        ('resource',  u'Resource',  dict()),
+    ]
+
+
+class ReleaseAction(DeclEnum):
+
+    symbols = [
+        ('power_off',   u'PowerOff',    dict()),
+        ('leave_on',    u'LeaveOn',     dict()),
+        ('reprovision', u'ReProvision', dict()),
+    ]
+
+class ImageType(DeclEnum):
+
+    symbols = [
+        ('kernel', u'kernel', dict()),
+        ('initrd', u'initrd', dict()),
+        ('live', u'live', dict()),
+        ('uimage', u'uimage', dict()),
+        ('uinitrd', u'uinitrd', dict())
+    ]
+
+class ResourceType(DeclEnum):
+    """Type discriminator for RecipeResource classes."""
+    symbols = [
+        ('system', u'system', dict()),
+        ('virt',   u'virt',   dict()),
+        ('guest',  u'guest',  dict()),
+    ]
+
+class RecipeVirtStatus(DeclEnum):
+
+    symbols = [
+        ('possible',    u'Possible',    dict()),
+        ('precluded',   u'Precluded',   dict()),
+        ('succeeded',   u'Succeeded',   dict()),
+        ('skipped',     u'Skipped',     dict()),
+        ('failed',      u'Failed',      dict()),
+    ]
+
+class SystemPermission(DeclEnum):
+
+    symbols = [
+        ('edit_policy',    u'edit_policy',    dict(label=_(u'Edit this policy'))),
+        ('edit_system',    u'edit_system',    dict(label=_(u'Edit system details'))),
+        ('loan_any',       u'loan_any',       dict(label=_(u'Loan to anyone'))),
+        ('loan_self',      u'loan_self',      dict(label=_(u'Loan to self'))),
+        ('control_system', u'control_system', dict(label=_(u'Control power'))),
+        ('reserve',        u'reserve',        dict(label=_(u'Reserve'))),
+    ]
+
+class UUID(TypeDecorator):
+    """
+    Database type for storing UUIDs as BINARY(16).
+    """
+    impl = BINARY
+
+    def __init__(self):
+        super(UUID, self).__init__(length=16)
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+        if isinstance(value, uuid.UUID):
+            return value.bytes
+        raise TypeError('Expected UUID but got %r' % value)
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return value
+        return uuid.UUID(bytes=value)
+
+    def is_mutable(self):
+        return False
+
+# A netaddr "dialect" for formatting MAC addresses... this is the most common 
+# format, and is expected by virt-install, so I'm not sure why netaddr doesn't 
+# ship with it...
+class mac_unix_padded_dialect(netaddr.mac_unix):
+    word_fmt = '%02x'
+
+class MACAddress(TypeDecorator):
+    """
+    Database type for MAC (EUI) addresses. Stores them as raw integers, which 
+    lets us do arithmetic on them in the database.
+    """
+    impl = BigInteger
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+        if isinstance(value, netaddr.EUI):
+            return int(value)
+        return int(netaddr.EUI(value))
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return value
+        return netaddr.EUI(value, dialect=mac_unix_padded_dialect)
