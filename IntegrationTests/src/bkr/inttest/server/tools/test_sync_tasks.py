beaker-project/beaker

commit 06cc9f7416d6b685fd9ea2cc95735fc50a09ed2d

beaker-sync-tasks: Do not use kobo, minor cleanups
- The features of Kobo for which it was originally used are no longer
  required, and hence using 'xmlrpclib' serves the purpose

- Minor cleanups

Bug: 1040226
Change-Id: Iefac836556c4d4f188189611dcdcd51f0441a915

Server/bkr/server/tools/sync_tasks.py
@@ -81,14 +81,7 @@ from bkr.common.helpers import atomically_replaced_file, unlink_ignore, siphon, 
     Flock
 from bkr.server.model import TaskLibrary, Task
 from bkr.server.util import load_config
-
-# We need kobo
-try:
-    import kobo.xmlrpc
-    from kobo.client import HubProxy
-except ImportError:
-    print 'Please install kobo client library'
-    sys.exit(1)
+import xmlrpclib
 
 __description__ = 'Script to sync local task RPMs from a remote Beaker instance'
 __version__ = '0.1'
@@ -114,10 +107,7 @@ class TaskLibrarySync:
 
         # Initialize core attributes
         if remote:
-            self.remote = remote.rstrip("/")
-            remote_proxy = self._get_server_proxy(self.remote)
-            self.proxy={'remote':remote_proxy,
-                        }
+            self.proxy = xmlrpclib.ServerProxy(remote.rstrip("/"))
 
         self.tasks_added = []
         self.t_downloaded = 0
@@ -140,14 +130,6 @@ class TaskLibrarySync:
             self.logger.critical('You should run this script as user: %s' % pwd.getpwuid(task_dir_uid).pw_name)
             sys.exit(-1)
 
-    def _get_server_proxy(self, server):
-
-        kobo_conf = {}
-        kobo_conf['HUB_URL'] = server
-        hub = HubProxy(kobo_conf)
-
-        return hub
-
     def get_tasks(self, server):
 
         # if local, directly read the database
@@ -155,7 +137,7 @@ class TaskLibrarySync:
             tasks = Task.query.filter(Task.valid == True).all()
             tasks = [task.to_dict() for task in tasks]
         else:
-            tasks = self.proxy[server].tasks.filter({'valid':1})
+            tasks = self.proxy.tasks.filter({'valid':1})
 
         return [task['name'] for task in tasks]
 
@@ -172,7 +154,7 @@ class TaskLibrarySync:
 
         try:
             self.logger.debug('Getting task XML for %s from %s' % (task, getattr(self, server)))
-            return self.proxy[server].tasks.to_xml(task, False)
+            return self.proxy.tasks.to_xml(task, False)
         except (xmlrpclib.Fault, xmlrpclib.ProtocolError) as e:
             # If something goes wrong with this task, for example:
             # https://bugzilla.redhat.com/show_bug.cgi?id=915549

commit 134781e18f0492aeb6eb5111583853e5c7853962

beaker-sync-tasks: Fix a regression introduced in 06cc9f7
Also adds a test to test the script logs (output).

Bug: 1040226
Change-Id: I18eb97606bae1e2700e26648886b9cdd778e35db

Server/bkr/server/tools/sync_tasks.py
@@ -72,16 +72,13 @@ import lxml.etree as ET
 import logging
 import urllib2
 from optparse import OptionParser
-from shutil import rmtree
 
 import turbogears.config
 from turbogears.database import session
 
-from bkr.common.helpers import atomically_replaced_file, unlink_ignore, siphon, \
-    Flock
+from bkr.common.helpers import siphon
 from bkr.server.model import TaskLibrary, Task
 from bkr.server.util import load_config
-import xmlrpclib
 
 __description__ = 'Script to sync local task RPMs from a remote Beaker instance'
 __version__ = '0.1'
@@ -101,19 +98,22 @@ class TaskLibrarySync:
 
     def __init__(self, remote=None):
 
+        # load configuration data
+        load_config()
+
         # setup, sanity checks
         self.task_dir = turbogears.config.get("basepath.rpms", "/var/www/beaker/rpms")
+
         self._setup_logging()
 
         # Initialize core attributes
         if remote:
-            self.proxy = xmlrpclib.ServerProxy(remote.rstrip("/"))
+            self.remote = remote.rstrip("/")
+            self.proxy = xmlrpclib.ServerProxy(self.remote)
 
         self.tasks_added = []
         self.t_downloaded = 0
         self.tasklib = TaskLibrary()
-        # load configuration data
-        load_config()
 
     def _setup_logging(self):
         formatter = logging.Formatter('%(asctime)s - %(message)s')
@@ -233,7 +233,7 @@ class TaskLibrarySync:
         try:
             self.sync_tasks(tasks_to_sync)
         except Exception, e:
-            log.exception(unicode(e))
+            self.logger.exception(unicode(e))
             self.logger.info('Failed to sync all tasks')
         else:
             self.logger.info('Synced all tasks')
@@ -292,7 +292,8 @@ def main():
     if not options.force:
         if len(old_tasks)>0:
             task_sync.logger.warning('Warning: %d tasks already present may be overwritten '
-                            'with the version from %s if the two versions are different' % (len(old_tasks), task_sync.remote))
+                                     'with the version from %s if the two versions are different' %
+                                     (len(old_tasks), task_sync.remote))
 
         proceed = raw_input('Proceed with task addition? (y/n) ')
         if proceed.lower() == 'y':

IntegrationTests/src/bkr/inttest/server/tools/test_sync_tasks.py
@@ -1,7 +1,6 @@
 import unittest2 as unittest
 import os
-import shutil
-import tempfile
+import subprocess
 from bkr.inttest import Process
 from turbogears.database import session
 from bkr.server.tools.sync_tasks import TaskLibrarySync
@@ -9,10 +8,10 @@ from bkr.server.model import Task
 from bkr.inttest import get_server_base
 import pkg_resources
 
+_beaker_sync_tasks = pkg_resources.resource_filename('bkr.server.tools', 'sync_tasks.py')
 _sync_tasks_dir = pkg_resources.resource_filename('bkr.inttest.server.tools', 'task_rpms')
 _http_server = pkg_resources.resource_filename('bkr.inttest', 'http_server.py')
 _current_dir = os.path.dirname(__file__)
-
 class TestTaskLibrarySync(unittest.TestCase):
 
     @classmethod
@@ -26,6 +25,36 @@ class TestTaskLibrarySync(unittest.TestCase):
     @classmethod
     def teardownClass(cls):
         cls.task_server.stop()
+
+    def run_as_script(self, remote=None, debug=True, force=True):
+        if not remote:
+            remote = get_server_base()
+        script_invocation = [_beaker_sync_tasks, '--remote', remote]
+        if debug:
+            script_invocation.append('--debug')
+        if force:
+            script_invocation.append('--force')
+        p = subprocess.Popen(script_invocation,
+                             stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        # sending 'y' is not required with --force, but
+        # saves a new if..else block
+        out, err = p.communicate('y')
+        self.assertEquals('', err)
+
+        return out
+
+    def assertRegexpMatchesIn(self, text, items):
+        for item in items:
+            try:
+                self.assertRegexpMatches(item, text)
+            except AssertionError:
+                continue
+            else:
+                return True
+
+        return False
 
     def test_sync_two_tasks(self):
         task_sync = TaskLibrarySync()
@@ -101,3 +130,28 @@ class TestTaskLibrarySync(unittest.TestCase):
 
 
         self.assertTrue(os.path.exists(task_rpm_disk))
+
+    def test_script_run_log(self):
+
+        remote = get_server_base()
+        out = self.run_as_script(remote).splitlines()
+
+        # Rough, but at least allows us to exercise the code path
+        # we get the tasks from the local DB only which is not exactly how
+        # it should be done - ideally we would be retrieving this task
+        # list from both local DB and the remote server
+        # However, here the remote server does not have a task which
+        # is not present locally (vice-versa is not true), so it works for
+        # us here
+        with session.begin():
+            tasks = Task.query.filter(Task.valid == True).all()
+        for task in tasks:
+            self.assertRegexpMatchesIn('Getting task XML for %s from %s' %
+                               (task.name, remote.rstrip('/')), out)
+            self.assertRegexpMatchesIn('Getting task XML for %s from local database' %
+                                       task.name, out)
+
+        # https://bugzilla.redhat.com/show_bug.cgi?id=1040226#c8
+        out = self.run_as_script(remote, force=False).splitlines()
+        self.assertRegexpMatchesIn('tasks already present may be overwritten' 
+                                   'with the version from %s' % remote, out)