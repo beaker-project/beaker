ab52e621cb8d4ba786ad0f36a3862b33225e985f
Dan Callaghan <dcallagh@redhat.com>
2014-01-20 14:17:02 +1000

    reduce spacing in read-only view of system form
    Buttons which live inside the system form are reduced to btn-mini which
    is as small as Bootstrap allows. Read-only values are shown as plain
    text instead of uneditable inputs. Also muted the label colour to avoid
    the wall-of-text feeling now that everything is just unadorned text.
    
    Bug: 1052043
    Change-Id: Id5144b1e845e86974f6999cc934a9b94f8be7326

--- Server/bkr/server/widgets.py
+++ Server/bkr/server/widgets.py
@@ -1434,7 +1434,7 @@ class SystemForm(Form):
         else:
             property_value = getattr(value, item, None)
 
-        span = Element('span', {'class': 'uneditable-input'})
+        span = Element('span', {'class': 'form-control-static'})
         if property_value is not None:
             span.text = unicode(property_value)
         return span
