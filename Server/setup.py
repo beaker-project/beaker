672ad5b96e97c00b97bd8d45409a70c1dcf9fa6e
Dan Callaghan <dcallagh@redhat.com>
2014-03-14 16:50:27 +1000

    Automatic commit of release 0.16.0

--- Server/setup.py
+++ Server/setup.py
@@ -221,7 +221,7 @@ else:
 setup(
     name="bkr.server",
     namespace_packages = ['bkr'],
-    version='0.16.0rc1',
+    version='0.16.0',
     # uncomment the following lines if you fill them out in release.py
     description=description,
     author=author,
