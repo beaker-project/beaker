9acc175aaf3aa0c020ad1d623f99f0c126912793
Dan Callaghan <dcallagh@redhat.com>
2014-10-17 11:32:01 +1000

    Automatic commit of release 0.18.3

--- IntegrationTests/setup.py
+++ IntegrationTests/setup.py
@@ -12,7 +12,7 @@ def get_compose_layout():
 
 setup(
     name='bkr.inttest',
-    version='0.18.2',
+    version='0.18.3',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     package_data={'': [
