e107313a9c16943dbcb8ed6f015c926d128ebe17
Dan Callaghan <dcallagh@redhat.com>
2014-11-13 11:52:52 +1000

    combine db migration 1c10e575800c into 3fc28d7eb6d3
    ... to avoid two sets of ALTER TABLE statements on the log_* tables.
    
    Bug: 1163540
    Change-Id: Ieec8014ab397206c649eef12271ecbdc4b2f2385

--- Server/bkr/server/alembic/versions/3fc28d7eb6d3_fix_nullability_and_fks.py
+++ Server/bkr/server/alembic/versions/3fc28d7eb6d3_fix_nullability_and_fks.py
@@ -3,7 +3,7 @@
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 
-"""Fix NULLability on a number of old columns
+"""Fix NULLability and FKs on a number of old columns
 
 Revision ID: 3fc28d7eb6d3
 Revises: 2d4258bf3f16
@@ -17,6 +17,7 @@ down_revision = '2d4258bf3f16'
 
 from alembic import op
 import sqlalchemy as sa
+from bkr.server.alembic.migration_utils import find_fk, create_fk_if_absent
 
 def upgrade():
     # This was an oversight in commit d8df7f836 for bug 598781.
@@ -33,13 +34,44 @@ def upgrade():
     # This was an oversight in commit 747902274 for bug 596410.
     op.alter_column('recipe_set_nacked', 'created',
             existing_type=sa.DateTime, nullable=False)
-    # This was an oversight in commit dbf2a15de for bug 751949.
-    op.alter_column('log_recipe', 'recipe_id',
-            existing_type=sa.Integer, nullable=False)
-    op.alter_column('log_recipe_task', 'recipe_task_id',
-            existing_type=sa.Integer, nullable=False)
-    op.alter_column('log_recipe_task_result', 'recipe_task_result_id',
-            existing_type=sa.Integer, nullable=False)
+    # The log_* tables unfortunately require special treatment. They are so big 
+    # that we need to avoid running multiple ALTER TABLEs on them in a single 
+    # upgrade, because each ALTER TABLE is so expensive (sometimes hours!)
+    # The missing nullability was an oversight in commit dbf2a15de for bug 751949.
+    # The FKs may be missing on very old databases, dating back to when we were 
+    # accidentally creating tables in MyISAM instead of InnoDB. However the FKs 
+    # will exist on more recently created databases, so we need two different 
+    # ALTER TABLE statements depending on whether we want to add the FKs ...
+    if find_fk('log_recipe', ['recipe_id']) is None:
+        op.execute("""
+            ALTER TABLE log_recipe
+            MODIFY recipe_id INT NOT NULL,
+            ADD CONSTRAINT fk_log_recipe_recipe_id
+                FOREIGN KEY (recipe_id) REFERENCES recipe (id)
+            """)
+    else:
+        op.alter_column('log_recipe', 'recipe_id',
+                existing_type=sa.Integer, nullable=False)
+    if find_fk('log_recipe_task', ['recipe_task_id']) is None:
+        op.execute("""
+            ALTER TABLE log_recipe_task
+            MODIFY recipe_task_id INT NOT NULL,
+            ADD CONSTRAINT fk_log_recipe_task_recipe_task_id
+                FOREIGN KEY (recipe_task_id) REFERENCES recipe_task (id)
+            """)
+    else:
+        op.alter_column('log_recipe_task', 'recipe_task_id',
+                existing_type=sa.Integer, nullable=False)
+    if find_fk('log_recipe_task_result', ['recipe_task_result_id']) is None:
+        op.execute("""
+            ALTER TABLE log_recipe_task_result
+            MODIFY recipe_task_result_id INT NOT NULL,
+            ADD CONSTRAINT fk_log_recipe_task_result_recipe_task_result_id
+                FOREIGN KEY (recipe_task_result_id) REFERENCES recipe_task_result (id)
+            """)
+    else:
+        op.alter_column('log_recipe_task_result', 'recipe_task_result_id',
+                existing_type=sa.Integer, nullable=False)
     # This was an oversight in commit e591d682 for bug 595801.
     op.alter_column('job', 'retention_tag_id',
             existing_type=sa.Integer, nullable=False)
@@ -49,6 +81,10 @@ def upgrade():
     # This was an oversight in commit 1ec1b4f1 for bug 703885.
     op.alter_column('tg_user', 'disabled',
             existing_type=sa.Boolean, nullable=False)
+    # As above, this FK may be missing on very old databases due to MyISAM.
+    create_fk_if_absent(
+            'task_property_needed', 'task',
+            ['task_id'], ['id'])
 
 def downgrade():
     op.alter_column('recipe', 'autopick_random',
