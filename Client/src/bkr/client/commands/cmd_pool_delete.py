c10ce28c140cf866139465c2e266fc8c17f8da23
Amit Saha <asaha@redhat.com>
2015-03-18 17:14:31 +1000

    CLI for System pool deletion
    Change-Id: I0bc0ea6e4410169fe808064b0ca35a01d70010b3

---
+++ Client/src/bkr/client/commands/cmd_pool_delete.py
@@ -0,0 +1,70 @@
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+"""
+.. _bkr-pool-delete:
+
+bkr pool-delete: Delete a system pool
+=====================================
+
+.. program:: bkr pool-delete
+
+Synopsis
+--------
+
+| :program:`bkr pool-delete` <poolname> ..
+
+Description
+-----------
+
+Deletes a Beaker system pool.
+
+(Note: this command requires Python 2.6 or later)
+
+.. versionadded:: 20
+
+Common :program:`bkr` options are described in the :ref:`Options
+<common-options>` section of :manpage:`bkr(1)`.
+
+Exit status
+-----------
+
+Non-zero on error, otherwise zero.
+
+Examples
+--------
+
+Delete an existing system pool called "beakerdevs"::
+
+    bkr pool-delete beakerdevs
+
+See also
+--------
+
+:manpage:`bkr(1)`, :manpage:`bkr pool-create`, :manpage:`bkr pool-modify`
+
+"""
+
+
+from bkr.client import BeakerCommand
+import urllib
+
+class Pool_Delete(BeakerCommand):
+    """Deletes a system pool"""
+    enabled = True
+
+    def options(self):
+        self.parser.usage = "%%prog %s <poolname>" % self.normalized_name
+
+    def run(self, *args, **kwargs):
+
+        if not args:
+            self.parser.error('One or more system pool names must be specified')
+        self.set_hub(**kwargs)
+        requests_session = self.requests_session()
+        for pool in args:
+            res = requests_session.delete('pools/%s/' % urllib.quote(pool, ''))
+            res.raise_for_status()
---
+++ IntegrationTests/src/bkr/inttest/client/test_pool_delete.py
@@ -0,0 +1,30 @@
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+from bkr.server.model import session, SystemPool
+from bkr.inttest import data_setup
+from bkr.inttest.client import ClientError, run_client, ClientTestCase
+from sqlalchemy.orm.exc import NoResultFound
+class DeleteSystemPool(ClientTestCase):
+
+    def test_delete_pool(self):
+        with session.begin():
+            pool_name = data_setup.unique_name('mypool%s')
+            data_setup.create_system_pool(name=pool_name)
+        run_client(['bkr', 'pool-delete', pool_name])
+
+        with session.begin():
+            session.expire_all()
+            with self.assertRaises(NoResultFound):
+                SystemPool.by_name(pool_name)
+
+        # attempt to delete non-existent pool
+        try:
+            run_client(['bkr', 'pool-delete', pool_name])
+            self.fail()
+        except ClientError as e:
+            self.assertIn('System pool %s does not exist' % pool_name,
+                          e.stderr_output)
