beaker-project/beaker

commit 2a03834583141109ab524ec5205dcf57d807692c

system search bar: handle invalid date
For the search fields which are using jQuery UI Datepicker, if a user
manually types an invalid date, it will be either handled by the date picker
or HTML5 form validation.The date picker will restore the value to default
current date. Also, if the user puts in a date which can be interpreted,
such as "15-05-01", it will reformat the date as "2015-05-01".

Bug: 1182545
Change-Id: I05dceca62a9f1bb5e8fe6ace7cd8f5b14ff444bb

Server/bkr/server/widgets.py
@@ -651,7 +651,7 @@ class SearchBar(RepeatingFormField):
     """Search Bar""" 
     css = [LocalCSSLink('bkr', '/static/css/smoothness/jquery-ui.css')]
     javascript = [LocalJSLink('bkr', '/static/javascript/search_object.js'),
-                  LocalJSLink('bkr', '/static/javascript/searchbar_v9.js'),
+                  LocalJSLink('bkr', '/static/javascript/searchbar_v10.js'),
                   LocalJSLink('bkr','/static/javascript/jquery-ui-1.9.2.min.js', order=3),]
     template = "bkr.server.templates.search_bar"
 

IntegrationTests/src/bkr/inttest/server/selenium/test_system_search.py
@@ -5,6 +5,7 @@
 # (at your option) any later version.
 
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
 from bkr.server.model import Numa, User, Key, Key_Value_String, Key_Value_Int, \
     Device, DeviceClass, Disk, Cpu, SystemPermission
 from bkr.inttest.server.selenium import WebDriverTestCase
@@ -514,11 +515,26 @@ class Search(WebDriverTestCase):
         b.get(get_server_base())
         b.find_element_by_link_text('Show Search Options').click()
         wait_for_animation(b, '#searchform')
+        # test for using invalid date
+        Select(b.find_element_by_name('systemsearch-0.table'))\
+            .select_by_visible_text('System/Added')
+        Select(b.find_element_by_name('systemsearch-0.operation'))\
+            .select_by_visible_text('after')
+        search_field = b.find_element_by_name('systemsearch-0.value')
+        search_field.click()
+        # close the date picker
+        search_field.clear()
+        search_field.send_keys(Keys.ESCAPE)
+        search_field.clear()
+        search_field.send_keys('02-02-2002')
+        # we can't actually check the HTML5 validation error
+        b.find_element_by_css_selector('input[name="systemsearch-0.value"]:invalid')
+
         Select(b.find_element_by_name('systemsearch-0.table'))\
             .select_by_visible_text('System/Added')
         Select(b.find_element_by_name('systemsearch-0.operation'))\
             .select_by_visible_text('before')
-        b.find_element_by_name('systemsearch-0.value').click()
+        search_field.click()
         date_picker = b.find_element_by_id('ui-datepicker-div')
         date_picker.find_element_by_class_name('ui-state-highlight').click()
         b.find_element_by_id('searchform').submit()

Server/bkr/server/static/javascript/searchbar_v10.js
@@ -203,7 +203,8 @@ SearchBar.prototype.createValueField = function(current, vals) {
                 clone_attrs['class'].push('datepicker')
             }
         }
-
+	clone_attrs['pattern'] = '\\d\\d\\d\\d-\\d\\d-\\d\\d';
+        clone_attrs['title'] = 'date in YYYY-MM-DD format';
     }
 
     if(vals) {//set up drop down menu

Server/bkr/server/templates/search_bar.kid
@@ -108,7 +108,11 @@ $(document).ready(function() {
         $(this).datepicker({ dateFormat: 'yy-mm-dd', 
                              changeMonth: true,
                              changeYear: true,
-                             yearRange: '-5:+0'
+                             yearRange: '-5:+0',
+                             onClose: function(dateText, inst) {
+                                var validDate = $.datepicker.formatDate( "yy-mm-dd", $(this).datepicker('getDate'));
+                                $(this).datepicker('setDate', validDate);
+                             }
                             }); 
     });
     $('#showadvancedsearch').click(function () {

commit 6b780c09141d01df9f04bd6634dd24e1e04d25aa

fix a regression with searchbar in commit 2a038345
Bug: 1207727
Change-Id: I44f645a10ca61070162ec4ecdf2193b6a7d67c09

Server/bkr/server/static/javascript/searchbar_v11.js
@@ -191,7 +191,8 @@ SearchBar.prototype.createValueField = function(current, vals) {
     for (index = 0;index < current_attrs_length ;index++) {
         var node_name = current_attrs[index].nodeName
         var node_val = current_attrs[index].nodeValue
-        if ( node_name != 'type' &&  node_name != 'class')
+        // we only need to clone id, name and value
+        if ( node_name == 'id' || node_name == 'name' || node_name == 'value')
             clone_attrs[node_name] =node_val
     }
     var table_value_lower = this.tableField.value.toLowerCase()

Server/bkr/server/widgets.py
@@ -651,7 +651,7 @@ class SearchBar(RepeatingFormField):
     """Search Bar""" 
     css = [LocalCSSLink('bkr', '/static/css/smoothness/jquery-ui.css')]
     javascript = [LocalJSLink('bkr', '/static/javascript/search_object.js'),
-                  LocalJSLink('bkr', '/static/javascript/searchbar_v10.js'),
+                  LocalJSLink('bkr', '/static/javascript/searchbar_v11.js'),
                   LocalJSLink('bkr','/static/javascript/jquery-ui-1.9.2.min.js', order=3),]
     template = "bkr.server.templates.search_bar"
 

IntegrationTests/src/bkr/inttest/server/selenium/test_distro_search.py
@@ -93,6 +93,21 @@ class Search(WebDriverTestCase):
         check_distro_search_results(b, present=[self.distro_one],
                                     absent=[self.distro_two, self.distro_three])
 
+    # https://bugzilla.redhat.com/show_bug.cgi?id=1207727
+    def test_search_by_name(self):
+        b = self.browser
+        b.get(get_server_base() + 'distros')
+        b.find_element_by_link_text('Show Search Options').click()
+        wait_for_animation(b, '#searchform')
+        b.find_element_by_xpath("//select[@id='distrosearch_0_table']/option[@value='Name']").click()
+        b.find_element_by_xpath('//input[@id="distrosearch_0_value" and '
+                            'not(contains(@title, "date in YYYY-MM-DD format"))]')
+        b.find_element_by_xpath('//input[@id="distrosearch_0_value"]').clear()
+        b.find_element_by_xpath('//input[@id="distrosearch_0_value"]').send_keys(self.distro_one.name)
+        b.find_element_by_id('searchform').submit()
+        check_distro_search_results(b, present=[self.distro_one],
+                    absent=[self.distro_two, self.distro_three])
+
     def test_search_by_osmajor(self):
         b = self.browser
         b.get(get_server_base() + 'distros')