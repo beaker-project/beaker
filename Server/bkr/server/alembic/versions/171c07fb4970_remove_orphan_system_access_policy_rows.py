5642e673aad2e5a1d5b93e4a26da25e813326d0d
mjia@redhat.com <mjia@redhat.com>
2015-05-19 13:57:56 +1000

    remove orphan system access policy rows
    Bug: 1198914
    Change-Id: I8c66effcf2481a7c8a8287711c0cf347324b7a52

---
+++ Server/bkr/server/alembic/versions/171c07fb4970_remove_orphan_system_access_policy_rows.py
@@ -0,0 +1,30 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+"""remove orphan system access policy rows
+
+Revision ID: 171c07fb4970
+Revises: 19d89d5fbde6
+Create Date: 2015-05-19 13:37:02.807126
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '171c07fb4970'
+down_revision = '19d89d5fbde6'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+def upgrade():
+    op.execute("""
+        DELETE FROM system_access_policy WHERE
+        NOT EXISTS (SELECT 1 FROM system WHERE custom_access_policy_id =
+        system_access_policy.id) AND NOT EXISTS(SELECT 1 FROM system_pool
+        WHERE access_policy_id = system_access_policy.id)
+        """)
+def downgrade():
+    pass # no downgrade because we are fixing up old data
