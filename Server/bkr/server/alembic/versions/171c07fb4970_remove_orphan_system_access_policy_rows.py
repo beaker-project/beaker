beaker-project/beaker

commit 5642e673aad2e5a1d5b93e4a26da25e813326d0d

remove orphan system access policy rows
Bug: 1198914
Change-Id: I8c66effcf2481a7c8a8287711c0cf347324b7a52

Server/bkr/server/alembic/versions/171c07fb4970_remove_orphan_system_access_policy_rows.py
@@ -1,0 +1,30 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+"""remove orphan system access policy rows
+
+Revision ID: 171c07fb4970
+Revises: 19d89d5fbde6
+Create Date: 2015-05-19 13:37:02.807126
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '171c07fb4970'
+down_revision = '19d89d5fbde6'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+def upgrade():
+    op.execute("""
+        DELETE FROM system_access_policy WHERE
+        NOT EXISTS (SELECT 1 FROM system WHERE custom_access_policy_id =
+        system_access_policy.id) AND NOT EXISTS(SELECT 1 FROM system_pool
+        WHERE access_policy_id = system_access_policy.id)
+        """)
+def downgrade():
+    pass # no downgrade because we are fixing up old data

commit 2c9a20683e54867a1b8ba37eba8f43d0ab58cc48

fix wrong revision identifier introduced in 5642e673
Change-Id: Ie3070887b80876d950653b6aead56dcc266564c7

Server/bkr/server/alembic/versions/171c07fb4970_remove_orphan_system_access_policy_rows.py
@@ -13,7 +13,7 @@ Create Date: 2015-05-19 13:37:02.807126
 
 # revision identifiers, used by Alembic.
 revision = '171c07fb4970'
-down_revision = '19d89d5fbde6'
+down_revision = '47af1e057a74'
 
 from alembic import op
 import sqlalchemy as sa