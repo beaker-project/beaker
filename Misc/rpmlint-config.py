b38a76d750e1a7db7aed5989557788a59d135643
Dan Callaghan <dcallagh@redhat.com>
2015-11-02 10:56:38 +1000

    rpmlint config
    Fixed one outstanding rpmlint error, added a config file to whitelist
    all the remaining ones which can be ignored.
    
    This will let us invoke rpmlint in our Jenkins build to ensure we don't
    introduce any new warnings or errors.
    
    Change-Id: Id32465343f41348f4eb80db2dfd84381ec178c7f

---
+++ Misc/rpmlint-config.py
@@ -0,0 +1,57 @@
+
+from Config import addFilter
+
+# It's true, our server-side man pages are deficient
+addFilter(r'(beaker-server|beaker-lab-controller)\.noarch: W: no-manual-page-for-binary')
+
+# Kid templates produce a .pyc without a corresponding .py when they are compiled
+addFilter(r'python-bytecode-without-source /usr/lib/python.*/site-packages/bkr/server/templates/.*\.pyc')
+
+# We ship the same logrotate config file for both server and lab-controller,
+# so it's just called "beaker"
+addFilter(r'incoherent-logrotate-file /etc/logrotate\.d/beaker')
+
+# These are intentionally non-world-readable
+addFilter(r'non-readable /etc/beaker/server\.cfg')
+addFilter(r'non-readable /etc/sudoers\.d/beaker_proxy_clear_netboot')
+
+# These are intentionally non-executable, they are executed on test systems instead
+addFilter(r'non-executable-script /usr/share/bkr/lab-controller/(anamon|anamon\.init)')
+
+# This cron job is both executable and configuration intentionally,
+# this might be violating some packaging guidelines... need to check.
+addFilter(r'executable-marked-as-config-file /etc/cron\.hourly/beaker_expire_distros')
+
+# Fake compose data uses empty hidden files to produce the directory structure
+addFilter(r'(zero-length|hidden-file-or-dir) /usr/lib/python.*/site-packages/bkr/inttest/labcontroller/compose_layout/')
+
+# Fake client config included with tests
+addFilter(r'hidden-file-or-dir /usr/lib/python.*/site-packages/bkr/inttest/client/.beaker_client')
+
+# We are guilty of using jargon from time to time
+addFilter(r'spelling-error.* (netboot|distro|distros)')
+
+# We know what we're doing
+addFilter(r'dangerous-command-in-%post rm')
+addFilter(r'dangerous-command-in-%preun rm')
+
+# No %doc is okay for these as they depend on other subpackages
+addFilter(r'(beaker-lab-controller-addDistro|beaker-integration-tests)\.noarch: W: no-documentation')
+
+# RPMs built from git have no %changelog, the proper ones maintained from dist-git do though
+addFilter(r'no-changelogname-tag')
+
+# /dev/null always exists
+addFilter(r'dangling-symlink .* /dev/null')
+
+# Our initscripts are okay, rpmlint just can't understand the variable substitution
+addFilter(r'incoherent-subsys .* \$prog')
+
+# rpmlint is being a bit preachy (we really don't need or want reload)
+addFilter(r'no-reload-entry /etc/init\.d/beakerd')
+
+# We can call our services whatever we want
+addFilter(r'incoherent-init-script-name beakerd')
+
+# This is just an rpmlint bug really
+addFilter(r'explicit-lib-dependency (libxml2-python|libxslt-python|python-passlib)')
