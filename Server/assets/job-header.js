beaker-project/beaker

commit c49a98a2ed75fd62e2c92dbf9deb84e132298e5e

js: extracted a utility function for showing errors for failed XmlHttpRequests
Also enhanced it to handle unexpected error conditions (like 500 and
Connection refused) with some nicer messages.

Change-Id: I441e01773823712cc4e4605d4548ee0913b39a9f

Server/assets/prefs.js
@@ -33,11 +33,7 @@ window.UserRootPasswordView = Backbone.View.extend({
         var new_root_password = $form.find('[name=root_password]').val();
         this.model.save({root_password: new_root_password}, {patch: true, wait: true})
             .always(function () { $form.find('button').button('reset'); })
-            .fail(function (xhr) {
-                $form.append(
-                    $('<div class="alert alert-error"/>')
-                        .text(xhr.statusText + ': ' + xhr.responseText));
-            });
+            .fail(function (xhr) { $form.append(alert_for_xhr(xhr)); });
     },
 });
 
@@ -62,10 +58,7 @@ window.UserSSHPublicKeysView = Backbone.View.extend({
         this.$('.alert-error').remove();
         this.model.add_ssh_public_key(this.$('[name=key]').val())
             .always(function () { $form.find('button').button('reset'); })
-            .fail(function (xhr) {
-                $el.append($('<div class="alert alert-error"/>')
-                        .text(xhr.statusText + ': ' + xhr.responseText));
-            });
+            .fail(function (xhr) { $el.append(alert_for_xhr(xhr)); });
     },
 });
 
@@ -88,9 +81,7 @@ var UserSSHPublicKeyListItem = Backbone.View.extend({
             .always(function () { $el.find('button').button('reset'); })
             .fail(function (xhr) {
                 // show it in a growl because we can't cram it inside the list item
-                $.bootstrapGrowl('<h4>Failed to remove SSH public key</h4>' +
-                        xhr.statusText + ': ' + xhr.responseText,
-                        {type: 'error'});
+                growl_for_xhr(xhr, 'Failed to remove SSH public key');
             });
     },
 });
@@ -126,10 +117,7 @@ window.UserSubmissionDelegatesView = Backbone.View.extend({
         $form.find('button').button('loading');
         this.model.add_submission_delegate(this.$('[name=user_name]').val())
             .always(function () { $form.find('button').button('reset'); })
-            .fail(function (xhr) {
-                $el.append($('<div class="alert alert-error"/>')
-                        .text(xhr.statusText + ': ' + xhr.responseText));
-            });
+            .fail(function (xhr) { $el.append(alert_for_xhr(xhr)); });
     },
     remove: function (evt) {
         var $button = $(evt.currentTarget);
@@ -138,9 +126,7 @@ window.UserSubmissionDelegatesView = Backbone.View.extend({
             .always(function () { $button.button('reset'); })
             .fail(function (xhr) {
                 // show it in a growl because we can't cram it inside the list item
-                $.bootstrapGrowl('<h4>Failed to remove submission delegate</h4>' +
-                        xhr.statusText + ': ' + xhr.responseText,
-                        {type: 'error'});
+                growl_for_xhr(xhr, 'Failed to remove submission delegate');
             });
     },
 });

Server/assets/beaker-backgrid.js
@@ -330,10 +330,7 @@ window.BeakerGrid = Backbone.View.extend({
         if ( xhr.statusText != 'abort' ) {
             this.grid.body.$el.empty();
             // can't put a <div/> inside a <tbody/> so it goes at the bottom instead
-            this.$el.append(
-                $('<div class="alert alert-error"/>')
-                .text('Failed to fetch data while populating grid: ' +
-                    xhr.statusText + ': ' + xhr.responseText)); 
+            this.$el.append(alert_for_xhr(xhr, 'Failed to fetch data while populating grid'));
         }
     },
     fetch_success: function (collection) {

Server/assets/group-create.js
@@ -43,9 +43,7 @@ window.GroupCreateModal = Backbone.View.extend({
         window.location = xhr.getResponseHeader('Location');
     },
     save_error: function (xhr) {
-        $('<div class="alert alert-error"/>')
-            .text(xhr.statusText + ': ' + xhr.responseText)
-            .appendTo(this.$('.sync-status'));
+        this.$('.sync-status').append(alert_for_xhr(xhr));
         this.$('.modal-footer button').button('reset');
     },
 });

Server/assets/group-details.js
@@ -44,9 +44,7 @@ window.GroupPageHeaderView = Backbone.View.extend({
     },
     delete_error: function(xhr) {
         if (!_.isEmpty(xhr)) {
-            $.bootstrapGrowl('<h4>Failed to delete</h4> ' +
-                  xhr.statusText + ': ' + xhr.responseText,
-                    {type: 'error'});
+            growl_for_xhr(xhr, 'Failed to delete');
             this.$('button.delete').button('reset');
         }
     },
@@ -103,9 +101,7 @@ var GroupEditModal = Backbone.View.extend({
             this.$el.modal('hide');
     },
     save_error: function (xhr) {
-        $('<div class="alert alert-error"/>')
-            .text(xhr.statusText + ': ' + xhr.responseText)
-            .appendTo(this.$('.sync-status'));
+        this.$('.sync-status').append(alert_for_xhr(xhr));
         this.$('.modal-footer button').button('reset');
     },
 });

Server/assets/group-members.js
@@ -26,9 +26,7 @@ window.GroupMembersListItemView = Backbone.View.extend({
        model.remove_member(this.member.get('user_name'))
         .fail(function (jqxhr, status, error) {
              $(evt.currentTarget).button('reset');
-             $.bootstrapGrowl('<h4>Failed to remove group member</h4> ' +
-                   jqxhr.statusText + ': ' + jqxhr.responseText,
-                     {type: 'error'});
+             growl_for_xhr(jqxhr, 'Failed to remove group member');
          });
        evt.preventDefault();
      },
@@ -54,9 +52,7 @@ window.GroupExcludedUsersListItemView = Backbone.View.extend({
        model.remove_excluded_user(this.user.get('user_name'))
         .fail(function (jqxhr, status, error) {
              $(evt.currentTarget).button('reset');
-             $.bootstrapGrowl('<h4>Failed to remove user</h4> ' +
-                   jqxhr.statusText + ': ' + jqxhr.responseText,
-                     {type: 'error'});
+             growl_for_xhr(jqxhr, 'Failed to remove user');
          });
        evt.preventDefault();
      },
@@ -135,9 +131,7 @@ var GroupAddMemberForm = Backbone.View.extend({
     },
     error: function (jqxhr, status, error) {
         this.$('button').button('reset');
-        this.$el.append(
-            $('<div class="alert alert-error"/>')
-            .text(jqxhr.statusText + ': ' + jqxhr.responseText));
+        this.$el.append(alert_for_xhr(jqxhr));
     },
 });
 
@@ -175,9 +169,7 @@ var GroupExcludeUserForm = Backbone.View.extend({
     },
     error: function (jqxhr, status, error) {
         this.$('button').button('reset');
-        this.$el.append(
-            $('<div class="alert alert-error"/>')
-            .text(jqxhr.statusText + ': ' + jqxhr.responseText));
+        this.$el.append(alert_for_xhr(jqxhr));
     },
 });
 

Server/assets/group-owners.js
@@ -26,9 +26,7 @@ window.GroupOwnersListItemView = Backbone.View.extend({
        model.remove_owner(this.owner.get('user_name'))
         .fail(function (jqxhr, status, error) {
              $(evt.currentTarget).button('reset');
-             $.bootstrapGrowl('<h4>Failed to remove group owner</h4> ' +
-                   jqxhr.statusText + ': ' + jqxhr.responseText,
-                     {type: 'error'});
+             growl_for_xhr(jqxhr, 'Failed to remove group owner');
          });
        evt.preventDefault();
      },
@@ -90,9 +88,7 @@ var GroupAddOwnerForm = Backbone.View.extend({
     },
     error: function (jqxhr, status, error) {
         this.$('button').button('reset');
-        this.$el.append(
-            $('<div class="alert alert-error"/>')
-            .text(jqxhr.statusText + ': ' + jqxhr.responseText));
+        this.$el.append(alert_for_xhr(jqxhr));
     },
 });
 

Server/assets/group-permissions.js
@@ -26,9 +26,7 @@ window.GroupPermissionsListItemView = Backbone.View.extend({
         $(evt.currentTarget).button('loading');
         model.remove_permission(this.permission)
           .fail(function (jqxhr, status, error) {
-            $.bootstrapGrowl('<h4>Failed to remove group permission</h4> ' +
-                jqxhr.statusText + ': ' + jqxhr.responseText,
-                {type: 'error'});
+                growl_for_xhr(jqxhr, 'Failed to remove group permission');
           });
         $(evt.currentTarget).button('reset');
         evt.preventDefault();
@@ -90,9 +88,7 @@ var GroupAddPermissionForm = Backbone.View.extend({
     },
     error: function (jqxhr, status, error) {
         this.$('button').button('reset');
-        this.$el.append(
-            $('<div class="alert alert-error"/>')
-            .text(jqxhr.statusText + ': ' + jqxhr.responseText));
+        this.$el.append(alert_for_xhr(jqxhr));
     },
 });
 

Server/assets/labcontrollers.js
@@ -80,8 +80,7 @@ var LabControllerListItem = Backbone.View.extend({
     on_error: function (model, xhr) {
         this.$('button').button('reset');
         // Inconsistent: Using growl here, since appending an error div to the list item will be worse.
-        $.bootstrapGrowl('<h4>Error</h4>' +
-                            xhr.statusText + ': ' + xhr.responseText, {type: 'error'});
+        growl_for_xhr(xhr, 'Error removing lab controller');
     },
 })
 
@@ -162,9 +161,7 @@ var CreateLabControllerModal = Backbone.View.extend({
     },
     error: function(model, xhr) {
         this.reset_button();
-        this.$el.append(
-            $('<div class="alert alert-error" />')
-                .text(xhr.statusText + ': ' + xhr.responseText));
+        this.$el.append(alert_for_xhr(xhr));
     }
 });
 
@@ -222,9 +219,7 @@ var EditLabControllerModal = Backbone.View.extend({
     },
     error: function(model, xhr) {
         this.reset_button();
-        this.$el.append(
-            $('<div class="alert alert-error" />')
-                .text(xhr.statusText + ': ' + xhr.responseText));
+        this.$el.append(alert_for_xhr(xhr));
     }
 })
 })();

commit c82b8784986110c0268da4e84180eec905bbeb43

js: use utility functions to show errors for failed XmlHttpRequests
This uses utility functions introduced in c49a98a2 to show errors for
failed XmlHttpRequest on the job page and the preferences page.

Change-Id: Ieb1003e020c9899f06cf9d58478827574b252f33

Server/assets/job-header.js
@@ -61,9 +61,7 @@ window.JobHeaderView = Backbone.View.extend({
             .done(function () { window.location = beaker_url_prefix + 'jobs/mine'; });
     },
     delete_error: function(xhr) {
-        $.bootstrapGrowl('<h4>Failed to delete</h4> ' +
-                xhr.statusText + ': ' + xhr.responseText,
-                {type: 'error'});
+        growl_for_xhr(xhr, 'Failed to delete');
         this.$('button.delete').button('reset');
     },
 });
@@ -126,9 +124,7 @@ var JobEditModal = Backbone.View.extend({
         this.$el.modal('hide');
     },
     save_error: function (xhr) {
-        $('<div class="alert alert-error"/>')
-            .text(xhr.statusText + ': ' + xhr.responseText)
-            .appendTo(this.$('.sync-status'));
+        alert_for_xhr(xhr).appendTo(this.$('.sync-status'));
         this.$('.modal-footer button').button('reset');
     },
 });
@@ -162,9 +158,7 @@ var JobCancelModal = Backbone.View.extend({
         this.$el.modal('hide');
     },
     save_error: function (xhr) {
-        $('<div class="alert alert-error"/>')
-            .text(xhr.statusText + ': ' + xhr.responseText)
-            .appendTo(this.$('.sync-status'));
+        alert_for_xhr(xhr).appendTo(this.$('.sync-status'));
         this.$('.modal-footer button').button('reset');
     },
 });

Server/assets/job-recipes.js
@@ -72,10 +72,7 @@ var RecipeSetView = Backbone.View.extend({
     },
     unwaive_error: function (xhr) {
         this.$('button.unwaive').button('reset');
-        $.bootstrapGrowl(
-                '<h4>Error unwaiving ' + this.model.get('t_id') + '</h4>' +
-                xhr.statusText + ': ' + xhr.responseText,
-                {type: 'error'});
+        growl_for_xhr(xhr, 'Error unwaiving ' + this.model.get('t_id'));
     },
     cancelling: function () {
         this.$('button.cancel').button('loading');
@@ -155,9 +152,7 @@ var RecipeSetCommentForm = Backbone.View.extend({
         this.$('textarea[name=comment]').val('');
     },
     save_error: function (model, xhr, options) {
-        $('<div class="alert alert-error"/>')
-            .text(xhr.statusText + ': ' + xhr.responseText)
-            .appendTo(this.$('.sync-status'));
+        alert_for_xhr(xhr).appendTo(this.$('.sync-status'));
         this.$('button').button('reset');
     },
 });
@@ -226,9 +221,7 @@ var RecipeSetPriorityModal = Backbone.View.extend({
         this.$el.modal('hide');
     },
     save_error: function (xhr) {
-        $('<div class="alert alert-error"/>')
-            .text(xhr.statusText + ': ' + xhr.responseText)
-            .appendTo(this.$('.sync-status'));
+        alert_for_xhr(xhr).appendTo(this.$('.sync-status'));
         this.$('.modal-footer button').button('reset');
     },
 });
@@ -262,9 +255,7 @@ var RecipeSetCancelModal = Backbone.View.extend({
         this.$el.modal('hide');
     },
     save_error: function (xhr) {
-        $('<div class="alert alert-error"/>')
-            .text(xhr.statusText + ': ' + xhr.responseText)
-            .appendTo(this.$('.sync-status'));
+        alert_for_xhr(xhr).appendTo(this.$('.sync-status'));
         this.$('.modal-footer button').button('reset');
     },
 });
@@ -298,9 +289,7 @@ var RecipeSetWaiveModal = Backbone.View.extend({
         this.$el.modal('hide');
     },
     save_error: function (xhr) {
-        $('<div class="alert alert-error"/>')
-            .text(xhr.statusText + ': ' + xhr.responseText)
-            .appendTo(this.$('.sync-status'));
+        alert_for_xhr(xhr).appendTo(this.$('.sync-status'));
         this.$('.modal-footer button').button('reset');
     },
 });

Server/assets/prefs.js
@@ -163,8 +163,7 @@ window.UserUIPreferencesView = Backbone.View.extend({
                 $el.find('.form-actions button').button('reset');
             })
             .fail(function (xhr) {
-                $el.append($('<div class="alert alert-error"/>')
-                        .text(xhr.statusText + ': ' + xhr.responseText));
+                $el.append(alert_for_xhr(xhr));
             })
             .done(function (xhr) {
                 // Bootstrap's button reset happens in setTimeout for... 