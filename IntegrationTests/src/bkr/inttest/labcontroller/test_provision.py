4ae73e703a99ba3a31409bce2f3885c2a304df13
Dan Callaghan <dcallagh@redhat.com>
2016-07-21 12:57:20 +1000

    test_provision: timeout exception message changed wording in Python 2.7
    Currently we get:
    
        URLError: <urlopen error timed out>
    
    but on RHEL7 it is instead:
    
        timeout: timed out
    
    They both seem fine so let's just have the test case accept either
    wording.
    
    Change-Id: Ia3abb6b826510a34bde85fa3407bd32ae392349c

--- IntegrationTests/src/bkr/inttest/labcontroller/test_provision.py
+++ IntegrationTests/src/bkr/inttest/labcontroller/test_provision.py
@@ -251,5 +251,4 @@ class ConfigureNetbootTest(LabControllerTestCase):
                 + get_conf().get('IMAGE_FETCH_TIMEOUT')))
         self.assertEquals(system.command_queue[0].action, u'configure_netboot')
         self.assertEquals(system.command_queue[0].status, CommandStatus.failed)
-        self.assertEquals(system.command_queue[0].new_value,
-                u'URLError: <urlopen error timed out>')
+        self.assertIn(u'timed out', system.command_queue[0].new_value)
