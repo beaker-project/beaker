098a9340f944baab5dee1a9fa475aaac635cab50
Dong Wang <dowang@redhat.com>
2016-09-01 05:54:47 -0400

    tests: Add testcase about Help
    Change-Id: Iab8639d05b1ab807c4eb8cd495a3f09ce06a0f2f

---
+++ IntegrationTests/src/bkr/inttest/server/selenium/test_help.py
@@ -0,0 +1,24 @@
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+from bkr.inttest.server.selenium import WebDriverTestCase
+
+from bkr.inttest.server.webdriver_utils import login
+
+
+class TestHelp(WebDriverTestCase):
+
+    def setUp(self):
+        self.browser = self.get_browser()
+
+    def test_help_is_available(self):
+        b = self.browser
+        login(b)
+        b.find_element_by_link_text('Help').click()
+        b.find_element_by_xpath("//ul[@id='help-menu']//a[text()='Documentation' \
+                                and @href='http://beaker-project.org/docs/']")
+        b.find_element_by_xpath("//ul[@id='help-menu']//a[text()='Report a Bug' \
+                                and @href='https://bugzilla.redhat.com/enter_bug.cgi?product=Beaker']")
