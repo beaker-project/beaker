05d6a3c9b087c898b9fd77acbbeb2c1640f69567
Dan Callaghan <dcallagh@redhat.com>
2016-09-02 11:21:24 +1000

    Automatic commit of release 23.2

--- Server/setup.py
+++ Server/setup.py
@@ -195,7 +195,7 @@ else:
 setup(
     name="bkr.server",
     namespace_packages = ['bkr'],
-    version='23.1',
+    version='23.2',
     # uncomment the following lines if you fill them out in release.py
     description=description,
     author=author,
