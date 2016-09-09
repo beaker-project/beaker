b74949d01c7836cb7545cf4a9b2f36eaed1b8409
Hui Wang <huiwang@redhat.com>
2016-09-09 17:01:11 +0800

    Add CLI bkr pool-systems
    There is no CLI to list systems in system pool.
    So add this CLI interface to list systems in system pool.
    
    Bug: 1374620
    Change-Id: Ida3d1f3c334c22e0753dbbd365a6e1e2a2c1ab1e

---
+++ Client/src/bkr/client/commands/cmd_pool_systems.py
@@ -0,0 +1,90 @@
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+"""
+bkr pool-systems: List systems in a pool
+========================================
+
+.. program:: bkr pool-systems
+
+Synopsis
+--------
+
+| :program:`bkr pool-systems` [*options*] <pool-name>
+
+Description
+-----------
+
+Lists systems in a pool.
+
+Options
+-------
+
+.. option:: --format list, --format json
+
+    Display results in the given format. The default format is ``list``,
+    which lists one system per line. The format ``json`` displays systems as a
+    JSON array.
+
+Common :program:`bkr` options are described in the :ref:`Options
+<common-options>` section of :manpage:`bkr(1)`.
+
+Exit status
+-----------
+
+Non-zero on error, otherwise zero.
+
+Examples
+--------
+
+List systems in the ``kernel-hw`` pool::
+
+    bkr pool-systems kernel-hw
+
+See also
+--------
+
+:manpage:`bkr(1)`
+
+"""
+
+from bkr.client import BeakerCommand
+import bkr.client.json_compat as json
+
+class Pool_Systems(BeakerCommand):
+    """List systems in a pool"""
+    enabled = True
+
+    def options(self):
+        self.parser.usage = "%%prog %s <pool-name>" % self.normalized_name
+        self.parser.add_option(
+            '--format',
+            type='choice',
+            choices=['list', 'json'],
+            default='list',
+            help='Results display format: json, list [default: %default]',
+        )
+
+    def run(self, *args, **kwargs):
+
+        if len(args) != 1:
+            self.parser.error('Exactly one pool name must be specified')
+
+        pool_name = args[0]
+
+        self.set_hub(**kwargs)
+        requests_session = self.requests_session()
+        response = requests_session.get('pools/%s' % pool_name,
+                                        headers={'Accept': 'application/json'})
+        response.raise_for_status()
+        attributes = response.json()
+        systems = attributes['systems']
+
+        if kwargs['format'] == 'json':
+            print json.dumps(systems)
+        else:
+            for system in systems:
+                print(system)
---
+++ IntegrationTests/src/bkr/inttest/client/test_pool_systems.py
@@ -0,0 +1,44 @@
+from bkr.server.model import session, SystemPool
+from bkr.inttest import data_setup
+from bkr.inttest.client import run_client, ClientTestCase, ClientError
+
+class PoolSystems(ClientTestCase):
+
+    def test_list_systems_in_a_pool_with_format_list(self):
+        with session.begin():
+            system1 = data_setup.create_system()
+            system2 = data_setup.create_system()
+            pool = data_setup.create_system_pool(systems=[system1, system2])
+
+        out = run_client(['bkr', 'pool-systems', pool.name])
+        self.assertIn(system1.fqdn, out)
+        self.assertIn(system2.fqdn, out)
+
+    def test_list_systems_in_a_pool_with_format_json(self):
+        with session.begin():
+            system1 = data_setup.create_system()
+            system2 = data_setup.create_system()
+            pool = data_setup.create_system_pool(systems=[system1, system2])
+
+        out = run_client(['bkr', 'pool-systems', pool.name,
+                      '--format', 'json'])
+        self.assertIn(system1.fqdn, out)
+        self.assertIn(system2.fqdn, out)
+
+    def test_list_systems_nonexistent_pool(self):
+        pool_name = data_setup.unique_name(u'mypool%s')
+        try:
+            out = run_client(['bkr', 'pool-systems',
+                              pool_name])
+            self.fail('Must fail or die')
+        except ClientError, e:
+            self.assertIn('System pool %s does not exist' % pool_name,
+                         e.stderr_output)
+
+    def test_list_systems_CLI_no_given_poolname(self):
+        try:
+            out = run_client(['bkr', 'pool-systems'])
+            self.fail('Must fail or die')
+        except ClientError, e:
+                self.assertIn('Exactly one pool name must be specified',
+                              e.stderr_output)
