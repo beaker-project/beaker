ab07bd4a1724ca6ba8801255659c6e133cf0aa19
Jon Orris <jorris@redhat.com>
2016-09-15 17:38:25 -0400

    delete log_recipe and log_recipe_task rows for deleted jobs
    Bug: 1337789
    Change-Id: Ie9bb3c40d4ea1b1cd6cfe9509a3a97c9f697fc16

--- IntegrationTests/src/bkr/inttest/server/bz1322700-and-bz1337790-migration-setup.sql
+++ IntegrationTests/src/bkr/inttest/server/bz1322700-and-bz1337790-migration-setup.sql
@@ -11,6 +11,13 @@ INSERT INTO recipe_task_result (id, recipe_task_id)
 VALUES (1, 1);
 INSERT INTO log_recipe_task_result (id, recipe_task_result_id, path, filename, start_time)
 VALUES (1, 1, '/', 'test_log1', '2016-02-06 00:00:00');
+
+insert into log_recipe_task (id, recipe_task_id, path, filename, start_time)
+VALUES (1, 1, '/', 'test_log1', '2016-02-06 00:00:00');
+insert into log_recipe (id, recipe_id, path, filename, start_time)
+VALUES (1, 1, '/', 'test_log1', '2016-02-06 00:00:00');
+
+
 -- Job which is deleted
 INSERT INTO job (id, owner_id, retention_tag_id, dirty_version, clean_version, status, deleted)
 VALUES (2, 1, 1, '', '', 'Completed', '2016-02-05 00:00:00');
@@ -24,4 +31,11 @@ INSERT INTO recipe_task_result (id, recipe_task_id)
 VALUES (2, 2);
 INSERT INTO log_recipe_task_result (id, recipe_task_result_id, path, filename, start_time)
 VALUES (2, 2, '/', 'test_log2', '2016-02-06 01:00:00');
+
+
+insert into log_recipe_task (id, recipe_task_id, path, filename, start_time)
+VALUES (2, 2, '/', 'test_log1', '2016-02-06 00:00:00');
+insert into log_recipe (id, recipe_id, path, filename, start_time)
+VALUES (2, 2, '/', 'test_log1', '2016-02-06 00:00:00');
+
 COMMIT;
