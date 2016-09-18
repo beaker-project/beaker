af01121378832d9bf77eb40eadc042b8826c880f
Dong Wang <dowang@redhat.com>
2016-09-18 02:58:18 -0400

    tests: Add testcase for bkr distros-tag
    Change-Id: I8453666cdc02f2cd3c056c8864fb9cce7729f43f

--- IntegrationTests/src/bkr/inttest/client/test_distros_tag.py
+++ IntegrationTests/src/bkr/inttest/client/test_distros_tag.py
@@ -7,6 +7,7 @@
 from turbogears.database import session
 from bkr.inttest import data_setup
 from bkr.inttest.client import run_client, ClientError, ClientTestCase
+from bkr.server.model import Distro
 
 class DistrosTagTest(ClientTestCase):
 
@@ -17,3 +18,28 @@ class DistrosTagTest(ClientTestCase):
         with session.begin():
             session.refresh(self.distro)
             self.assert_(u'LOL' in self.distro.tags)
+
+    def test_cannot_add_empty_tag(self):
+        try:
+            run_client(['bkr', 'distros-tag'])
+            self.fail('should raise')
+        except ClientError, e:
+            self.assertEqual(e.status, 2)
+            self.assertIn('Please specify a tag', e.stderr_output)
+
+    def test_cannot_add_tag_without_distro_name(self):
+        try:
+            run_client(['bkr', 'distros-tag', 'asdf'])
+            self.fail('should raise')
+        except ClientError, e:
+            self.assertEqual(e.status, 2)
+            self.assertIn('If you really want to tag every distro in Beaker, use --name=%', e.stderr_output)
+
+    def test_successful_add_tag_for_all_distros(self):
+        with session.begin():
+            self.distro = data_setup.create_distro()
+        out = run_client(['bkr', 'distros-tag', '--name=%', 'addAll'])
+        with session.begin():
+            session.expire_all()
+            for distro in Distro.query:
+                self.assertIn(u'addAll', distro.tags)
