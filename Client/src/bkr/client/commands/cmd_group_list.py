ee32f04b6f06a6b2b335b6cd3fa4eb838f5e18e7
Hui Wang <huiwang@redhat.com>
2016-09-21 11:36:21 +0800

    Add CLI for bkr group-list
    CLI bkr group-list can list groups and filter groups by owner.
    
    Bug: 1373409
    Change-Id: I1793f02b0c1d2eca5c475d2e6c3cba3046e5abcc

---
+++ Client/src/bkr/client/commands/cmd_group_list.py
@@ -0,0 +1,100 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+"""
+bkr group-list: List groups
+===========================
+
+.. program:: bkr group-list
+
+Synopsis
+--------
+
+| :program:`bkr group-list`
+|       [:option:`--owner` <user>]
+|       [:option:`--limit` <number>]
+
+Description
+-----------
+
+Lists groups or groups owned by the given user.
+
+Options
+-------
+
+.. option:: --owner <username>
+
+   List groups owned by <username>.
+
+.. option:: --limit <number>
+
+   Return at most <number> groups.
+
+Common :program:`bkr` options are described in the :ref:`Options
+<common-options>` section of :manpage:`bkr(1)`.
+
+Exit status
+-----------
+
+Non-zero on error, otherwise zero.
+
+Examples
+--------
+
+List all groups which are owned by the user ``test``::
+
+    bkr group-list --owner test
+
+See also
+--------
+
+:manpage:`bkr(1)`, :manpage:`bkr-group-members(1)`
+
+"""
+
+from bkr.client import BeakerCommand
+import sys
+
+class Group_List(BeakerCommand):
+    """List groups"""
+    enabled = True
+
+    def options(self):
+        self.parser.usage = "%%prog %s [options] ..." % self.normalized_name
+
+        self.parser.add_option('--owner',
+                               metavar='USERNAME',
+                               help='List groups owned by owner USERNAME')
+
+        self.parser.add_option("--limit",
+                               default=50,
+                               type=int,
+                               help='Limit results to this many [default: %default]')
+
+    def run(self, *args, **kwargs):
+        owner = kwargs.get('owner', None)
+        limit = kwargs.get('limit')
+
+        self.set_hub(**kwargs)
+
+        requests_session = self.requests_session()
+
+        params = {}
+        params['page_size'] = limit
+        if owner:
+            params['q'] = 'owner.user_name:%s' % owner
+
+        response = requests_session.get('groups/', params=params, headers={'Accept': 'application/json'})
+
+        response.raise_for_status()
+        attributes = response.json()
+        groups = attributes['entries']
+
+        if not groups:
+            sys.stderr.write('Nothing Matches\n')
+            sys.exit(1)
+
+        for group in groups:
+            print(group['group_name'])
---
+++ IntegrationTests/src/bkr/inttest/client/test_group_list.py
@@ -0,0 +1,44 @@
+from bkr.server.model import session
+from bkr.inttest import data_setup
+from bkr.inttest.client import run_client, ClientError, ClientTestCase, create_client_config
+
+class GroupList(ClientTestCase):
+
+    def test_list_groups(self):
+        with session.begin():
+            group1 = data_setup.create_group()
+            group2 = data_setup.create_group()
+        out = run_client(['bkr', 'group-list'])
+        self.assertIn(group1.group_name, out)
+        self.assertIn(group2.group_name, out)
+
+    def test_list_groups_by_owner(self):
+        with session.begin():
+            user1 = data_setup.create_user()
+            user2 = data_setup.create_user()
+            group1 = data_setup.create_group(owner=user1)
+            group2 = data_setup.create_group(owner=user2)
+        out = run_client(['bkr', 'group-list', '--owner', str(user1.user_name)])
+        self.assertIn(group1.group_name, out)
+        self.assertNotIn(group2.group_name, out)
+
+    def test_list_groups_limit(self):
+        with session.begin():
+            data_setup.create_group()
+            data_setup.create_group()
+        out1 = run_client(['bkr', 'group-list'])
+        groups1 = out1.split()
+        out2 = run_client(['bkr', 'group-list', '--limit=1'])
+        groups2 = out2.split()
+        self.assertEquals(len(groups2), 1)
+        self.assertEquals(groups1[0], groups2[0])
+
+    def test_list_group_of_owner_that_has_no_group(self):
+        with session.begin():
+            user = data_setup.create_user()
+        try:
+            run_client(['bkr', 'group-list', '--owner', str(user.user_name)])
+            self.fail('Must fail or die')
+        except ClientError as e:
+            self.assertEqual(e.status, 1)
+            self.assertIn('Nothing Matches', e.stderr_output)
