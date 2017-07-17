804254ac2eafb2584274c38afde90d5279648376
Dan Callaghan <dcallagh@redhat.com>
2017-07-17 15:52:27 +1000

    use correct pylint binary on Fedora
    Change-Id: If0a1aa3497f4978b3f214bdf6f6fcbe5144fa6b9

--- Misc/run-pylint.sh
+++ Misc/run-pylint.sh
@@ -1,4 +1,13 @@
-# Run this as Misc/run-pylint.sh from the base of a working copy
+# Run this from the base of a working copy, for example:
+#   Misc/run-pylint.sh -E bkr
+
+# On Fedora 26+, /usr/bin/pylint is Python 3 and /usr/bin/pylint-2 is Python 2.
+# On RHEL 6 there is only /usr/bin/pylint (Python 2).
+if command -v pylint-2 >/dev/null ; then
+    PYLINT=pylint-2
+else
+    PYLINT=pylint
+fi
 
 export PYTHONPATH=Common:Server:LabController/src:Client/src:IntegrationTests/src${PYTHONPATH:+:$PYTHONPATH}
-pylint --rcfile=Misc/pylint-errors.cfg "$@"
+$PYLINT --rcfile=Misc/pylint-errors.cfg "$@"
