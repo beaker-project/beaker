157935e1cd3b6863574cd48ba1b599e01cb1cb1e
Dan Callaghan <dcallagh@redhat.com>
2018-08-28 13:29:53 +1000

    beaker-repo-update: handle missing OS majors as a special case
    ... instead of suppressing *all* exceptions when syncing the repo. In
    case of an "expected" missing repo (such as Atomic Host) the warning
    goes from this scary-looking message:
    
        2018-08-28 12:48:33,815 beaker-repo-update WARNING Cannot retrieve repository metadata (repomd.xml) for repository: http:--localhost:19998-BlueShoeLinux5-. Please verify its path and try again
    
    to this much clearer message:
    
        2018-08-28 12:56:46,640 beaker-repo-update WARNING Harness packages not found for OS major BlueShoeLinux5, ignoring
    
    And more importantly, any other error conditions (like a full disk or
    a failure to fetch a particular package) will cause beaker-repo-update
    to bail out immediately with a traceback. We want that, because
    otherwise it is too easy to miss the real errors as they fly by buried
    under subsequent debug messages.
    
    Bug: 1619969
    Change-Id: If972a87be5ed9b246b418ba5bcd08f31f273df64

--- Server/bkr/server/tools/repo_update.py
+++ Server/bkr/server/tools/repo_update.py
@@ -22,6 +22,7 @@ import urlparse
 import shutil
 import yum, yum.misc, yum.packages
 import urllib
+import requests
 import logging
 
 __description__ = 'Script to update harness repos'
@@ -48,6 +49,17 @@ def get_parser():
 def usage():
     print USAGE_TEXT
     sys.exit(-1)
+
+# Use a single global requests.Session for this process
+# for connection pooling.
+requests_session = requests.Session()
+
+# We distinguish this particular situation from other errors which may occur
+# while fetching packages, because it's "expected" in some cases -- for example
+# Atomic Host or RHVH which can be imported into Beaker, but cannot have
+# harness packages and thus there will be no corresponding directory to
+# download from.
+class HarnessRepoNotFoundError(ValueError): pass
 
 # Can't use /usr/bin/reposync for this, because it tries to replicate the 
 # ../../ in rpm paths and generally makes a mess of things.
@@ -78,6 +90,11 @@ class RepoSyncer(yum.YumBase):
         yum.packages.base = None
 
     def sync(self):
+        # First check if the harness repo exists.
+        response = requests_session.head(urlparse.urljoin(self.repo_url, 'repodata/repomd.xml'))
+        if response.status_code != 200:
+            raise HarnessRepoNotFoundError()
+
         has_new_packages = False
         log.info('Syncing packages from %s to %s', self.repo_url, self.output_dir)
         self.doRepoSetup()
@@ -118,8 +135,8 @@ def update_repos(baseurl, basepath):
             has_new_packages = syncer.sync()
         except KeyboardInterrupt:
             raise
-        except Exception, e:
-            log.warning('%s', e)
+        except HarnessRepoNotFoundError:
+            log.warning('Harness packages not found for OS major %s, ignoring', osmajor)
             continue
         if has_new_packages:
             createrepo_results = run_createrepo(cwd=dest)
