662be24497169a6d8940f0e5310bd2bef2e45754
Dan Callaghan <dcallagh@redhat.com>
2018-08-31 09:54:35 +1000

    Automatic commit of release 25.6

--- Client/setup.py
+++ Client/setup.py
@@ -13,7 +13,7 @@ def bash_completion_dir():
 
 setup(
     name='beaker-client',
-    version='25.5',
+    version='25.6',
     description='Command-line client for interacting with Beaker',
     author='Red Hat, Inc.',
     author_email='beaker-devel@lists.fedorahosted.org',
