843ff506f7a1ea983bbfcb4897f4546d3e35fcee
Dan Callaghan <dcallagh@redhat.com>
2018-10-04 15:58:52 +1000

    Automatic commit of release 26.0

--- IntegrationTests/setup.py
+++ IntegrationTests/setup.py
@@ -12,7 +12,7 @@ def get_compose_layout():
 
 setup(
     name='beaker-integration-tests',
-    version='26.0rc1',
+    version='26.0',
     description='Integration tests for Beaker',
     author='Red Hat, Inc.',
     author_email='beaker-devel@lists.fedorahosted.org',
