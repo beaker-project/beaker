5a0455df25127efb512c45e6bbcbb93b1cd53c8d
Christopher Beer <cbeer@redhat.com>
2018-10-31 09:20:00 -0400

    Automatic commit of release 26.1
    Change-Id: I754a16d383270f6df4fa11c1a7209ced48826de1

--- LabController/setup.py
+++ LabController/setup.py
@@ -49,7 +49,7 @@ else:
 
 setup(
     name='beaker-lab-controller',
-    version='26.0',
+    version='26.1',
     description='Daemons for controlling a Beaker lab',
     author='Red Hat, Inc.',
     author_email='beaker-devel@lists.fedorahosted.org',
