6e3e2ebd738db1ae7a3e35bf1fd3d3bc80548bdd
Carol Bouchard <cbouchar@redhat.com>
2019-03-18 11:22:48 -0400

    Add json output format for system-details
    Add json output format for system-details bkr command.
    This prints all system data excluding data found
    in system-status bkr command.
    
    Bug: 1597923
    Change-Id: I15e659ca5998ce613da3c167ce7e0388adcda667

--- IntegrationTests/src/bkr/inttest/client/test_system_details.py
+++ IntegrationTests/src/bkr/inttest/client/test_system_details.py
@@ -6,6 +6,7 @@
 
 from bkr.inttest import data_setup, with_transaction
 from bkr.inttest.client import run_client, ClientTestCase
+import json
 import rdflib
 
 class SystemDetailsTest(ClientTestCase):
@@ -14,8 +15,18 @@ class SystemDetailsTest(ClientTestCase):
     def setUp(self):
         self.system = data_setup.create_system()
 
-    def test_system_details(self):
+    def test_system_details_xml(self):
         out = run_client(['bkr', 'system-details', self.system.fqdn])
         g = rdflib.Graph()
         g.parse(data=out)
         self.assert_(len(g) > 0)
+
+    def test_system_details_json(self):
+        out = run_client(['bkr', 'system-details', '--format', 'json',
+                         self.system.fqdn])
+        self.assert_(len(out) > 0)
+
+        # Make sure you got what you asked for
+        payload = json.loads(out)
+        self.assertEquals(payload['fqdn'],
+                          self.system.fqdn)
