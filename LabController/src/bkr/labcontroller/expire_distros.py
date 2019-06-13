998b4278bee72c26540f17e8a1b1246fd2efd90a
Martin Styk <mastyk@redhat.com>
2019-06-13 07:38:40 +0200

    Add opt to run expire-distoros on different LC
    Bug: 1720067
    Change-Id: I55543889d48e48a24d9fa4ec62499a04609bfd65
    Signed-off-by: Martin Styk <mastyk@redhat.com>

--- LabController/src/bkr/labcontroller/expire_distros.py
+++ LabController/src/bkr/labcontroller/expire_distros.py
@@ -9,6 +9,7 @@ import xmlrpclib
 import urllib2
 import urlparse
 
+
 def check_http(url):
     try:
         urllib2.urlopen(url, timeout=120)
@@ -18,6 +19,7 @@ def check_http(url):
             return False
         else:
             raise
+
 
 def check_ftp(url):
     try:
@@ -29,11 +31,16 @@ def check_ftp(url):
         else:
             raise
 
-class NFSServerInaccessible(ValueError): pass
+
+class NFSServerInaccessible(ValueError):
+    pass
+
 
 def check_nfs(tree):
-    """ Make sure the tree is accessible, check that the server is up first.
     """
+    Make sure the tree is accessible, check that the server is up first.
+    """
+
     _, nfs_server, nfs_path, _, _, _ = urlparse.urlparse(tree)
     # Beaker uses a non-standard syntax for NFS URLs, inherited from Cobbler:
     # nfs://server:/path
@@ -45,16 +52,18 @@ def check_nfs(tree):
     tree_path = os.path.join(server_path, nfs_path)
     if not os.path.exists(server_path):
         raise NFSServerInaccessible('Cannot access NFS server %s '
-                'or autofs not running (%s does not exist)'
-                % (nfs_server, server_path))
+                                    'or autofs not running (%s does not exist)'
+                                    % (nfs_server, server_path))
     if not os.path.exists(tree_path):
         return False
     return True
+
 
 def check_url(url):
     """
     Returns True if the given URL exists.
     """
+
     scheme = urlparse.urlparse(url).scheme
     if scheme == 'nfs' or scheme.startswith('nfs+'):
         return check_nfs(url)
@@ -65,8 +74,9 @@ def check_url(url):
     else:
         raise ValueError('Unrecognised URL scheme %s for tree %s' % (scheme, url))
 
-def check_all_trees(ignore_errors=False, dry_run=False):
-    proxy = xmlrpclib.ServerProxy('http://localhost:8000', allow_none=True)
+
+def check_all_trees(ignore_errors=False, dry_run=False, lab_controller='http://localhost:8000'):
+    proxy = xmlrpclib.ServerProxy(lab_controller, allow_none=True)
     rdistro_trees = []
     distro_trees = proxy.get_distro_trees()
     for distro_tree in distro_trees:
@@ -76,15 +86,17 @@ def check_all_trees(ignore_errors=False, dry_run=False):
                 if check_url(url):
                     accessable = True
                 else:
-                    print '%s is missing' % url
-            except (urllib2.URLError, urllib2.HTTPError, NFSServerInaccessible), e:
+                    print('{0} is missing [Distro Tree ID {1}]'.format(
+                        url,
+                        distro_tree['distro_tree_id']))
+            except (urllib2.URLError, urllib2.HTTPError, NFSServerInaccessible) as e:
                 if ignore_errors:
                     # suppress exception, assume the tree still exists
                     accessable = True
                 else:
                     sys.stderr.write('Error checking for existence of URL %s '
-                            'for distro tree %s:\n%s\n'
-                            % (url, distro_tree['distro_tree_id'], e))
+                                     'for distro tree %s:\n%s\n'
+                                     % (url, distro_tree['distro_tree_id'], e))
                     sys.exit(1)
         if not accessable:
             # All methods were unaccessable!
@@ -97,11 +109,11 @@ def check_all_trees(ignore_errors=False, dry_run=False):
                 print('No longer accessible distro %s:%d' % (distro_tree['distro_name'],
                                                              distro_tree['distro_tree_id']))
             else:
-                print "Removing distro %s:%d" % (distro_tree['distro_name'],
-                                                 distro_tree['distro_tree_id'])
+                print('Removing distro %s:%d' % (distro_tree['distro_name'],
+                                                 distro_tree['distro_tree_id']))
                 proxy.remove_distro_trees([distro_tree['distro_tree_id']])
     else:
-        sys.stderr.write("All distros are missing! Please check your server!!\n")
+        sys.stderr.write('All distros are missing! Please check your server!\n')
         sys.exit(1)
 
 
@@ -109,12 +121,16 @@ def main():
     from optparse import OptionParser
     parser = OptionParser()
     parser.add_option('--ignore-errors', default=False, action='store_true',
-            help='Ignore all network errors when communicating with mirrors')
+                      help='Ignore all network errors when communicating with mirrors')
     parser.add_option('--dry-run', default=False, action='store_true',
                       help='Prints no longer accessible distro without updating the database.')
+    parser.add_option('--lab-controller',
+                      default='http://localhost:8000',
+                      help='Specify which lab controller to import to. '
+                           'Defaults to http://localhost:8000.')
     options, args = parser.parse_args()
     try:
-        check_all_trees(options.ignore_errors, options.dry_run)
+        check_all_trees(options.ignore_errors, options.dry_run, options.lab_controller)
     except KeyboardInterrupt:
         pass
 
