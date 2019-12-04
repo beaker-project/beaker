ecac7476764e357e80be666aa64118c9517a875a
Martin Styk <mastyk@redhat.com>
2019-12-04 10:23:21 +0100

    Automatic commit of release 27.0rc3
    Change-Id: I0a6d89f3a8261a0e4fd6841c4c706c927ef95a63

--- Client/setup.py
+++ Client/setup.py
@@ -22,7 +22,7 @@ def bash_completion_dir():
 
 setup(
     name='beaker-client',
-    version='27.0rc2',
+    version='27.0rc3',
     description='Command-line client for interacting with Beaker',
     author='Red Hat, Inc.',
     author_email='beaker-devel@lists.fedorahosted.org',
--- LabController/setup.py
+++ LabController/setup.py
@@ -49,7 +49,7 @@ else:
 
 setup(
     name='beaker-lab-controller',
-    version='27.0rc2',
+    version='27.0rc3',
     description='Daemons for controlling a Beaker lab',
     author='Red Hat, Inc.',
     author_email='beaker-devel@lists.fedorahosted.org',
