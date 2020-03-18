21fe3ae524beea144ea9e6e79a4937367a9e1b0f
Martin Styk <mastyk@redhat.com>
2020-03-18 17:55:53 +0100

    Automatic commit of release 27.3
    Change-Id: I3b0f3ddb777b5118e9290fe63ee6b7f66a0b6125

--- LabController/setup.py
+++ LabController/setup.py
@@ -49,7 +49,7 @@ else:
 
 setup(
     name='beaker-lab-controller',
-    version='27.2',
+    version='27.3',
     description='Daemons for controlling a Beaker lab',
     author='Red Hat, Inc.',
     author_email='beaker-devel@lists.fedorahosted.org',
