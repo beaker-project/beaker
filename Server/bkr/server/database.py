e39b2605dd66a6196468fd1e832f9d4d1f277fff
Martin Styk <mart.styk@gmail.com>
2021-04-24 18:43:04 +0200

    fix: add bind_metadata to database
    Signed-off-by: Martin Styk <mart.styk@gmail.com>

--- Server/bkr/server/database.py
+++ Server/bkr/server/database.py
@@ -1,5 +1,6 @@
-from turbogears.database import session, metadata, get_engine
+from turbogears.database import session, metadata, get_engine, bind_metadata
 
+bind_metadata = bind_metadata
 session = session
 metadata = metadata
 get_engine = get_engine
