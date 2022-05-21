3c4a7687faf0441a872f1ef5438f96199a176b91
Martin Styk <mart.styk@gmail.com>
2022-05-21 15:09:37 +0200

    Automatic commit of release 28.3

--- LabController/setup.py
+++ LabController/setup.py
@@ -49,7 +49,7 @@ else:
 
 setup(
     name='beaker-lab-controller',
-    version='28.2',
+    version='28.3',
     description='Daemons for controlling a Beaker lab',
     author='Red Hat, Inc.',
     author_email='beaker-devel@lists.fedorahosted.org',
