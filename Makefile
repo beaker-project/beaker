beaker-project/beaker

commit acfabb4bf4413f4a2f65d94e0933c8a0f649deb9

Devel patch to activate beaker from local git tree
Add a 'devel' target to all Makefiles. This target causes the local
beaker source tree to be installed on the local host.

Change-Id: Ifc6087a61e36aa86ef700eeb6785ff3d952efd57
Signed-off-by: Clark Williams <williams@redhat.com>

Makefile
@@ -4,6 +4,8 @@
 # the Free Software # Foundation, either version 2 of the License, or
 # (at your option) any later version.
 
+DEPCMD  :=  $(shell if [ -f /usr/bin/dnf ]; then echo "dnf builddep"; else echo "yum-builddep"; fi)
+
 SUBDIRS := Common Client documentation
 ifdef WITH_SERVER
     SUBDIRS += Server
@@ -27,6 +29,16 @@ install:
 clean:
 	set -e; for i in $(SUBDIRS); do $(MAKE) -C $$i clean; done
 
-.PHONY: check
 check:
 	set -e; for i in $(SUBDIRS); do $(MAKE) -C $$i check; done
+
+.PHONY: devel
+devel: build
+	set -e; for i in $(SUBDIRS); do $(MAKE) -C $$i devel; done
+
+deps:
+	sudo $(DEPCMD) -y beaker.spec
+
+submods:
+	git submodules init
+	git submodules update

Client/Makefile
@@ -21,3 +21,7 @@ clean:
 .PHONY: check
 check:
 	./run-tests.sh
+
+.PHONY: devel
+devel:
+	DESTDIR="/" make install

Common/Makefile
@@ -27,3 +27,7 @@ clean:
 .PHONY: check
 check:
 	./run-tests.sh
+
+.PHONY: devel
+devel:
+	DESTDIR="/" make install

LabController/Makefile
@@ -20,3 +20,7 @@ clean:
 .PHONY: check
 check:
 	./run-tests.sh
+
+.PHONY: devel
+devel:
+	DESTDIR="/" make install

Server/Makefile
@@ -1,6 +1,8 @@
 # License: GPL v2 or later
 # Copyright Red Hat Inc. 2008
 
+INITSYS :=  $(shell if [ -f /usr/bin/systemctl ]; then echo "systemd"; else echo "sysvinit"; fi)
+
 .PHONY: build
 build:
 	env PYTHONPATH=../Common$${PYTHONPATH:+:$$PYTHONPATH} \
@@ -19,6 +21,21 @@ clean:
 .PHONY: check
 check:
 	./run-tests.sh
+
+.PHONY: devel
+devel:
+	-if [[ $(INITSYS) == "systemd" ]] ; then \
+		systemctl stop beakerd; \
+	else \
+		service beakerd stop; \
+	fi
+	DESTDIR="/" make install
+	if [[ $(INITSYS) == "systemd" ]] ; then \
+		systemctl daemon-reload; \
+		systemctl restart beakerd; \
+	else \
+		service beakerd restart; \
+	fi
 
 assets/favicon.ico: assets/favicon-16.png assets/favicon-32.png assets/favicon-64.png
 	icotool -c -o $@ $^

documentation/Makefile
@@ -5,23 +5,23 @@ SHELL = /bin/bash
 export PYTHONPATH=../Common:../Server:../Client/src
 SPHINXBUILD ?= $(firstword $(shell command -v sphinx-1.0-build sphinx-build))
 
-# This Makefile contains some frustrating hacks, centering around the fact that 
-# the docs use autodoc and therefore need to be able to import bkr modules from 
+# This Makefile contains some frustrating hacks, centering around the fact that
+# the docs use autodoc and therefore need to be able to import bkr modules from
 # the source tree which they live inside.
 #
-# pkg_resources.requires() does not work if multiple versions are installed in 
+# pkg_resources.requires() does not work if multiple versions are installed in
 # parallel, but we need to be able to load bkr for autodoc generation. The
 # semi-supported hack using __requires__ in the $SPHINXBUILD definition
 # is a workaround: http://bugs.python.org/setuptools/issue139
 # (Fedora/EPEL has python-cherrypy2 = 2.3 and python-cherrypy = 3)
 # (EPEL6 has python-sphinx = 0.6 and python-sphinx10 = 1.0)
 #
-# In addition, if the pkg_resources dependency chain includes anything which 
-# needs to be added to sys.path *and* anything which is in site-packages, then 
-# site-packages gets prepended to sys.path in front of anything we pass in 
-# through PYTHONPATH. That means, if you have bkr in site-packages it will be 
-# imported instead of the local source. To work around the pkg_resources 
-# behaviour we have to fiddle with bkr.__path__ ourselves in addition to 
+# In addition, if the pkg_resources dependency chain includes anything which
+# needs to be added to sys.path *and* anything which is in site-packages, then
+# site-packages gets prepended to sys.path in front of anything we pass in
+# through PYTHONPATH. That means, if you have bkr in site-packages it will be
+# imported instead of the local source. To work around the pkg_resources
+# behaviour we have to fiddle with bkr.__path__ ourselves in addition to
 # setting PYTHONPATH.
 #
 # Also see: https://bitbucket.org/pypa/setuptools/issue/6/
@@ -199,3 +199,6 @@ doctest:
 	$(SPHINXBUILD) -b doctest $(ALLSPHINXOPTS) $(BUILDDIR)/doctest
 	@echo "Testing of doctests in the sources finished, look at the " \
 	      "results in $(BUILDDIR)/doctest/output.txt."
+
+devel:
+	@echo "nothing needed for devel target in documentation"

commit f834e28ef106ddb9ab6aea55bff5029b37086bf3

Fix submods target in Makefile
Fixes: #172
Fixes: acfabb4bf441 ("Devel patch to activate beaker from local git tree")

Makefile
@@ -36,5 +36,5 @@ deps:
 	sudo $(DEPCMD) -y beaker.spec
 
 submods:
-	git submodules init
-	git submodules update
+	git submodule init
+	git submodule update