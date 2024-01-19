d5dafec51bfd09f0fd72eb5e808fff7a7bde141a
Martin Styk <mart.styk@gmail.com>
2024-01-19 14:43:57 +0100

    feat: enable lab-controller build for Python 3
    Signed-off-by: Martin Styk <mart.styk@gmail.com>

--- LabController/run-tests.sh
+++ LabController/run-tests.sh
@@ -1,5 +1,16 @@
-#/bin/bash
+#!/bin/bash
 set -x
 
-env PYTHONPATH=src:../Common${PYTHONPATH:+:$PYTHONPATH} \
-    nosetests ${*:--v --traverse-namespace bkr.labcontroller}
+# Use nosetests with python2 interpreter
+if [[ -z ${BKR_PY3} ]] || [[ ${BKR_PY3} != 1 ]]; then
+    command="nosetests ${*:--v --traverse-namespace bkr.labcontroller}";
+else
+    # Check if pytest-3 is available
+    if command -v pytest-3 >/dev/null 2>&1; then
+        command="pytest-3";
+    else
+        command="pytest";
+    fi
+fi
+
+env PYTHONPATH=src:../Common${PYTHONPATH:+:$PYTHONPATH} $command
