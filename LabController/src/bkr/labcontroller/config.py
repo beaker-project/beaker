0acf0e1bc16b60800a91c40b9b131cc0a5c03d29
Martin Styk <mart.styk@gmail.com>
2024-01-19 19:32:15 +0100

    chore(config): format code and sort imports
    Signed-off-by: Martin Styk <mart.styk@gmail.com>

--- LabController/src/bkr/labcontroller/config.py
+++ LabController/src/bkr/labcontroller/config.py
@@ -1,36 +1,39 @@
-
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 
 import copy
-import socket
 import os
+import socket
+
 from bkr.common.pyconfig import PyConfigParser
 
-__all__ = ['load_conf', 'get_conf']
+__all__ = ["load_conf", "get_conf"]
 
 
 class Config(PyConfigParser):
-
     def get_url_domain(self):
         # URL_DOMAIN used to be called SERVER
-        return self.get('URL_DOMAIN',
-            self.get('SERVER', socket.gethostname()))
+        return self.get("URL_DOMAIN", self.get("SERVER", socket.gethostname()))
 
 
 _conf = Config()
-default_config = os.path.abspath(os.path.join(os.path.dirname(__file__), "default.conf"))
+default_config = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "default.conf")
+)
 _conf.load_from_file(default_config)
 default_system_conf_file = "/etc/beaker/labcontroller.conf"
 
 _conf_loaded = False
+
+
 def load_conf(conf_file=default_system_conf_file):
     global _conf, _conf_loaded
     # Will throw IOError if file does not exist
     _conf.load_from_file(conf_file)
     _conf_loaded = True
+
 
 def get_conf():
     global _conf, _conf_loaded
