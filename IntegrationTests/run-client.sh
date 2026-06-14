ccbe790cfb6e2ae6282037cca8df27650a09ddb0
Martin Styk <mart.styk@gmail.com>
2026-06-14 13:51:03 +0200

    ci: add Python 3 client integration test job

--- IntegrationTests/run-client.sh
+++ IntegrationTests/run-client.sh
@@ -4,4 +4,4 @@
 # building or installing anything. Used by integration tests.
 
 exec env PYTHONPATH=$(dirname "$0")/../Common:$(dirname "$0")/../Client/src${PYTHONPATH:+:$PYTHONPATH} \
-    python2 -u $(dirname "$0")/../Client/src/bkr/client/main.py "$@"
+    "${BEAKER_CLIENT_PYTHON:-python2}" -u $(dirname "$0")/../Client/src/bkr/client/main.py "$@"
