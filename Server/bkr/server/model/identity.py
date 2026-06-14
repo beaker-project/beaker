13280baf694cc06684ff5a03e2994983b417508c
Martin Styk <mart.styk@gmail.com>
2026-06-14 16:27:57 +0200

    refactor: decode passlib hash with six.ensure_text on python 3
    Signed-off-by: Martin Styk <mart.styk@gmail.com>

--- Server/bkr/server/model/identity.py
+++ Server/bkr/server/model/identity.py
@@ -371,7 +371,7 @@ class User(DeclarativeMappedObject, ActivityMixin):
     )
 
     def _set_password(self, raw_password):
-        self._password = self._password_context.encrypt(raw_password).decode('ascii')
+        self._password = six.ensure_text(self._password_context.encrypt(raw_password))
 
     def _get_password(self):
         return self._password
