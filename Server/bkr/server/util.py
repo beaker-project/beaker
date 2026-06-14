2337c7396ed5376602fbd5086e11ace6a9b4e6d4
Martin Styk <mart.styk@gmail.com>
2026-06-14 16:51:57 +0200

    refactor: use util.ensure_text instead of six.ensure_text
    Signed-off-by: Martin Styk <mart.styk@gmail.com>

--- Server/bkr/server/util.py
+++ Server/bkr/server/util.py
@@ -123,6 +123,13 @@ def ensure_str(s, encoding='utf-8', errors='strict'):
         raise TypeError("not expecting type '%s'" % type(s))
     return s
 
+def ensure_text(s, encoding='utf-8', errors='strict'):
+    if isinstance(s, six.binary_type):
+        return s.decode(encoding, errors)
+    elif isinstance(s, six.text_type):
+        return s
+    raise TypeError("not expecting type '%s'" % type(s))
+
 def _query_arg(value):
     if six.PY2 and isinstance(value, six.text_type):
         return value.encode('utf-8')
