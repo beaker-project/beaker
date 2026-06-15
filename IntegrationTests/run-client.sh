70fc14d40a69ff56f80bd6c164b7b9ee875fe84b
Martin Styk <mart.styk@gmail.com>
2026-06-15 10:52:53 +0200

    test: enable bkr.inttest.client suite on Python 3
    Signed-off-by: Martin Styk <mart.styk@gmail.com>

--- IntegrationTests/run-client.sh
+++ IntegrationTests/run-client.sh
@@ -3,5 +3,11 @@
 # Just a wrapper to run bkr client directly from a source checkout, without 
 # building or installing anything. Used by integration tests.
 
+if [[ ${BKR_PY3} == 1 ]]; then
+    python=python3
+else
+    python=python2
+fi
+
 exec env PYTHONPATH=$(dirname "$0")/../Common:$(dirname "$0")/../Client/src${PYTHONPATH:+:$PYTHONPATH} \
-    python2 -u $(dirname "$0")/../Client/src/bkr/client/main.py "$@"
+    $python -u $(dirname "$0")/../Client/src/bkr/client/main.py "$@"
